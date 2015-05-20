47e031f71fa8c7cdd5739b78c3a9ea6f6ba40d97
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-20 02:13:36 +0200

    polybench: autopep8
    Former-commit-id: 4e702e46643ecb683c4e47d514abf6a2bd70d7d5
    Former-commit-id: 85b1b23d621b8495707a622568db4e00434fb1b6 [formerly 5be3adbf32848dbb01f7ca14db823c926011b394] [formerly dcd71dc227d7224f99386b280f6fd575398d713e [formerly ae701eff813ef4f64bf6f6b10676c60b960fadef]]
    Former-commit-id: ddc2c9f2a03b70a0d0a017bd35a0b9bbe5c12b93 [formerly 77ccf088168cfb4ee4a5bbec2d42126a99af4761]
    Former-commit-id: 04adb392c1ad417f9f4bd13716c1cb06e974d3f2

--- driver/pprof/projects/polybench/polybench.py
+++ driver/pprof/projects/polybench/polybench.py
@@ -7,6 +7,7 @@ from pprof.settings import config
 from os import path
 from plumbum import FG, local
 
+
 class PolyBenchGroup(Project):
 
     path_dict = {
@@ -43,17 +44,18 @@ class PolyBenchGroup(Project):
     }
 
     def __init__(self, exp, name):
-        super(PolyBenchGroup, self).__init__(exp, name, "polybench", "polybench")
+        super(PolyBenchGroup, self).__init__(
+            exp, name, "polybench", "polybench")
         self.sourcedir = path.join(config["sourcedir"],
                                    "polybench", self.path_dict[name], name)
         self.setup_derived_filenames()
         self.calls_f = path.join(self.builddir, "papi.calls.out")
         self.prof_f = path.join(self.builddir, "papi.profile.out")
 
-
     src_dir = "polybench-c-4.1"
     src_file = src_dir + ".tar.gz"
     src_uri = "http://downloads.sourceforge.net/project/polybench/" + src_file
+
     def download(self):
         from pprof.utils.downloader import Wget
         from plumbum.cmd import tar
@@ -65,7 +67,7 @@ class PolyBenchGroup(Project):
         from plumbum.cmd import cp
         with local.cwd(self.builddir):
             cp("-ar", path.join(self.src_dir, self.path_dict[self.name],
-               self.name), self.name + ".dir")
+                                self.name), self.name + ".dir")
             cp("-ar", path.join(self.src_dir, "utilities"), ".")
 
     def build(self):
@@ -79,212 +81,272 @@ class PolyBenchGroup(Project):
             print myclang
             myclang()
 
+
 class Correlation(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Correlation(exp, "correlation")
     ProjectFactory.addFactory("Correlation", Factory())
 
 
 class Covariance(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Covariance(exp, "covariance")
     ProjectFactory.addFactory("Covariance", Factory())
 
 
 class TwoMM(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return TwoMM(exp, "2mm")
     ProjectFactory.addFactory("TwoMM", Factory())
 
 
 class ThreeMM(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return ThreeMM(exp, "3mm")
     ProjectFactory.addFactory("ThreeMM", Factory())
 
 
 class Atax(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Atax(exp, "atax")
     ProjectFactory.addFactory("Atax", Factory())
 
 
 class BicG(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return BicG(exp, "bicg")
     ProjectFactory.addFactory("BicG", Factory())
 
 
 class Cholesky(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Cholesky(exp, "cholesky")
     ProjectFactory.addFactory("Cholesky", Factory())
 
 
 class Doitgen(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Doitgen(exp, "doitgen")
     ProjectFactory.addFactory("Doitgen", Factory())
 
 
 class Gemm(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Gemm(exp, "gemm")
     ProjectFactory.addFactory("Gemm", Factory())
 
 
 class Gemver(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Gemver(exp, "gemver")
     ProjectFactory.addFactory("Gemver", Factory())
 
 
 class Gesummv(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Gesummv(exp, "gesummv")
     ProjectFactory.addFactory("Gesummv", Factory())
 
 
 class Mvt(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Mvt(exp, "mvt")
     ProjectFactory.addFactory("Mvt", Factory())
 
 
 class Symm(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Symm(exp, "symm")
     ProjectFactory.addFactory("Symm", Factory())
 
 
 class Syr2k(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Syr2k(exp, "syr2k")
     ProjectFactory.addFactory("Syr2k", Factory())
 
 
 class Syrk(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Syrk(exp, "syrk")
     ProjectFactory.addFactory("Syrk", Factory())
 
 
 class Trisolv(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Trisolv(exp, "trisolv")
     ProjectFactory.addFactory("Trisolv", Factory())
 
 
 class Trmm(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Trmm(exp, "trmm")
     ProjectFactory.addFactory("Trmm", Factory())
 
 
 class Durbin(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Durbin(exp, "durbin")
     ProjectFactory.addFactory("Durbin", Factory())
 
 
 class Deriche(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Deriche(exp, "deriche")
     ProjectFactory.addFactory("Deriche", Factory())
 
+
 class Gramschmidt(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Gramschmidt(exp, "gramschmidt")
     ProjectFactory.addFactory("Gramschmidt", Factory())
 
 
 class Lu(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Lu(exp, "lu")
     ProjectFactory.addFactory("Lu", Factory())
 
 
 class LuDCMP(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return LuDCMP(exp, "ludcmp")
     ProjectFactory.addFactory("LuDCMP", Factory())
 
 
 class FloydWarshall(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return FloydWarshall(exp, "floyd-warshall")
     ProjectFactory.addFactory("FloydWarshall", Factory())
 
 
 class Adi(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Adi(exp, "adi")
     ProjectFactory.addFactory("Adi", Factory())
 
 
 class FDTD2D(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return FDTD2D(exp, "fdtd-2d")
     ProjectFactory.addFactory("FDTD2D", Factory())
 
 
 class Jacobi1D(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Jacobi1D(exp, "jacobi-1d")
     ProjectFactory.addFactory("Jacobi1D", Factory())
 
 
 class Jacobi2Dimper(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Jacobi2Dimper(exp, "jacobi-2d")
     ProjectFactory.addFactory("Jacobi2D", Factory())
 
 
 class Seidel2D(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Seidel2D(exp, "seidel-2d")
     ProjectFactory.addFactory("Seidel2D", Factory())
 
 
 class Nussinov(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Nussinov(exp, "nussinov")
     ProjectFactory.addFactory("Nussinov", Factory())
 
 
 class Heat3D(PolyBenchGroup):
+
     class Factory:
+
         def create(self, exp):
             return Heat3D(exp, "heat-3d")
     ProjectFactory.addFactory("heat-3d", Factory())
-
-

7963394dc6cdc1979fe4a54dcb17d1c5cd4e5545
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-21 01:00:29 +0200

    povray: fix runtime tests

--- driver/pprof/projects/pprof/povray.py
+++ driver/pprof/projects/pprof/povray.py
@@ -22,32 +22,40 @@ class Povray(PprofGroup):
     ProjectFactory.addFactory("Povray", Factory())
 
     src_uri = "https://github.com/POV-Ray/povray"
-    src_dir = "povray"
+    src_dir = "povray.git"
     def download(self):
         from pprof.utils.downloader import Git
-        from plumbum.cmd import git
         with local.cwd(self.builddir):
             Git(self.src_uri, self.src_dir)
 
     def configure(self):
         povray_dir = path.join(self.builddir, self.src_dir)
         with local.cwd(path.join(povray_dir, "unix")):
-            prebuild_sh = local["./prebuild.sh"]
-            prebuild_sh()
+            from plumbum.cmd import sh
+            sh("prebuild.sh")
 
         with local.cwd(povray_dir):
+            from pprof.project import clang, clang_cxx
             configure = local["./configure"]
-            with local.env(COMPILED_BY="PPROF <no@mail.nono>"):
+            with local.env(COMPILED_BY="PPROF <no@mail.nono>",
+                           CC=clang(),
+                           CFLAGS=" ".join(self.cflags),
+                           CXX=clang_cxx(),
+                           CXXFLAGS=" ".join(self.cflags),
+                           LDFLAGS=" ".join(self.ldflags)):
                 configure()
 
     def build(self):
-        from plumbum.cmd import make, ln
+        from plumbum.cmd import make, ln, mv, rm
         povray_dir = path.join(self.builddir, self.src_dir)
+        povray_binary = path.join(povray_dir, "unix", self.name)
 
         with local.cwd(povray_dir):
+            rm("-f", povray_binary)
             make & FG
-
-        ln("-sf", path.join(povray_dir, "bin", "povray"), self.bin_f)
+            #make["clean", "all"] & FG
+        mv(path.join(povray_dir, "unix", self.name), self.bin_f)
+        self.run_f = self.bin_f
 
     def prepare(self):
         super(Povray, self).prepare()
@@ -58,22 +66,31 @@ class Povray(PprofGroup):
         cp["-ar", path.join(self.testdir, "test"), self.builddir] & FG
 
     def run_tests(self, experiment):
+        from plumbum.cmd import mkdir, chmod
+
+        povray_dir = path.join(self.builddir, self.src_dir)
+        povray_binary = path.join(povray_dir, "unix", self.name)
         tmpdir = path.join(self.builddir, "tmp")
         povini = path.join(self.builddir, "cfg", ".povray", "3.6", "povray.ini")
-
+        scene_dir = path.join(self.builddir, "share", "povray-3.6", "scenes")
         mkdir(tmpdir, retcode=None)
-        bin_name = path.join(self.builddir, self.name + ".sh")
-        scene_dir = path.join(self.builddir, "share", "povray-3.6",
-                              "scenes")
 
-        echo["#!/bin/sh"] >> bin_name & FG
-        echo[str(experiment) + " $*"] >> bin_name & FG
-        chmod("+x", bin_name)
+        with open(povray_binary, 'w') as povray:
+            povray.write("#!/bin/sh\n")
+            povray.write(str(experiment) + " \"$@\"")
+        chmod("+x", povray_binary)
 
-        render = local[path.join(self.builddir, "test", "scripts",
+        render = local[path.join(povray_dir, "scripts",
                                  "render_scene.sh")]
         pov_files = find(scene_dir, "-name", "*.pov").splitlines()
         for pov_f in pov_files:
-            with local.env(POVRAY=bin_name, INSTALL_DIR=self.builddir,
+            from plumbum.cmd import head, grep, sed
+            with local.env(POVRAY=povray_binary, INSTALL_DIR=self.builddir,
                            OUTPUT_DIR=tmpdir, POVINI=povini):
-                render[tmpdir, "--all", pov_f] & FG(retcode=None)
+                options = ((((head["-n", "50", "\""+pov_f+"\""] |
+                          grep["-E", "'^//[ ]+[-+]{1}[^ -]'"]) |
+                          head["-n", "1"]) |
+                          sed["s?^//[ ]*??"]) & FG)
+                povray = local[povray_binary]
+                povray("+L" + scene_dir, "+L" + tmpdir, "-i" + pov_f,
+                       "-o" + tmpdir, options, "-p", retcode = None)

6014d91b639911d075c0ca47410de373410ff7d7
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-24 17:16:41 +0200

    bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 setup(name='pprof',
-      version='0.6',
+      version='0.6.1',
       packages= find_packages(),
       author = "Andreas Simbuerger",
       author_email = "simbuerg@fim.uni-passau.de",

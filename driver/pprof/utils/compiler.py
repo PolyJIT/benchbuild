5a01ab4a4730c916b6a0dcd65aba248444db4802
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-24 13:51:02 +0200

    add missing compiler.py
    woops.
    
    
    Former-commit-id: 6e10dd48f177b97ef4ada38028ed4dcbad8a90ab
    Former-commit-id: d91bba271891093c55da48c3120b25c9b7422e8e [formerly 7b01ea89b716fcdbd7f3a55bdb9d8b42ffc16843] [formerly 710da1ea55531c8bc13e4ae55e59180054b828fb [formerly 85ca5b7539255db741a9d97bf7469fa572e520c2]]
    Former-commit-id: 137afda7267570702a8761bb4eee0ff80c11de6c [formerly c6bbaa690a9a39e501cc30113307d635ce29d2b2]
    Former-commit-id: 2ef86eea5ace623adb1c7c6f218520fefca85229

---
+++ driver/pprof/utils/compiler.py
@@ -0,0 +1,74 @@
+from pprof.settings import config
+from plumbum import local
+from os import path
+
+
+def lt_clang(flags_to_hide):
+    """Return a clang that hides :flags_to_hide: from reordering of libtool.
+
+    This will generate a wrapper script in :p:'s builddir and return a path
+    to it.
+
+    :flags_to_hide: the flags libtool is not allowed to see.
+    :returns: path to the new clang.
+
+    """
+    from plumbum import local
+    from os import path
+
+    print_libtool_sucks_wrapper("clang", flags_to_hide, clang)
+    return local["./clang"]
+
+
+def lt_clang_cxx(flags_to_hide):
+    """Return a clang++ that hides :flags_to_hide: from reordering of libtool.
+
+    This will generate a wrapper script in :p:'s builddir and return a path
+    to it.
+
+    :flags_to_hide: the flags libtool is not allowed to see.
+    :returns: path to the new clang++.
+
+    """
+    from plumbum import local
+    from os import path
+    print_libtool_sucks_wrapper("clang++", flags_to_hide, clang_cxx)
+
+    return local["./clang++"]
+
+def print_libtool_sucks_wrapper(filepath, flags_to_hide, compiler):
+    """Print a libtool wrapper that hides :flags_to_hide: from libtool.
+
+    :filepath:
+        Where should the new compiler be?
+    :flags_to_hide:
+        List of flags that should be hidden from libtool
+    :compiler:
+        The compiler we should actually call
+    """
+    from plumbum.cmd import chmod
+
+    with open(filepath, 'w') as wrapper:
+        wrapper.writelines(
+            [
+                "#!/bin/sh\n",
+                'FLAGS="' + " ".join(flags_to_hide) + '"\n',
+                str(compiler()) + " $FLAGS $*\n"
+            ]
+        )
+    chmod("+x", filepath)
+
+def llvm():
+    return path.join(config["llvmdir"], "bin")
+
+
+def llvm_libs():
+    return path.join(config["llvmdir"], "lib")
+
+
+def clang_cxx():
+    return local[path.join(llvm(), "clang++")]
+
+
+def clang():
+    return local[path.join(llvm(), "clang")]

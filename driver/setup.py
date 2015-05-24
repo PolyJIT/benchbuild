f0f46f415aa1c377c4fa34728a594114b5ec91a2
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-24 13:52:16 +0200

    bump version to 0.6

--- driver/setup.py
+++ driver/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 setup(name='pprof',
-      version='0.5.6',
+      version='0.6',
       packages= find_packages(),
       author = "Andreas Simbuerger",
       author_email = "simbuerg@fim.uni-passau.de",

4d60fa5c90572a4d04bd3d2a8f69a1fc5206104e
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-26 20:41:30 +0200

    chimaira: actually make pprof run on chimaira

--- pprof/settings.py
+++ pprof/settings.py
@@ -127,39 +127,18 @@ config = {
     "path": os.environ["PATH"],
     "ld_library_path": os.environ["LD_LIBRARY_PATH"],
     "jobs": os.getenv("PPROF_MAKE_JOBS", str(available_cpu_count())),
-    "experiment": os.getenv("PPROF_EXPERIMENT_ID", uuid4())
+    "experiment": os.getenv("PPROF_EXPERIMENT_ID", uuid4()),
+    "db_host" : os.getenv("PPROF_DB_HOST", "localhost"),
+    "db_port" : os.getenv("PPROF_DB_PORT", 49153),
+    "db_name" : os.getenv("PPROF_DB_NAME", "pprof"),
+    "db_user" : os.getenv("PPROF_DB_USER", "pprof"),
+    "db_pass" : os.getenv("PPROF_DB_PASS", "pprof"),
 }
 
 
 def setup_db_config():
     global config
-
-    config["db_host"] = "localhost"
-    config["db_port"] = 49153
-    config["db_name"] = "pprof"
-    config["db_user"] = "pprof"
-    config["db_pass"] = "pprof"
-
-    db_host = os.environ.get("PPROF_DB_HOST")
-    if db_host:
-        config["db_host"] = db_host
-
-    db_port = os.environ.get("PPROF_DB_PORT")
-    if db_port:
-        config["db_port"] = db_port
-
-    db_name = os.environ.get("PPROF_DB_NAME")
-    if db_name:
-        config["db_name"] = db_name
-
-    db_user = os.environ.get("PPROF_DB_USER")
-    if db_user:
-        config["db_user"] = db_user
-
-    db_pass = os.environ.get("PPROF_DB_PASS")
-    if db_pass:
-        config["db_pass"] = db_pass
-
+    pass
 
 _db_connection = None
 

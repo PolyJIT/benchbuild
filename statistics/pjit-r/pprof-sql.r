9051d55f21fb504ff729b45dc3501e966dfcf3cc
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-26 17:22:43 +0200

    pprof-sql: prepare heatmap plotting
    Former-commit-id: 03ae66d10c5ab773b7f482d3bffcc35a46b25b61
    Former-commit-id: 8d05f63ac15998fce93655cfc75f9af5da481984 [formerly e158f36c979506a91697a28df69e10e03801c408] [formerly 8327d9427e12c0699d5236f8330f447407e96db4 [formerly f34dd6e019417ebce7baef4a654ce9206f8c6787]]
    Former-commit-id: 405b77c59a31421fb215861b4009d79af066043c [formerly a46a925713029e5b426bde101e1921f047967e67]
    Former-commit-id: 85f5949e6d83a0fdbf25276a44c5255c6bd9dcc4

--- statistics/pjit-r/pprof-sql.r
+++ statistics/pjit-r/pprof-sql.r
@@ -3,6 +3,15 @@ library(ggplot2)
 library(reshape)
 library(scales)
 library(plyr)
+if(!require("scales")) {
+  install.packages("scales", dependencies = TRUE)
+  library(scales)
+}
+
+if(!require("gplots")) {
+  install.packages("gplots", dependencies = TRUE)
+  library(gplots)
+}
 
 plot_dyncov <- function(experiment, connection) {
   cat(experiment)
@@ -77,6 +86,60 @@ plot_experiment <- function(experiment, connection) {
    }
 }
 
+plot_heatmap_run <- function(run, connection) {
+  heatmap_query <- sprintf(paste("SELECT r.project_name, pr.id, pr.type, pr.timestamp ",
+                                 "FROM public.run AS r, public.papi_results AS pr",
+                                 "WHERE r.id = pr.run_id ",
+                                 "AND r.id = %s ",
+                                 "AND type != 6 ",
+                                 "ORDER BY run_id, timestamp ;"), run)
+  heatmap_data <- dbGetQuery(connection, heatmap_query)
+
+  buckets <- 50
+  # Bucket Size depends on total runtime of the whole test.
+  bucket_size <- sum(papi[papi$Region == "START",3]) %/% buckets
+  papi[,1] <- sapply(papi[,1], function(x) x %/% bucket_size)
+  papi <- melt(papi, id=1:2)
+  #> head(papi_molten)
+  #StartTime Region variable   value
+  #1         0  START Duration 2357836
+  #2         0      a Duration     752
+  #3         0      b Duration     261
+  #4         0      a Duration     477
+  #5         0      b Duration     160
+  #6         0      a Duration     473
+
+  papi <- cast(papi, StartTime + variable ~ Region, sum)
+  #> head(cast(papi_molten, StartTime + variable ~ Region, sum))
+  #StartTime variable   START     a     b c
+  #1         0 Duration 2357836 28750 11078 0
+  #2         1 Duration       0 34014 11727 0
+  #3         2 Duration       0 36060 12304 0
+  #4         3 Duration       0 35677 12855 0
+  #5         4 Duration       0 37503 13328 0
+  #6         5 Duration       0 38061 13373 0
+  papi[1,3] <- 0
+  papi_matrix <- t(data.matrix(papi[,4:ncol(papi)]))
+
+  rownames(papi) <- papi[,1]
+  my_colors <- colorRampPalette(c("white", "yellow", "red"))(n = 299)
+  heatmap.2(papi_matrix,
+            notecol="black",
+            main=opt$name,
+            trace="none",
+            col=my_colors,
+            scale= c("none"),
+            dendrogram="none",
+            Colv=FALSE)
+}
+
+plot_heatmap <- function(experiment, connection) {
+  run_query <- sprintf(paste("SELECT DISTINCT id FROM run WHERE experiment_group = '%s';"), experiment)
+  runs <- dbGetQuery(connection, run_query)
+
+  lapply(X = runs[,1], FUN = plot_heatmap_run, connection = con)
+}
+
 get_experiments <- function(connection) {
   rs <- dbGetQuery(connection, "SELECT DISTINCT experiment_group FROM run WHERE NOT experiment_group = '00000000-0000-0000-0000-000000000000'")
   return <- c(rs[,1])
@@ -86,6 +149,7 @@ drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname="pprof", user="pprof", host="localhost", port=32768, password="pprof")
 
 exps = get_experiments(connection = con)
+#lapply(X = exps, FUN = plot_heatmap, connection = con)
 lapply(X = exps, FUN = plot_dyncov, connection = con)
 lapply(X = exps, FUN = plot_experiment, connection = con)
 

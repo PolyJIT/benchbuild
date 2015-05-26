c32160d524d4f3aba23596dd905e556ce71568e9
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-05-26 17:22:30 +0200

    pprof-sql: plot dynamic scop coverage

--- statistics/pjit-r/pprof-sql.r
+++ statistics/pjit-r/pprof-sql.r
@@ -2,6 +2,42 @@ library(RPostgreSQL)
 library(ggplot2)
 library(reshape)
 library(scales)
+library(plyr)
+
+plot_dyncov <- function(experiment, connection) {
+  cat(experiment)
+  dyncov_query <- sprintf(paste("SELECT run.project_name, metrics.name, metrics.value",
+                                "FROM public.run, public.metrics ",
+                                "WHERE run.id = metrics.run_id ",
+                                "AND experiment_group ='%s' ",
+                                "AND metrics.name = 'pprof.dyncov'",
+                                "AND project_name != '%s' ",
+                                "AND project_name != '%s' ",
+                                "AND project_name != '%s' ",
+                                "AND project_name != '%s' ",
+                                ";"),
+                          experiment,
+                          "MultiSourceApplications",
+                          "SingleSourceApplications",
+                          "SingleSourceBenchmarks",
+                          "unknown")
+
+  dyncov_data <- melt(dbGetQuery(connection, dyncov_query))
+  if (nrow(dyncov_data) > 0) {
+    # Summarize data using median(!) over all runs, check if that makes any sense.
+    summarized <- ddply(dyncov_data, .(project_name, name), summarise, value=median(value))
+    summarized <- subset(summarized, value > 0)
+
+    plot <- ggplot(data = summarized, aes(x = project_name, y = value)) +
+      geom_point(stat = "identity", aes(color=project_name)) +
+      coord_flip() +
+      ggtitle(paste("Dynamic SCoP coverage for experiment run ", experiment)) +
+      theme(legend.position = "none",
+            axis.title.x = element_blank(),
+            axis.title.y = element_blank())
+    plot
+  }
+}
 
 plot_experiment <- function(experiment, connection) {
   cat(experiment)
@@ -22,8 +58,8 @@ plot_experiment <- function(experiment, connection) {
 
   rt_data <- melt(dbGetQuery(connection, rt_query))
   main_data <- melt(dbGetQuery(connection, main_query))
-  
-  
+
+
   if (nrow(rt_data) > 0 && nrow(main_data) > 0) {
     rt_data[,3] <- "Runtime Breakdown"
     main_data[,3] <- "Total Runtime"
@@ -37,6 +73,7 @@ plot_experiment <- function(experiment, connection) {
       #facet_wrap(~ project_name, scales = "free_y", ncol = 8) +
       ggtitle(paste("Experiment ID ", experiment)) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    plot
    }
 }
 
@@ -48,8 +85,9 @@ get_experiments <- function(connection) {
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname="pprof", user="pprof", host="localhost", port=32768, password="pprof")
 
-get_experiments(connection = con)
-lapply(X = get_experiments(con), FUN = plot_experiment, connection = con)
+exps = get_experiments(connection = con)
+lapply(X = exps, FUN = plot_dyncov, connection = con)
+lapply(X = exps, FUN = plot_experiment, connection = con)
 
 dbDisconnect(con)
 dbUnloadDriver(drv)

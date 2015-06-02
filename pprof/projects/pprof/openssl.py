5ed170cf0d7733d4c124bf366ba9003477f99230
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-06-02 07:42:48 +0200

    libressl: reuse openssl_dir

--- pprof/projects/pprof/openssl.py
+++ pprof/projects/pprof/openssl.py
@@ -49,15 +49,17 @@ class LibreSSL(PprofGroup):
         from plumbum.cmd import make, ln
 
         openssl_dir = path.join(self.builddir, self.src_dir)
+
+    def run_tests(self, experiment):
+        from plumbum.cmd import find, make
+        from pprof.project import wrap
+
+        openssl_dir = path.join(self.builddir, self.src_dir)
         with local.cwd(openssl_dir):
             """ This could double the number of runs in the database
             because we need to run the new binaries before replacing them
             """
             make("-j", config["jobs"], "check")
-
-    def run_tests(self, experiment):
-        from plumbum.cmd import find, make
-        from pprof.project import wrap
 
         with local.cwd(path.join(self.src_dir, "tests", ".libs")):
             files = find(".", "-type", "f", "-executable")

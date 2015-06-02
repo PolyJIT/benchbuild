e661c2abe9ae1b661673103db3885669da31d1ab
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-06-02 07:45:15 +0200

    lnt: don't try to force virtualenv
    Former-commit-id: d3728548d1496474d6411b12f8fd9d5c315751bd
    Former-commit-id: 75e291e488dc8d7e76103a6fb2cc5830db1e5d64 [formerly ed645a7d30ed9e238ade29997d7e93ca7943e87b] [formerly 9b5bb7e0ebadef79a4329ec9f317c37d56e4e55b [formerly f9a33f5f9871719d882ce263e4bb9fb9c8f0fc9f]]
    Former-commit-id: f32c4f424164abf023bbe3911cd2d9f62a5e2c67 [formerly 251e3dd64bd789c21d076812d5a9ac29bc2f73ef]
    Former-commit-id: 8d08a65eec9905e4a49c6cd130b3cff8f843e970

--- pprof/projects/lnt/lnt.py
+++ pprof/projects/lnt/lnt.py
@@ -28,9 +28,6 @@ class LNTGroup(Project):
             Git(self.src_uri, self.src_dir)
             Git(self.test_suite_uri, self.test_suite_dir)
 
-        from plumbum.cmd import pip
-        pip("install", "--user", "virtualenv")
-
         from plumbum.cmd import virtualenv
         with local.cwd(self.builddir):
             virtualenv("local")

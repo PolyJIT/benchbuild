1f12b834c43b4adfbda8450c90b05848208c4ed7
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-06-03 14:06:49 +0200

    project: update fetch_time_output to work on a list of lines

--- pprof/project.py
+++ pprof/project.py
@@ -306,14 +306,14 @@ def fetch_time_output(marker, format_s, ins):
     :format_s:
         The format string used to parse the timings
     :ins:
-        The stream we look for timings in
+        A list of lines we look for the output.
 
     :returns:
         A list of timing tuples
     """
     from parse import parse
 
-    timings = filter(lambda x: marker in x, list(ins))
+    timings = filter(lambda x: marker in x, ins)
     res = [ parse(format_s, t) for t in timings ]
     return filter(None, res)
 

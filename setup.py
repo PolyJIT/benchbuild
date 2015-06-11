dea720a1ce7a22e0985e962267541fa7bde6ba66
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-06-12 01:02:19 +0200

    Bump version to 0.9.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 setup(name='pprof',
-      version='0.9.4',
+      version='0.9.5',
       packages= find_packages(),
       setup_requires=['parse', 'plumbum', 'cloud', 'psycopg2', 'virtualenv', 'regex'],
       author = "Andreas Simbuerger",

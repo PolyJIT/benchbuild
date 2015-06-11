fe5dcfe6ddfe9df01d03483e88da869cf9d0d39b
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-06-11 15:26:19 +0200

    chimaira: make run_on_chimaira a little bit more generic
    Former-commit-id: 3a1d59500edd394460a9275adb6c1b725a295338
    Former-commit-id: fe5761ee12436e3253ef3e195cfa25422143f264 [formerly 53448cbc25484e0fad5f3b192af4b6d933785806] [formerly e3098f21a70ac4445f124d11491e6f8e085a056f [formerly 0d2154c37065c1f26375529a8a3a30825ca2c06f]]
    Former-commit-id: 4d1d84bea6b63c0bf3c083c61073f9ce3fde0da9 [formerly fcf9abe7e899da5b04f6e09c9f66ac18233ef94e]
    Former-commit-id: eb4f426619c13ed02f232ac561d08a6359f91b84

--- run_on_chimaira.sh
+++ run_on_chimaira.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-INSTALL_LLVM_TO=/scratch/simbuerg/pprof/install
-BUILD_LLVM_IN=/scratch/simbuerg/pprof
+INSTALL_LLVM_TO=/scratch/${USER}/pprof/install
+BUILD_LLVM_IN=/scratch/${USER}/pprof
 PAPI_DIR=${HOME}/opt/papi
-export PPROF_OBJ_DIR=/scratch/simbuerg/pprof/obj/$(date -Ihours)
+export PPROF_OBJ_DIR=/scratch/${USER}/pprof/obj/$(date -Ihours)
 
 # Adjust these, if necessary
 export PPROF_DB_PASS=pprof
 export PPROF_USE_DATABASE=1
 export PPROF_DB_PORT=32769
 export PPROF_DB_HOST=debussy
-export PPROF_TMP_DIR=/scratch/simbuerg/pprof/src
-export PPROF_TESTINPUTS=/home/simbuerg/src/polyjit/pprof-test-data
-export PPROF_CLUSTER_NODEDIR=/local/hdd/simbuerg
+export PPROF_TMP_DIR=/scratch/${USER}/pprof/src
+export PPROF_TESTINPUTS=${HOME}/src/polyjit/pprof-test-data
+export PPROF_CLUSTER_NODEDIR=/local/hdd/${USER}
 export PPROF_LIKWID_DIR=/usr
 export LD_LIBRARY_PATH=$PAPI_DIR/lib:$LD_LIBRARY_PATH
 

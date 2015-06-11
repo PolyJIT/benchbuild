aeaf3029e0ce814a05a3ffb5b58e94700b55fee4
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-06-11 14:29:01 +0200

    utils/run: add handle_stdin
    helper function that redirects stdin from wrapped binaries into the
    plumbum command, when needed.
    
    
    Former-commit-id: 86e902e186167434dedc7f4084e76893ff5c53d4
    Former-commit-id: 97e44e0f8fcf945c279832a90b172b88b58d1498 [formerly 8de9a193eb178b58ad8f75833e64127e54b18b2e] [formerly d482ef87d45f3e6c298e25b4eaded35a635f50bd [formerly 9fc2889759924e9d2de794b503be4437262f762e]]
    Former-commit-id: e2e68a27b639c29b6013c2abe7ef6b1dbc182088 [formerly 90ef92d647f66aa10005208b3fa080e1d887a10b]
    Former-commit-id: 5e665a3b32345dbb3d8938dc4b62b8454aa77c44

--- pprof/utils/run.py
+++ pprof/utils/run.py
@@ -3,6 +3,24 @@ Experiment helpers
 """
 
 
+def handle_stdin(cmd, kwargs):
+    """
+    Handle stdin for wrapped runtime executors.
+    :return:
+        A new plumbum command that deals with stdin redirection, if needed.
+    """
+    assert isinstance(kwargs, dict)
+    import sys
+
+    has_stdin = kwargs.get("has_stdin", False)
+    if has_stdin:
+        run_cmd = (cmd < sys.stdin)
+    else:
+        run_cmd = cmd
+
+    return run_cmd
+
+
 def fetch_time_output(marker, format_s, ins):
     """
     Fetch the output /usr/bin/time from a

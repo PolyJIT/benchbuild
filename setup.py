df96bdbcb70794afcb9f7465eef318bbe6f23333
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-06-13 10:20:40 +0200

    setup: setup_requires -> install_requires

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup, find_packages
 setup(name='pprof',
       version='0.9.5',
-      packages= find_packages(),
-      setup_requires=['parse', 'plumbum', 'cloud', 'psycopg2', 'virtualenv', 'regex'],
-      author = "Andreas Simbuerger",
-      author_email = "simbuerg@fim.uni-passau.de",
-      description = "This is the experiment driver for the pprof study",
-      license = "MIT",
+      packages=find_packages(),
+      install_requires=["cloud==2.8.5", "plumbum==1.4.2",
+                        "regex==2015.5.28", "wheel==0.24.0", "parse==1.6.6"],
+      author="Andreas Simbuerger",
+      author_email="simbuerg@fim.uni-passau.de",
+      description="This is the experiment driver for the pprof study",
+      license="MIT",
       entry_points={
-          'console_scripts': [ 'pprof=pprof.driver:main' ]
-          }
-      )
+          'console_scripts': ['pprof=pprof.driver:main']
+      }
+)

dafa9e2af3af569f5a9f653f7ddd234ff44d4e22
cib <cib123@googlemail.com>
2015-06-24 16:03:22 +0200

    Added EWPTCompilestatsExperiment.

---
+++ pprof/experiments/compilestats_ewpt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+The 'compilestats_ewpt' Experiment.
+
+Gathers compilation statistics for compiling the project with the
+EWPT alias analysis enabled.
+
+"""
+
+from pprof.experiments.compilestats import CompilestatsExperiment
+
+class EWPTCompilestatsExperiment(CompilestatsExperiment):
+    def extra_cflags(self):
+        return ["-O3", "-Xclang", "-load", "-Xclang", "LLVMPolyJIT.so", "-mllvm", "-polly"]

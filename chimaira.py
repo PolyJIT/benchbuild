574496c6236f01a8be906205a45fc5b790ddcb9c
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-07-03 13:01:17 +0200

    chimaira: remove unused flags

--- chimaira.py
+++ chimaira.py
@@ -115,8 +115,7 @@ def prepare_slurm_script(experiment, project, experiment_id):
                               "-B", config["nodedir"], "-I", config["isl"],
                               "-L", config["likwid"], "-P", config["papi"]])
     commands.append(pprof["run", "-P", project, "-E", experiment, "-B",
-                          config["nodedir"], "-c", "-x", "-L",
-                          config["llvm"]])
+                          config["nodedir"], "-L", config["llvm"]])
     # commands.append(
     #    cp["-ar", node_results, os.path.join(config["resultsdir"],
     #                                          experiment)])

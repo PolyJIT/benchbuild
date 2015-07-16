3f5aa0018978d219258060f6fd4860c9cf51533c
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-07-17 00:04:15 +0200

    project: update documentation to google's style.

--- pprof/project.py
+++ pprof/project.py
@@ -39,6 +39,16 @@ class Project(object):
     """
 
     def __init__(self, exp, name, domain, group=None):
+        """
+        Setup a new project.
+
+        Args:
+            exp: The experiment this project belongs to.
+            name: The name of this project.
+            domain: The domain this project belongs to, e.g. 'Scientific'
+            group: The group this project belongs to. This is useful for
+                finding group specific test input files.
+        """
         self.experiment = exp
         self.name = name
         self.domain = domain
@@ -58,14 +68,32 @@ class Project(object):
         persist_project(self)
 
     def setup_derived_filenames(self):
+        """ Construct all derived file names. """
         self.run_f = path.join(self.builddir, self.name)
         self.bin_f = self.run_f + PROJECT_BIN_F_EXT
 
     def run_tests(self, experiment):
+        """
+        Run the tests of this project.
+
+        Clients override this method to provide customized run-time tests.
+
+        Args:
+            experiment: The experiment we run this project under
+        """
         exp = wrap(self.run_f, experiment)
         exp()
 
     def run(self, experiment):
+        """
+        Run the tests of this project.
+
+        This method initializes the default environment and takes care of
+        cleaning up the mess we made, after a successfull run.
+
+        Args:
+            experiment: The experiment we run this project under
+        """
         with local.cwd(self.builddir):
             with local.env(PPROF_USE_DATABASE=1,
                            PPROF_DB_RUN_GROUP=self.run_uuid,
@@ -76,6 +104,7 @@ class Project(object):
                 self.clean()
 
     def clean(self):
+        """ Clean the project build directory. """
         if path.exists(self.builddir) and listdir(self.builddir) == []:
             rmdir(self.builddir)
         elif path.exists(self.builddir) and listdir(self.builddir) != []:
@@ -95,15 +124,23 @@ class Project(object):
         """
         Set a function as compiler extension.
 
-        :func:
-            The compiler extension function. Minimum signature required:
-            f(cc, **kwargs), where cc is the original compiler command.
+        Args:
+            func: The compiler extension function. The minimal signature that
+                is required is ::
+                    f(cc, **kwargs)
+                where cc is the original compiler command.
 
         """
         self._compiler_extension = func
 
     @property
-    def run_uuid(self):
+    def run_uuid(self, create_new=False):
+        """
+        Get the UUID that groups all tests for one project run.
+
+        Args:
+            create_new: Create a fresh UUID (Default: False)
+        """
         from os import getenv
         from uuid import uuid4
         try:
@@ -114,24 +151,28 @@ class Project(object):
         return self._run_uuid
 
     def prepare(self):
+        """ Prepare the build diretory. """
         if not path.exists(self.builddir):
             mkdir(self.builddir)
 
     @abstractmethod
     def download(self):
+        """ Download the input source for this project. """
         pass
 
     @abstractmethod
     def configure(self):
+        """ Configure the project. """
         pass
 
     @abstractmethod
     def build(self):
+        """ Build the project. """
         pass
 
 
 def wrap(name, runner):
-    """Wrap the binary :name: with the function :runner:.
+    """ Wrap the binary :name: with the function :runner:.
 
     This module generates a python tool that replaces :name:
     The function in runner only accepts the replaced binaries
@@ -139,10 +180,12 @@ def wrap(name, runner):
     perform the serialization, make sure :runner: can be serialized
     with it and you're fine.
 
-    :name: Binary we want to wrap
-    :runner: Function that should run instead of :name:
-    :returns: plumbum command, readty to launch.
+    Args:
+        name: Binary we want to wrap
+        runner: Function that should run instead of :name:
 
+    Returns:
+        A plumbum command, ready to launch.
     """
     from plumbum import local
     from plumbum.cmd import mv, chmod
@@ -203,16 +246,19 @@ if path.exists("{blobf}"):
 
 
 def wrap_dynamic(name, runner):
-    """Wrap the binary :name: with the function :runner:.
+    """
+    Wrap the binary :name with the function :runner.
 
     This module generates a python tool :name: that can replace
     a yet unspecified binary.
     It behaves similar to the :wrap: function. However, the first
     argument is the actual binary name.
 
-    :name: name of the python module
-    :runner: Function that should run the real binary
-    :returns: plumbum command, readty to launch.
+    Args:
+        name: name of the python module
+        runner: Function that should run the real binary
+
+    Returns: plumbum command, readty to launch.
 
     """
     from plumbum import local

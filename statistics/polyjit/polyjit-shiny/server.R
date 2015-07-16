9ebfd0b27be5d2761eed8616b58437dbd186a67a
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-07-16 13:32:14 +0200

    statistics: migrate data functions to own R script
    Former-commit-id: f3af096a372e0ca18e2637e58562482844eed288
    Former-commit-id: 30ab1189e8faf542b44d8249a1ce43709b70097a [formerly e991bf72f736c319e10aee2de8890e97ad0608ee] [formerly 1fc8314aa65cd0ee1784e66655e9b30e0888c91c [formerly f892c856ccb2dba8a38a17cdbc862a81555698e7]]
    Former-commit-id: 07563b246a75566f1779343fc4fa12d9286b752e [formerly 7339b9bb1733791ce1d9bcb0cf62a02a140c86bf]
    Former-commit-id: 9384b7414f49bb221433f9c9e8acaf493122e6b7

--- statistics/polyjit/polyjit-shiny/server.R
+++ statistics/polyjit/polyjit-shiny/server.R
@@ -2,10 +2,9 @@ library(shiny)
 library(reshape)
 library(ggplot2)
 if (!require("RPostgres")) install.packages("RPostgres")
-library(polyjit)
+library(RPostgres)
 if (!require("DT")) install.packages('DT')
 
-source("./shiny-data.R")
 
 con <- dbConnect(RPostgres::Postgres(),
                  dbname="pprof",
@@ -14,110 +13,8 @@ con <- dbConnect(RPostgres::Postgres(),
                  port=32768,
                  password="pprof")
 
-options(repr.plot.family = 'mono', repr.plot.width = 8, repr.plot.height = 6, warn = -1)
-mytheme <- theme(plot.title = element_text(family="Fantasque Mono", size = 10))
-
-getSelections <- function(name, exps) {
-  exps.filtered <- exps[exps$experiment_name == name, ]
-  newNames <- paste0(exps.filtered[,"experiment_name"],
-                     rep(" @ ", nrow(exps.filtered)),
-                     exps.filtered[,"completed"])
-  groups <- exps.filtered[, "experiment_group"]
-  names(groups) <- newNames
-  return(groups)
-}
-
-timingPlot <- function(id, exps) {
-  exp.id <- id
-  exp.name <- exps[exps$experiment_group == exp.id, "experiment_name"]
-  exp.date <- exps[exps$experiment_group == exp.id, "completed"]
-
-  d <- timingPlotData(exp.id, exp.name, con)
-  p <- ggplot(data = d, aes(x = project_name))
-  p <- p + theme(axis.ticks.x = element_blank(),
-                 axis.title.x = element_blank(),
-                 axis.text.x  = element_blank(),
-                 axis.title.y = element_blank(),
-                 legend.position = "none",
-                 plot.title = element_text(size = 10))
-  p <- p + mytheme
-  p <- p + ggtitle(sprintf("Runtime breakdown '%s' @ '%s'\n(%s)", exp.name, exp.date, exp.id))
-  if (is.element("raw.time.real_s", names(d))) {
-    p <- p + geom_point(aes(y = raw.time.real_s), size = 1.5, colour = "red")
-  }
-  if (is.element("raw.time.user_s", names(d))) {
-    p <- p + geom_point(aes(y = raw.time.user_s), size = 1.5, colour = "blue")
-  }
-
-  if (is.element("papi.time.real_s", names(d))) {
-    p <- p + geom_point(aes(y = papi.time.real_s), size = 1.5, colour = "red")
-  }
-  if (is.element("papi.time.user_s", names(d))) {
-    p <- p + geom_point(aes(y = papi.time.user_s), size = 1.5, colour = "blue")
-  }
-
-  if (is.element("time.real_s", names(d))) {
-    p <- p + geom_point(aes(y = time.real_s), size = 1.5, colour = "red")
-  }
-  if (is.element("time.user_s", names(d))) {
-    p <- p + geom_point(aes(y = time.user_s), size = 1.5, colour = "blue")
-  }
-  return(p)
-}
-
-papiPlot <- function(id, exps) {
-  exp.id <- id
-  exp.name <- exps[exps$experiment_group == exp.id, "experiment_name"]
-  exp.date <- exps[exps$experiment_group == exp.id, "completed"]
-
-  d <- papiPlotData(exp.id, con)
-  p <- ggplot(data = d, aes(x = project_name))
-  p <- p + theme(axis.ticks.x = element_blank(),
-                 axis.text.x = element_blank(),
-                 legend.position = "right",
-                 plot.title = element_text(size = 10))
-  p <- p + mytheme
-  p <- p + ggtitle(sprintf(" Dynamic SCoP coverage ordered by domain '%s'\n'%s' (%s)", exp.name, exp.date, exp.id))
-  p <- p + labs(y = "Dynamic SCoP coverage [%]", x = "Project")
-  p <- p + geom_point(aes(y = value, color = domain), size = 1.5)
-  return(p)
-}
-
-papiBoxplot <- function(id, exps) {
-  exp.id <- id
-  exp.name <- exps[exps$experiment_group == exp.id, "experiment_name"]
-  exp.date <- exps[exps$experiment_group == exp.id, "completed"]
-
-  d <- papiPlotData(exp.id, con)
-  cov0 <- d[d$value > 1,]
-
-  p <- qplot(data = cov0, x = domain, y = value)
-  p <- p + ggtitle(sprintf(" Runtime breakdown '%s' @ '%s' (%s)", exp.name, exp.date, exp.id))
-  p <- p + theme(plot.title = element_text(size = 10))
-  p <- p + mytheme
-  p <- p + geom_boxplot(outlier.size = 1, fill = "white")
-  return(p)
-}
-
-polyjitPlot <- function(id, metric, aggregation, exps) {
-  exp.name <- exps[exps$experiment_group == id, "experiment_name"]
-  exp.date <- exps[exps$experiment_group == id, "completed"]
-
-  lw.total <- likwid.total(con, id, aggregation, metric)
-  lw.runtime <- likwid.runtime(con, id, aggregation, metric)
-  lw.overhead <- likwid.overhead(con, id, aggregation, metric)
-
-  lw <- rbind(lw.runtime, lw.overhead)
-
-  p <- ggplot(data = lw, aes(x = project_name, y = value, fill = variable))
-  p <- p + ggtitle(sprintf("Overhead vs. Runtime of jitte'd functions '%s' @ '%s'\nMetric '%s' (%s)",
-                           exp.name, exp.date, metric, id))
-  p <- p + labs(y = metric, x = "Project")
-  p <- p + geom_bar(position = "dodge", stat= "identity")
-  p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 8))
-  p <- p + mytheme
-  return(p)
-}
+source("./polyjit.R")
+source("./shiny-data.R")
 
 shinyServer(function(input, output, session) {
   experiments <- reactive({ get_experiments(con) })
@@ -153,20 +50,29 @@ shinyServer(function(input, output, session) {
     timingPlotData(input$rawExperiments, exp.name, con)
   })
 
-  output$timing <- renderPlot({ timingPlot(input$rawExperiments, experiments()) })
+  output$timing <- renderPlot({ timingPlot(input$rawExperiments, experiments(), con) })
 
-  output$papi <- renderPlot({ papiPlot(input$papiExperiments, experiments()) })
-  output$papiTable <- renderDataTable({ papiPlotData(input$papiExperiments, con) })
-  output$papiBoxplot <- renderPlot({ papiBoxplot(input$papiExperiments, experiments()) })
-  output$papiStd <- renderPlot({ papiPlot(input$papiStdExperiments, experiments()) })
-  output$papiStdBoxplot <- renderPlot({ papiBoxplot(input$papiStdExperiments, experiments()) })
+  output$papi <- renderPlot({ papiPlot(input$papiExperiments, experiments(), con) })
+  output$papiTable <- DT::renderDataTable({ papiPlotData(input$papiExperiments, con) })
+  output$papiBoxplot <- renderPlot({ papiBoxplot(input$papiExperiments, experiments(), con) })
+  output$papiStd <- renderPlot({ papiPlot(input$papiStdExperiments, experiments(), con) })
+  output$papiStdBoxplot <- renderPlot({ papiBoxplot(input$papiStdExperiments, experiments(), con) })
   output$polyjit <- renderPlot({ polyjitPlot(input$polyjitExperiments,
                                              input$polyjitMetrics,
                                              input$polyjitAggregation,
-                                             experiments())
+                                             experiments(), con)
+  output$polyjitTable <- DT::renderDataTable({ polyjitData(input$polyjitExperiments,
+                                                           input$polyjitMetrics,
+                                                           input$polyjitAggregation,
+                                                           experiments(), con) },
+                                             style = 'bootstrap',
+                                             class = 'table-condensed'
+                                             )
 
   # Log table.
   output$polyjitLog <- DT::renderDataTable({ runlog(con, input$polyjitExperiments) },
+                                           style = 'bootstrap',
+                                           class = 'table-condensed',
                                            options = list(
                                              pageLength = 50,
                                              rownames = FALSE,
@@ -183,7 +89,7 @@ shinyServer(function(input, output, session) {
                                                  "  }",
                                                  "}")
                                                ),
-                                               list(targets = 6, render = JS(
+                                               list(targets = 5, render = JS(
                                                  "function(data, type, row, meta) {",
                                                  "return type === 'display' && data.length > 80 ?",
                                                  "'<span title=\"' + data + '\">' + data.substr(0, 80) + '...</span>' : data;",
--- statistics/polyjit/polyjit-shiny/shiny-data.R
+++ statistics/polyjit/polyjit-shiny/shiny-data.R
@@ -1,11 +1,13 @@
-library(polyjit)
 library(RPostgres)
+
+options(repr.plot.family = 'mono', repr.plot.width = 8, repr.plot.height = 6, warn = -1)
+mytheme <- theme(plot.title = element_text(family="Fantasque Mono", size = 10))
 
 timingPlotData <- function(id, name, con) {
   d <- get_raw_runtime(name, id, con)
   d.cast <- subset(d, value < 100)
   d.cast <- cast(data = d.cast, project_name ~ name, fun.aggregate = sum)
-  
+
   return(d.cast)
 }
 
@@ -14,6 +16,89 @@ papiPlotData <- function(id, con) {
   cov.dom <- subset(cov.dom, value > 0)
   return(cov.dom)
 }
+
+getSelections <- function(name, exps) {
+  exps.filtered <- exps[exps$experiment_name == name, ]
+  newNames <- paste0(exps.filtered[,"experiment_name"],
+                     rep(" @ ", nrow(exps.filtered)),
+                     exps.filtered[,"completed"])
+  groups <- exps.filtered[, "experiment_group"]
+  names(groups) <- newNames
+  return(groups)
+}
+
+timingPlot <- function(id, exps, con) {
+  exp.id <- id
+  exp.name <- exps[exps$experiment_group == exp.id, "experiment_name"]
+  exp.date <- exps[exps$experiment_group == exp.id, "completed"]
+
+  d <- timingPlotData(exp.id, exp.name, con)
+  p <- ggplot(data = d, aes(x = project_name))
+  p <- p + theme(axis.ticks.x = element_blank(),
+                 axis.title.x = element_blank(),
+                 axis.text.x  = element_blank(),
+                 axis.title.y = element_blank(),
+                 legend.position = "none",
+                 plot.title = element_text(size = 10))
+  p <- p + mytheme
+  p <- p + ggtitle(sprintf("Runtime breakdown '%s' @ '%s'\n(%s)", exp.name, exp.date, exp.id))
+  if (is.element("raw.time.real_s", names(d))) {
+    p <- p + geom_point(aes(y = raw.time.real_s), size = 1.5, colour = "red")
+  }
+  if (is.element("raw.time.user_s", names(d))) {
+    p <- p + geom_point(aes(y = raw.time.user_s), size = 1.5, colour = "blue")
+  }
+
+  if (is.element("papi.time.real_s", names(d))) {
+    p <- p + geom_point(aes(y = papi.time.real_s), size = 1.5, colour = "red")
+  }
+  if (is.element("papi.time.user_s", names(d))) {
+    p <- p + geom_point(aes(y = papi.time.user_s), size = 1.5, colour = "blue")
+  }
+
+  if (is.element("time.real_s", names(d))) {
+    p <- p + geom_point(aes(y = time.real_s), size = 1.5, colour = "red")
+  }
+  if (is.element("time.user_s", names(d))) {
+    p <- p + geom_point(aes(y = time.user_s), size = 1.5, colour = "blue")
+  }
+  return(p)
+}
+
+papiPlot <- function(id, exps, con) {
+  exp.id <- id
+  exp.name <- exps[exps$experiment_group == exp.id, "experiment_name"]
+  exp.date <- exps[exps$experiment_group == exp.id, "completed"]
+
+  d <- papiPlotData(exp.id, con)
+  p <- ggplot(data = d, aes(x = project_name))
+  p <- p + theme(axis.ticks.x = element_blank(),
+                 axis.text.x = element_blank(),
+                 legend.position = "right",
+                 plot.title = element_text(size = 10))
+  p <- p + mytheme
+  p <- p + ggtitle(sprintf(" Dynamic SCoP coverage ordered by domain '%s'\n'%s' (%s)", exp.name, exp.date, exp.id))
+  p <- p + labs(y = "Dynamic SCoP coverage [%]", x = "Project")
+  p <- p + geom_point(aes(y = value, color = domain), size = 1.5)
+  return(p)
+}
+
+papiBoxplot <- function(id, exps, con) {
+  exp.id <- id
+  exp.name <- exps[exps$experiment_group == exp.id, "experiment_name"]
+  exp.date <- exps[exps$experiment_group == exp.id, "completed"]
+
+  d <- papiPlotData(exp.id, con)
+  cov0 <- d[d$value > 1,]
+
+  p <- qplot(data = cov0, x = domain, y = value)
+  p <- p + ggtitle(sprintf(" Runtime breakdown '%s' @ '%s' (%s)", exp.name, exp.date, exp.id))
+  p <- p + theme(plot.title = element_text(size = 10))
+  p <- p + mytheme
+  p <- p + geom_boxplot(outlier.size = 1, fill = "white")
+  return(p)
+}
+
 polyjitData <- function(id, metric, aggregation, exps, con) {
   exp.name <- exps[exps$experiment_group == id, "experiment_name"]
   exp.date <- exps[exps$experiment_group == id, "completed"]
@@ -24,4 +109,24 @@ polyjitData <- function(id, metric, aggregation, exps, con) {
 
   lw <- lw.overhead
   return(lw)
+}
+
+polyjitPlot <- function(id, metric, aggregation, exps, con) {
+  exp.name <- exps[exps$experiment_group == id, "experiment_name"]
+  exp.date <- exps[exps$experiment_group == id, "completed"]
+
+  lw.total <- likwid.total(con, id, aggregation, metric)
+  lw.runtime <- likwid.runtime(con, id, aggregation, metric)
+  lw.overhead <- likwid.overhead(con, id, aggregation, metric)
+
+  lw <- rbind(lw.runtime, lw.overhead)
+
+  p <- ggplot(data = lw, aes(x = project_name, y = value, fill = variable))
+  p <- p + ggtitle(sprintf("Overhead vs. Runtime of jitte'd functions '%s' @ '%s'\nMetric '%s' (%s)",
+                           exp.name, exp.date, metric, id))
+  p <- p + labs(y = metric, x = "Project")
+  p <- p + geom_bar(position = "dodge", stat= "identity")
+  p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 8))
+  p <- p + mytheme
+  return(p)
 }

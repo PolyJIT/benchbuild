5c3c2fda9824ab88bb0b6dc2bb0c54bdeb58e8e5
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-07-17 15:22:53 +0200

    polyjit: migrate run_with_likwid out of the experiment class
    Use the static_var decorator to provide the correct configuration to the method
    before using the cloudpickle.
    
    This makes the code a little bit cleaner and explicitly states what we need from
    our environment, without resorting to extra function parameters. In this case it
    is actually cleaner, because we don't have to push them down the pipeline until
    the project binary gets wrapped.
    
    
    Former-commit-id: da43a6e81a70d02a38597fe307b1c07c8c33e5aa
    Former-commit-id: 0e42d9ba05c149cd2262bec8007199481bd95158 [formerly 4e127f8034e6a0acd0658c0326ff4525ba0260dd] [formerly 57e0889d008bd06faa45b916000a3554d1a10335 [formerly 03d165606b4c9007def98f7e037525a56b74d4f0]]
    Former-commit-id: 2d8d773ead9dd35588ed7d49098c99f9c093dc06 [formerly 8db8910d1128410cd682d6050c8cde4f4ea18516]
    Former-commit-id: deaece1f0b3346c9e493b48b6faed24f30b49c2d

--- pprof/experiments/polyjit.py
+++ pprof/experiments/polyjit.py
@@ -7,12 +7,77 @@ This experiment uses likwid to measure the performance of all binaries
 when running with polyjit support enabled.
 """
 from pprof.experiments.compilestats import get_compilestats
-from pprof.experiment import step, substep, RuntimeExperiment
+from pprof.experiment import step, substep, static_var, RuntimeExperiment
+from pprof.project import Project
+from pprof.experiment import Experiment
 from pprof.settings import config
 from pprof.utils.schema import CompileStat
 
 from plumbum import local
 from os import path
+
+
+@static_var("config", None)
+@static_var("experiment", None)
+@static_var("project", None)
+@static_var("jobs", 0)
+def run_with_likwid(run_f, args, **kwargs):
+    """
+    Run the given file wrapped by likwid.
+
+    Args:
+        run_f: The file we want to execute.
+        args: List of arguments that should be passed to the wrapped binary.
+        **kwargs: Dictionary with our keyword args. We support the following
+            entries:
+
+            project_name: The real name of our project. This might not
+                be the same as the configured project name, if we got wrapped
+                with ::pprof.project.wrap_dynamic
+            has_stdin: Signals whether we should take care of stdin.
+    """
+    from pprof.utils import run as r
+    from pprof.utils.db import persist_likwid
+    from pprof.likwid import get_likwid_perfctr
+    from pprof.settings import config
+    from plumbum.cmd import rm
+
+    p = run_with_likwid.project
+    e = run_with_likwid.experiment
+    c = run_with_likwid.config
+    jobs = run_with_likwid.jobs
+
+    config.update(c)
+
+    assert p is not None, "run_with_likwid.project attribute is None."
+    assert e is not None, "run_with_likwid.experiment attribute is None."
+    assert c is not None, "run_with_likwid.config attribute is None."
+    assert isinstance(p, Project), "Wrong type: %r Want: Project" % p
+    assert isinstance(e, Experiment), "Wrong type: %r Want: Experiment" % e
+    assert isinstance(c, dict), "Wrong type: %r Want: Experiment" % e
+
+    project_name = kwargs.get("project_name", p.name)
+    likwid_f = project_name + ".txt"
+
+    for group in ["CLOCK"]:
+        likwid_path = path.join(c["likwiddir"], "bin")
+        likwid_perfctr = local[
+            path.join(likwid_path, "likwid-perfctr")]
+        run_cmd = \
+            likwid_perfctr["-O", "-o", likwid_f, "-m",
+                           "-C", "0-{:d}".format(jobs),
+                           "-g", group, run_f]
+        run_cmd = r.handle_stdin(run_cmd[args], kwargs)
+
+        run, session, retcode, stdout, stderr = \
+            r.guarded_exec(run_cmd, project_name, e.name, p.run_uuid)
+
+        likwid_measurement = get_likwid_perfctr(likwid_f)
+        """ Use the project_name from the binary, because we
+            might encounter dynamically generated projects.
+        """
+        persist_likwid(run, session, likwid_measurement)
+        rm("-f", likwid_f)
 
 
 class PolyJIT(RuntimeExperiment):
@@ -21,116 +86,89 @@ class PolyJIT(RuntimeExperiment):
 
     def run_step_compilestats(self, p):
         """ Compile the project and track the compilestats. """
-        with step("Track Compilestats @ -O3"):
-            p.clean()
-            p.prepare()
-            p.download()
-            with substep("Configure Project"):
-                def track_compilestats(cc, **kwargs):
-                    from pprof.utils import run as r
-                    from pprof.utils.db import persist_compilestats
-                    from pprof.utils.run import handle_stdin
+        p.clean()
+        p.prepare()
+        p.download()
+        with substep("Configure Project"):
+            def track_compilestats(cc, **kwargs):
+                from pprof.utils import run as r
+                from pprof.utils.db import persist_compilestats
+                from pprof.utils.run import handle_stdin
 
-                    new_cc = handle_stdin(cc["-mllvm", "-stats"], kwargs)
+                new_cc = handle_stdin(cc["-mllvm", "-stats"], kwargs)
 
-                    run, session, retcode, stdout, stderr = \
-                        r.guarded_exec(new_cc, p.name, self.name,
-                                       p.run_uuid)
+                run, session, retcode, stdout, stderr = \
+                    r.guarded_exec(new_cc, p.name, self.name,
+                                   p.run_uuid)
 
-                    if retcode == 0:
-                        stats = []
-                        for stat in get_compilestats(stderr):
-                            c = CompileStat()
-                            c.name = stat["desc"].rstrip()
-                            c.component = stat["component"].rstrip()
-                            c.value = stat["value"]
-                            stats.append(c)
-                        persist_compilestats(run, session, stats)
+                if retcode == 0:
+                    stats = []
+                    for stat in get_compilestats(stderr):
+                        c = CompileStat()
+                        c.name = stat["desc"].rstrip()
+                        c.component = stat["component"].rstrip()
+                        c.value = stat["value"]
+                        stats.append(c)
+                    persist_compilestats(run, session, stats)
 
-                p.compiler_extension = track_compilestats
-                p.configure()
+            p.compiler_extension = track_compilestats
+            p.configure()
 
-            with substep("Build Project"):
-                p.build()
+        with substep("Build Project"):
+            p.build()
 
     def run_step_jit(self, p):
         """Run the experiment without likwid."""
-        with step("JIT, no instrumentation"):
-            p.clean()
-            p.prepare()
-            p.download()
-            with substep("Build"):
-                p.configure()
-                p.build()
-            with substep("Execute {}".format(p.name)):
-                def run_with_time(run_f, args, **kwargs):
-                    from pprof.utils import run as r
-                    from pprof.utils.db import persist_time
-                    from plumbum.cmd import time
+        p.clean()
+        p.prepare()
+        p.download()
+        with substep("Build"):
+            p.configure()
+            p.build()
+        with substep("Execute {}".format(p.name)):
+            def run_with_time(run_f, args, **kwargs):
+                from pprof.utils import run as r
+                from pprof.utils.db import persist_time
+                from plumbum.cmd import time
 
-                    project_name = kwargs.get("project_name", p.name)
-                    timing_tag = "PPROF-JIT: "
+                project_name = kwargs.get("project_name", p.name)
+                timing_tag = "PPROF-JIT: "
 
-                    run_cmd = time["-f", timing_tag + "%U-%S-%e", run_f]
-                    run_cmd = r.handle_stdin(run_cmd[args], kwargs)
+                run_cmd = time["-f", timing_tag + "%U-%S-%e", run_f]
+                run_cmd = r.handle_stdin(run_cmd[args], kwargs)
 
-                    run, session, retcode, stdout, stderr = \
-                        r.guarded_exec(run_cmd, project_name, self.name,
-                                       p.run_uuid)
-                    timings = r.fetch_time_output(
-                        timing_tag, timing_tag + "{:g}-{:g}-{:g}",
-                        stderr.split("\n"))
-                    if len(timings) == 0:
-                        return
+                run, session, retcode, stdout, stderr = \
+                    r.guarded_exec(run_cmd, project_name, self.name,
+                                   p.run_uuid)
+                timings = r.fetch_time_output(
+                    timing_tag, timing_tag + "{:g}-{:g}-{:g}",
+                    stderr.split("\n"))
+                if len(timings) == 0:
+                    return
 
-                    persist_time(run, session, timings)
-                p.run(run_with_time)
+                persist_time(run, session, timings)
+            p.run(run_with_time)
 
-    def run_step_likwid(self, p, jobs):
+    def run_step_likwid(self, p):
         """Run the experiment with likwid."""
         from pprof.settings import config
+        from uuid import uuid4
 
-        def run_with_likwid(run_f, args, **kwargs):
-            from pprof.utils import run as r
-            from pprof.utils.db import persist_likwid
-            from pprof.likwid import get_likwid_perfctr
-            from plumbum.cmd import rm
+        p.cflags = ["-DLIKWID_PERFMON"] + p.cflags
 
-            project_name = kwargs.get("project_name", p.name)
-            likwid_f = project_name + ".txt"
-
-            for group in ["CLOCK"]:
-                likwid_path = path.join(config["likwiddir"], "bin")
-                likwid_perfctr = local[
-                    path.join(likwid_path, "likwid-perfctr")]
-                with substep("{} cores & uuid {}".format(jobs+1, p.run_uuid)):
-                    run_cmd = \
-                        likwid_perfctr["-O", "-o", likwid_f, "-m",
-                                       "-C", "0-{:d}".format(jobs),
-                                       "-g", group, run_f]
-                    run_cmd = r.handle_stdin(run_cmd[args], kwargs)
-
-                    run, session, retcode, stdout, stderr = \
-                        r.guarded_exec(run_cmd, project_name, self.name,
-                                       p.run_uuid)
-
-                    likwid_measurement = get_likwid_perfctr(likwid_f)
-                    """ Use the project_name from the binary, because we
-                        might encounter dynamically generated projects.
-                    """
-                    persist_likwid(run, session, likwid_measurement)
-                    rm("-f", likwid_f)
-
-        with step("JIT, likwid"):
-            p.clean()
-            p.prepare()
-            p.download()
-            p.cflags = ["-DLIKWID_PERFMON"] + p.cflags
-
-            with substep("Build"):
+        for i in range(int(config["jobs"])):
+            with substep("{} cores & uuid {}".format(i+1, p.run_uuid)):
+                p.clean()
+                p.prepare()
+                p.download()
                 p.configure()
                 p.build()
-            with substep("Execute {}".format(p.name)):
+
+                p.run_uuid = uuid4()
+                run_with_likwid.config = config
+                run_with_likwid.experiment = self
+                run_with_likwid.project = p
+                run_with_likwid.jobs = i
                 p.run(run_with_likwid)
 
     def run_project(self, p):
@@ -154,10 +192,9 @@ class PolyJIT(RuntimeExperiment):
                     "-mllvm", "-polly-detect-keep-going",
                     "-mllvm", "-polli"]
         with local.env(PPROF_ENABLE=0):
-            from uuid import uuid4
-
-            for i in range(int(config["jobs"])):
-                p.run_uuid = uuid4()
-                self.run_step_likwid(p, i)
-            self.run_step_jit(p)
-            self.run_step_compilestats(p)
+            with step("JIT, likwid"):
+                self.run_step_likwid(p)
+            with step("JIT, no instrumentation"):
+                self.run_step_jit(p)
+            with step("Track Compilestats @ -O3"):
+                self.run_step_compilestats(p)

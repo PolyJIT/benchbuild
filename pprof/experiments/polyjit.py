3411f28788518636706382f2e48c316837c2054b
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-07-21 10:25:52 +0200

    polyjit: run without likwid over the complete job range
    Former-commit-id: 423bacb5df988e706a6cfce4c7a46864499fd3bf
    Former-commit-id: 8db5646961c3a61b4d653b8d8f849d04ba8a35f2 [formerly cacc900eb87be638e6546e2a6131efcf0bf43c39] [formerly e6156a7692760667e7bbebe1e327a41d485deaa5 [formerly 1dd1b71f3cb2f478d46ea8fc50c89264fa4feebf]]
    Former-commit-id: a724db28e401256da0e44a99c9087c6edadb394e [formerly 1619956f2536769741798d1e420111efc26ff54b]
    Former-commit-id: 3d1bb12e53c050243529d5d3dd55af85b639a4ac

--- pprof/experiments/polyjit.py
+++ pprof/experiments/polyjit.py
@@ -80,6 +80,61 @@ def run_with_likwid(run_f, args, **kwargs):
         rm("-f", likwid_f)
 
 
+@static_var("config", None)
+@static_var("experiment", None)
+@static_var("project", None)
+@static_var("jobs", 0)
+def run_with_time(run_f, args, **kwargs):
+    """
+    Run the given binary wrapped with time.
+
+    Args:
+        run_f: The file we want to execute.
+        args: List of arguments that should be passed to the wrapped binary.
+        **kwargs: Dictionary with our keyword args. We support the following
+            entries:
+
+            project_name: The real name of our project. This might not
+                be the same as the configured project name, if we got wrapped
+                with ::pprof.project.wrap_dynamic
+            has_stdin: Signals whether we should take care of stdin.
+    """
+    from pprof.utils import run as r
+    from pprof.utils.db import persist_time
+    from plumbum.cmd import time
+
+    p = run_with_time.project
+    e = run_with_time.experiment
+    c = run_with_time.config
+    jobs = run_with_time.jobs
+
+    config.update(c)
+
+    assert p is not None, "run_with_likwid.project attribute is None."
+    assert e is not None, "run_with_likwid.experiment attribute is None."
+    assert c is not None, "run_with_likwid.config attribute is None."
+    assert isinstance(p, Project), "Wrong type: %r Want: Project" % p
+    assert isinstance(e, Experiment), "Wrong type: %r Want: Experiment" % e
+    assert isinstance(c, dict), "Wrong type: %r Want: Experiment" % e
+
+    project_name = kwargs.get("project_name", p.name)
+    timing_tag = "PPROF-JIT: "
+
+    run_cmd = time["-f", timing_tag + "%U-%S-%e", run_f]
+    run_cmd = r.handle_stdin(run_cmd[args], kwargs)
+
+    with local.env(OMP_NUM_THREADS=str(jobs)):
+        run, session, retcode, stdout, stderr = \
+            r.guarded_exec(run_cmd, project_name, e.name, p.run_uuid)
+        timings = r.fetch_time_output(
+            timing_tag, timing_tag + "{:g}-{:g}-{:g}",
+            stderr.split("\n"))
+        if len(timings) == 0:
+            return
+
+    persist_time(run, session, timings)
+
+
 class PolyJIT(RuntimeExperiment):
 
     """The polyjit experiment."""
@@ -119,35 +174,23 @@ class PolyJIT(RuntimeExperiment):
 
     def run_step_jit(self, p):
         """Run the experiment without likwid."""
-        p.clean()
-        p.prepare()
-        p.download()
-        with substep("Build"):
-            p.configure()
-            p.build()
-        with substep("Execute {}".format(p.name)):
-            def run_with_time(run_f, args, **kwargs):
-                from pprof.utils import run as r
-                from pprof.utils.db import persist_time
-                from plumbum.cmd import time
+        from pprof.settings import config
+        from uuid import uuid4
 
-                project_name = kwargs.get("project_name", p.name)
-                timing_tag = "PPROF-JIT: "
+        for i in range(1, int(config["jobs"])):
+            with substep("{} cores & uuid {}".format(i+1, p.run_uuid)):
+                p.clean()
+                p.prepare()
+                p.download()
+                p.configure()
+                p.build()
 
-                run_cmd = time["-f", timing_tag + "%U-%S-%e", run_f]
-                run_cmd = r.handle_stdin(run_cmd[args], kwargs)
-
-                run, session, retcode, stdout, stderr = \
-                    r.guarded_exec(run_cmd, project_name, self.name,
-                                   p.run_uuid)
-                timings = r.fetch_time_output(
-                    timing_tag, timing_tag + "{:g}-{:g}-{:g}",
-                    stderr.split("\n"))
-                if len(timings) == 0:
-                    return
-
-                persist_time(run, session, timings)
-            p.run(run_with_time)
+                p.run_uuid = uuid4()
+                run_with_time.config = config
+                run_with_time.experiment = self
+                run_with_time.project = p
+                run_with_time.jobs = i
+                p.run(run_with_time)
 
     def run_step_likwid(self, p):
         """Run the experiment with likwid."""
@@ -156,7 +199,7 @@ class PolyJIT(RuntimeExperiment):
 
         p.cflags = ["-DLIKWID_PERFMON"] + p.cflags
 
-        for i in range(int(config["jobs"])):
+        for i in range(1, int(config["jobs"])):
             with substep("{} cores & uuid {}".format(i+1, p.run_uuid)):
                 p.clean()
                 p.prepare()

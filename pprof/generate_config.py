69e63ccbcb6b9ea9f10b4b8b17e83384cf1ae06c
cib <cib123@googlemail.com>
2015-07-29 14:06:55 +0200

    Ask to create the base build directory if it does not exist yet.
    Former-commit-id: fbaf102afc4dc927e383f2775ca93812f3b1c61f
    Former-commit-id: 5f0e4b6416997d91b338f8c07e64793f17a78628 [formerly 5ee533d71af8e4cac138f375911dcfea67d4ca80] [formerly e079dae8e3cd28fd317ea38b11c945bf4f61ce22 [formerly e2b823433f1cb9342e65b77788777212d36752eb]]
    Former-commit-id: 2921954abf6c121f16b7b7ee9eb321a1120d0af1 [formerly 93104ba9b69cb33d4f86f1c134ad7425ae9175ed]
    Former-commit-id: ae70bd901911ff59cedab386c3988fe35f966276

--- pprof/generate_config.py
+++ pprof/generate_config.py
@@ -1,5 +1,6 @@
 from plumbum import cli
 from pprof.driver import PollyProfiling
+from pprof.utils.user_interface import query_yes_no
 import sys
 import os.path
 
@@ -33,37 +34,3 @@ class PprofGenConfig(cli.Application):
                 outfile.write("\n\n")
 
         print("Configuration file has been written to " + self._outpath)
-
-
-# Taken from the following recipe: http://code.activestate.com/recipes/577058/
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")

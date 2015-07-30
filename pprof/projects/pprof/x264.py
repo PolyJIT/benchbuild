db92cab45ade9f611c2fb73d43a65f6c9eff62e0
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-07-30 13:45:14 +0200

    x264: simplify test notation
    Former-commit-id: 1b2203f019ed9de7a0ca2e1ff7b1ecb894a3d686
    Former-commit-id: 450739a3ed667baad48da0f0710de88c44c6cd7d [formerly fe955729a31c6b36b92a4ef8dcc8cc5353f1d94f] [formerly 2b1500c477c02a334dfd7239cc58e59e40e993c8 [formerly ff326ee1d5a724da9e219ea056222753f314c008]]
    Former-commit-id: ec75cb33f64c9e0a83ddb5ecfbc9c1a526e20d6e [formerly 4bc1b105251b4ca66d892e63099fecaa9f7cacc9]
    Former-commit-id: dbc4d7975b9c523afe5221fa47ae6d7abee03c68

--- pprof/projects/pprof/x264.py
+++ pprof/projects/pprof/x264.py
@@ -68,63 +68,19 @@ class X264(PprofGroup):
         x264_dir = path.join(self.builddir, self.src_dir)
         exp = wrap(path.join(x264_dir, "x264"), experiment)
 
+        tests = [
+            "--crf 30 -b1 -m1 -r1 --me dia --no-cabac --direct temporal --ssim --no-weightb",
+            "--crf 16 -b2 -m3 -r3 --me hex --no-8x8dct --direct spatial --no-dct-decimate -t0  --slice-max-mbs 50",
+            "--crf 26 -b4 -m5 -r2 --me hex --cqm jvt --nr 100 --psnr --no-mixed-refs --b-adapt 2 --slice-max-size 1500",
+            "--crf 18 -b3 -m9 -r5 --me umh -t1 -A all --b-pyramid normal --direct auto --no-fast-pskip --no-mbtree",
+            "--crf 22 -b3 -m7 -r4 --me esa -t2 -A all --psy-rd 1.0:1.0 --slices 4",
+            "--frames 50 --crf 24 -b3 -m10 -r3 --me tesa -t2",
+            "--frames 50 -q0 -m9 -r2 --me hex -Aall",
+            "--frames 50 -q0 -m2 -r1 --me hex --no-cabac",
+        ]
+
         for ifile in self.inputfiles:
             testfile = path.join(self.testdir, ifile)
-            exp(
-                testfile,
-                self.inputfiles[ifile],
-                "--threads", "1",
-                "-o", "/dev/null",
-                "--frames", "5",
-                "--crf", "30",
-                "-b1", "-m1", "-r1", "--me", "dia", "--no-cabac",
-                "--direct", "temporal", "--ssim", "--no-weightb")
-            exp(
-                testfile,
-                self.inputfiles[ifile],
-                "--threads", "1",
-                "-o", "/dev/null",
-                "--frames", "5",
-                "--crf", "16",
-                "-b2", "-m3", "-r3", "--me", "hex", "--no-8x8dct",
-                "--direct", "spatial", "--no-dct-decimate", "-t0",
-                "--slice-max-mbs", "50")
-            exp(
-                testfile,
-                self.inputfiles[ifile],
-                "--threads", "1",
-                "-o", "/dev/null",
-                "--frames", "5",
-                "--crf", "26",
-                "-b4", "-m5", "-r2", "--me", "hex", "--cqm", "jvt",
-                "--nr", "100", "--psnr", "--no-mixed-refs",
-                "--b-adapt", "2", "--slice-max-size", "1500")
-            exp(
-                testfile,
-                self.inputfiles[ifile],
-                "--threads", "1", "-o", "/dev/null", "--frames", "5",
-                "--crf", "18", "-b3", "-m9", "-r5", "--me", "umh",
-                "-t1", "-A", "all", "--b-pyramid", "normal",
-                "--direct", "auto", "--no-fast-pskip", "--no-mbtree")
-            exp(
-                testfile,
-                self.inputfiles[ifile],
-                "--threads", "1", "-o", "/dev/null", "--frames", "5",
-                "--crf", "22", "-b3", "-m7", "-r4", "--me", "esa", "-t2",
-                "-A", "all", "--psy-rd", "1.0:1.0", "--slices", "4")
-            exp(
-                ifile,
-                self.inputfiles[ifile],
-                "--threads", "1", "-o", "/dev/null", "--frames", "5",
-                "--crf", "24", "-b3", "-m10", "-r3", "--me", "tesa",
-                "-t2")
-            exp(
-                ifile,
-                self.inputfiles[ifile],
-                "--threads", "1", "-o", "/dev/null", "--frames", "5",
-                "-q0", "-m9", "-r2", "--me", "hex", "-Aall")
-            exp(
-                testfile,
-                self.inputfiles[ifile],
-                "--threads", "1", "-o", "/dev/null", "--frames", "5",
-                "-q0", "-m2", "-r1", "--me", "hex", "--no-cabac")
+            for i in range(len(tests)):
+                exp(testfile, self.inputfiles[ifile], "--threads", "1",
+                    "-o", "/dev/null", tests[i].split(" "))

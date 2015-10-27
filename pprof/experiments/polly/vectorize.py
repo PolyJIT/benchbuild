57c0db4c909a456a883c89a2d015ec91cd416243
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-10-27 15:44:49 +0100

    vectorize: use raw's run_with_time
    Like the commit before.

--- pprof/experiments/polly/vectorize.py
+++ pprof/experiments/polly/vectorize.py
@@ -43,50 +43,11 @@ class PollyVectorizer(RuntimeExperiment):
                 p.configure()
                 p.build()
             with substep("run {}".format(p.name)):
-                def run_with_time(run_f, args, **kwargs):
-                    """
-                    Function runner for the polly experiment.
+                from pprof.experiments.raw import run_with_time
 
-                    This executes the given project command wrapped in the
-                    time command. Afterwards the result is sent to the
-                    database.
-
-                    3 Metrics are generated during this experiment:
-                        time.user_s - The time spent in user space in
-                                      seconds (aka virtual time)
-                        time.system_s - The time spent in kernel space in
-                                        seconds (aka system time)
-                        time.real_s - The time spent overall in seconds
-                                      (aka Wall clock)
-
-                    :run_f:
-                        The file we actually execute.
-                    :args:
-                        Arguments to forwards to :run_f:
-                    :has_stdin:
-                        If the program requires access to a file redirected
-                        via stdin, say so.
-                    :project_name:
-                        Name of the project to enter into the db.
-                    :kwargs:
-                        Rest.
-                    """
-                    from plumbum.cmd import time
-                    from pprof.utils.run import fetch_time_output, handle_stdin
-
-                    project_name = kwargs.get("project_name", p.name)
-
-                    run_cmd = handle_stdin(
-                        time["-f", "PPROF-POLLY: %U-%S-%e", run_f, args],
-                        kwargs)
-                    _, _, stderr = run_cmd.run()
-                    timings = fetch_time_output("PPROF-POLLY: ",
-                                                "PPROF-POLLY: {:g}-{:g}-{:g}",
-                                                stderr.split("\n"))
-                    if len(timings) == 0:
-                        return
-
-                    self.persist_run(str(run_cmd), project_name, p.run_uuid,
-                                     timings)
+                run_with_time.config = config
+                run_with_time.experiment = self
+                run_with_time.project = p
+                run_with_time.jobs = config["jobs"]
 
                 p.run(run_with_time)

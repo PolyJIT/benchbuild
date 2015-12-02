3496db597a62ad0abc5ae7d22ae883985e2f19ec
Florian Sattler <vuld3r@gmail.com>
2015-12-02 16:38:42 +0100

    fixed python indent and added property to call uchroot
    Former-commit-id: 278b027f96578449182c88b903cee7ccff9fae64
    Former-commit-id: 4d3bbecfdfc5f99c463a106175b78ff27e0d3d8a [formerly 987bedc952847b4693f8e25aad2c88761fe94b2e] [formerly aa2adde07414dda4c3336745b0ee0d4fac143d01 [formerly 8095f439ad054373f0aec973039234483ab4cc7e]]
    Former-commit-id: ab6780832fe58b341536a4e6ee7b75bcbacda93f [formerly 9c78f55f3fea8908f1d8cf511e670702722650bb]
    Former-commit-id: f848b697906f3728a7cd3adee003e5df26a3813d

--- pprof/projects/gentoo/gentoo.py
+++ pprof/projects/gentoo/gentoo.py
@@ -6,54 +6,54 @@ from plumbum import local
 
 class GentooGroup(Project):
 
-  """
-  Gentoo ProjectGroup for running the gentoo test suite.
+    """
+    Gentoo ProjectGroup for running the gentoo test suite.
 
-  The following packages are required to run GentooGroup
-  * fakeroot
+    The following packages are required to run GentooGroup
+    * fakeroot
 
-  """
+    """
 
-  def __init__(self, exp, name):
-    super(GentooGroup, self).__init__(exp, name, "gentoo", "gentoo")
+    def __init__(self, exp, name):
+        super(GentooGroup, self).__init__(exp, name, "gentoo", "gentoo")
 
-  src_dir = "gentoo"
-  src_file = src_dir + ".tar.bz2"
+    src_dir = "gentoo"
+    src_file = src_dir + ".tar.bz2"
 
-  # download location for gentoo stage3 tarball
-  day = "20151119"
-  src_uri = "http://distfiles.gentoo.org/releases/amd64/current-stage3-amd64/" + "stage3-amd64-" + day + ".tar.bz2"
+    # download location for gentoo stage3 tarball
+    day = "20151119"
+    src_uri = "http://distfiles.gentoo.org/releases/amd64/current-stage3-amd64/" + "stage3-amd64-" + day + ".tar.bz2"
 
-  # download location for portage files
-  src_uri_portage = "ftp://sunsite.informatik.rwth-aachen.de/pub/Linux/gentoo/snapshots/portage-latest.tar.bz2"
-  src_file_portage = "portage_snap.tar.bz2"
+    # download location for portage files
+    src_uri_portage = "ftp://sunsite.informatik.rwth-aachen.de/pub/Linux/gentoo/snapshots/portage-latest.tar.bz2"
+    src_file_portage = "portage_snap.tar.bz2"
 
-  # test dirs
-  test_suite_dir = "TODO"
-  test_suite_uri = "TODO"
+    # test dirs
+    test_suite_dir = "TODO"
+    test_suite_uri = "TODO"
 
-  def download(self):
-    from pprof.utils.downloader import Wget
-    from pprof.utils.run import run
-    from plumbum import FG
-    from plumbum.cmd import virtualenv, cp, tar, fakeroot, rm
-    with local.cwd(self.builddir):
-      Wget(self.src_uri, self.src_file)
+    def download(self):
+        from pprof.utils.downloader import Wget
+        from pprof.utils.run import run
+        from plumbum import FG
+        from plumbum.cmd import virtualenv, cp, tar, fakeroot, rm
+        with local.cwd(self.builddir):
+            Wget(self.src_uri, self.src_file)
 
-      # TODO replace with standart path
-      cp("/home/sattlerf/gentoo/uchroot","uchroot")
-      run(fakeroot["tar", "xfj", self.src_file])
-      rm(self.src_file)
-      with local.cwd(self.builddir + "/usr"):
-        Wget(self.src_uri_portage, self.src_file_portage)
-        run(tar["xfj", self.src_file_portage])
-        rm(self.src_file_portage)
+            # TODO replace with standart path
+            cp("/home/sattlerf/gentoo/uchroot","uchroot")
+            run(fakeroot["tar", "xfj", self.src_file])
+            rm(self.src_file)
+            with local.cwd(self.builddir + "/usr"):
+                Wget(self.src_uri_portage, self.src_file_portage)
+                run(tar["xfj", self.src_file_portage])
+                rm(self.src_file_portage)
 
-  def configure(self):
-    from plumbum.cmd import mkdir, rm
-    with local.cwd(self.builddir):
-      with open("etc/portage/make.conf", 'w') as makeconf:
-        lines = '''# These settings were set by the catalyst build script that automatically
+    def configure(self):
+        from plumbum.cmd import mkdir, rm
+        with local.cwd(self.builddir):
+            with open("etc/portage/make.conf", 'w') as makeconf:
+                lines = '''# These settings were set by the catalyst build script that automatically
 # built this stage.
 # Please consult /usr/share/portage/config/make.conf.example for a more
 # detailed example.
@@ -82,34 +82,45 @@ USE="bindist mmx sse sse2"
 PORTDIR="/usr/portage"
 DISTDIR="${PORTDIR}/distfiles"
 PKGDIR="${PORTDIR}/packages"'''
-        makeconf.write(lines)
-      mkdir("etc/portage/metadata")
-      with open("etc/portage/metadata/layout.conf", 'w') as layoutconf:
-          lines = '''masters = gentoo'''
-          layoutconf.write(lines)
-      with open("etc/resolv.conf", 'w') as resolfconf:
-          lines = '''nameserver 132.231.51.4
+                makeconf.write(lines)
+            print self.builddir
+            print mkdir["etc/portage/metadata"]
+            mkdir("etc/portage/metadata")
+            with open("etc/portage/metadata/layout.conf", 'w') as layoutconf:
+                lines = '''masters = gentoo'''
+                layoutconf.write(lines)
+            with open("etc/resolv.conf", 'w') as resolfconf:
+                #cp("/etc/resolv.conf","etc/resolv.conf")
+                lines = '''nameserver 132.231.51.4
 nameserver 132.231.1.24
 nameserver 132.231.1.19
 search fim.uni-passau.de'''
-          resolfconf.write(lines)
-        # cp jit into gentoo
+                resolfconf.write(lines)
+            # cp jit into gentoo
+
+    @property
+    def execWithChroot(self):
+        '''  '''
+
+        from plumbum.cmd import binddev
+        uchroot = local["./uchroot"]
+        return binddev["-r",".","--", uchroot["-w", "/", "-r", ".", "-u", "0", "-g", "0", "--"]]
 
 class Eix(GentooGroup):
 
-  class Factory:
+    class Factory:
 
-      def create(self, exp):
-          return Eix(exp, "eix")
-  ProjectFactory.addFactory("Eix", Factory())
+        def create(self, exp):
+            return Eix(exp, "eix")
+    ProjectFactory.addFactory("Eix", Factory())
 
-  def build(self):
-    from plumbum.cmd import binddev
-    with local.cwd(self.builddir):
-        binddev("-r",".","--","./uchroot","-w", "/", "-r", ".", "-u", "0", "-g", "0", "--","/usr/bin/emerge", "eix")
-    pass
+    def build(self):
+        with local.cwd(self.builddir):
+            print "Foo code"
+            print self.execWithChroot["/usr/bin/emerge", "eix"]
+            self.execWithChroot("/usr/bin/emerge", "eix")
 
-  def run_tests(self, experiment):
-    pass
+    def run_tests(self, experiment):
+        pass
 
 

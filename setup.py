6aedab6b3157bc15be4024c7f648455f27a74b4f
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-12-03 01:07:15 +0100

    setup: improve setup.py
    We prepare for publishing in the python package index. This should ease the
    installation process for users.
    
    
    Former-commit-id: f178418b2df9eb9fe871e2ff858a8464d75ba353
    Former-commit-id: 0d43c4c6242a5012bb2cb7296cf440270bcea947 [formerly a4617aabd7e1cb3c13a4b8a037ceeff1a0e67e83] [formerly 70257233c168a668585387512b74701d5e26979d [formerly 15f500050da9797896f3d893d4cd53c8db143128]]
    Former-commit-id: 6a985068cdf8204aedb26a118588091b4815f1b6 [formerly 280d3f8235f697717a450af9a7d776c06a91fd99]
    Former-commit-id: 9ad97d671ee2002b56e9be4373023f47e4684428

--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
-setup(name='pprof',
-      version='1.0',
-      packages=find_packages(),
-      install_requires=["SQLAlchemy==1.0.4", "cloud==2.8.5", "plumbum>=1.5.0",
-                        "regex==2015.5.28", "wheel==0.24.0", "parse==1.6.6",
-                        "virtualenv==13.1.0", "sphinxcontrib-napoleon",
-                        "psycopg2", "sqlalchemy-migrate"],
-      author="Andreas Simbuerger",
-      author_email="simbuerg@fim.uni-passau.de",
-      description="This is the experiment driver for the pprof study",
-      license="MIT",
-      entry_points={
-          'console_scripts': ['pprof=pprof.driver:main']
-      })
+setup(
+    name='pprof',
+    version='1.0',
+    url='https://github.com/simbuerg/pprof-study',
+    packages=find_packages(exclude=["docs",
+                                    "extern",
+                                    "filters",
+                                    "linker",
+                                    "src",
+                                    "statistics",
+                                    "tests"
+                                    ]),
+    install_requires=["SQLAlchemy==1.0.4",
+                      "cloud==2.8.5",
+                      "plumbum>=1.5.0",
+                      "regex==2015.5.28",
+                      "wheel==0.24.0",
+                      "parse==1.6.6",
+                      "virtualenv==13.1.0",
+                      "sphinxcontrib-napoleon",
+                      "psycopg2",
+                      "sqlalchemy-migrate"],
+    author="Andreas Simbuerger",
+    author_email="simbuerg@fim.uni-passau.de",
+    description="This is the experiment driver for the pprof study",
+    license="MIT",
+    entry_points={
+        'console_scripts': ['pprof=pprof.driver:main']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Experimentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2'
+    ],
+    keywords="polyjit pprof experiments run-time",
+    )

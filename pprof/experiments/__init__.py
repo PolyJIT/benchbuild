8e975740f739625e4e308bb8803246b3d48e755c
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-12-12 20:07:18 +0100

    empty: add an empty experiment
    This can be used to test-drive the build process of a new project.
    This will create the necessary directories and run all phases up to
    build.
    
    In one of the next commits, this will be used for preparing a new
    gentoo stage3 image for pprof

--- pprof/experiments/__init__.py
+++ pprof/experiments/__init__.py
@@ -1,4 +1,5 @@
 """Add your new experiment here, if you want it to be discovered by pprof."""
 __all__ = [
-    "polly", "compilestats_ewpt", "compilestats", "papi", "polyjit", "raw"
+    "empty", "polly", "compilestats_ewpt", "compilestats", "papi", "polyjit",
+    "raw"
 ]

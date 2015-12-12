b403557556777f1413b8a981d5146eacc0866db2
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-12-12 20:07:18 +0100

    empty: add an empty experiment
    This can be used to test-drive the build process of a new project.
    This will create the necessary directories and run all phases up to
    build.
    
    In one of the next commits, this will be used for preparing a new
    gentoo stage3 image for pprof
    
    
    Former-commit-id: 773af9b42bf1cd4af3ac499a280ab12c14e34a65
    Former-commit-id: 30b6775ab4a36c241f6fef059a22a2477a2de6f7 [formerly 4bc76538ddb99ff669bbc6b9d6bb861379452b2a] [formerly 38909f97c61d4fcdf30c487b77f4e5ed295d4c5c [formerly 80e9fad980dc340b08db30652c90fb1d01e5bbf0]]
    Former-commit-id: 4768312f969dfafaf8920a442a3e6505315bc0c2 [formerly 6baa72a3e72ed51c5e42195ee017781aee386f51]
    Former-commit-id: be275c65aeb61c122e214f23f7b168b2c7725a40

---
+++ pprof/experiments/empty.py
@@ -0,0 +1,20 @@
+"""
+The 'empty' Experiment.
+
+This experiment is for debugging purposes. It only prepares the basic
+directories for pprof. No compilation & no run can be done with it.
+"""
+
+from pprof.experiment import Experiment
+
+
+class Empty(Experiment):
+    """The empty experiment."""
+
+    NAME = "empty"
+
+    def run_project(self, p):
+        """ Do nothing. """
+        p.download()
+        p.configure()
+        p.build()

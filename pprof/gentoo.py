56d5d7da6e85feceea0b9c4f14f9dfb27957aba9
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2015-12-12 20:12:08 +0100

    new tool: gentoo. Prepare your stage3 tarball
    This is the frontend tool for updating your stage3 tarball.

---
+++ pprof/gentoo.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+from plumbum import cli
+from pprof.driver import PollyProfiling
+from pprof.settings import config
+from pprof.utils.user_interface import query_yes_no
+from plumbum.cmd import mkdir
+import os, os.path
+
+
+@PollyProfiling.subcommand("gentoo")
+class Gentoo(cli.Application):
+    """ Frontend for running experiments in the pprof study framework. """
+
+    @cli.switch(["-S", "--sourcedir"], str, help="Where are the source files")
+    def sourcedir(self, dirname):
+        config["sourcedir"] = dirname
+
+    @cli.switch(["--llvm-srcdir"], str, help="Where are the llvm source files")
+    def llvm_sourcedir(self, dirname):
+        config["llvm-srcdir"] = dirname
+
+    @cli.switch(["-B", "--builddir"], str, help="Where should we build")
+    def builddir(self, dirname):
+        config["builddir"] = dirname
+
+    @cli.switch(["--likwid-prefix"], str, help="Where is likwid installed?")
+    def likwiddir(self, dirname):
+        config["likwiddir"] = dirname
+
+    @cli.switch(["-L", "--llvm-prefix"], str, help="Where is llvm?")
+    def llvmdir(self, dirname):
+        config["llvmdir"] = dirname
+
+    def main(self):
+        # Only try to create the build dir if we're actually running some projects.
+        builddir = os.path.abspath(config["builddir"])
+        if not os.path.exists(builddir):
+            response = query_yes_no(
+                "The build directory {dirname} does not exist yet. Create it?".format(
+                    dirname=builddir),
+                "no")
+            if response:
+                mkdir("-p", builddir)
+
+        from pprof.project import ProjectRegistry
+        from pprof.projects.gentoo import gentoo
+        from pprof.experiments import empty
+
+        exp = empty.Empty(["stage3"])
+        exp.clean()
+        exp.prepare()
+        exp.run()

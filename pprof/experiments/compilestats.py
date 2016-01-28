ea8748bc9ec61939f397b8409567f72469bd35d3
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-01-29 00:48:43 +0100

    compilestats: small refactoring

--- pprof/experiments/compilestats.py
+++ pprof/experiments/compilestats.py
@@ -23,60 +23,45 @@ class CompilestatsExperiment(RuntimeExperiment):
     def extra_cflags(self):
         return ["-O3"]
 
-    def persist_run(self, cmd, project_name, run_uuid, stats):
-        """ Persist the run results in the database."""
-        from pprof.utils.db import create_run
-
-        # Do not forget to update the table schema
-        run, session = create_run(cmd, project_name, self.name, run_uuid)
-
-        for stat in stats:
-            stat.run_id = run.id
-            session.add(stat)
-
-        session.commit()
-
     def run_project(self, p):
         """Compile & Run the experiment."""
         from pprof.settings import config
         from pprof.utils.run import partial
 
         llvm_libs = path.join(config["llvmdir"], "lib")
+        p.ldflags = ["-L" + llvm_libs] + self.extra_ldflags()
+        p.cflags = self.extra_cflags()
+        with step("Configure Project"):
+            p.download()
 
-        with step("Track Compilestats @ -O3"):
-            p.ldflags = ["-L" + llvm_libs] + self.extra_ldflags()
-            p.cflags = self.extra_cflags()
-            with substep("Configure Project"):
-                p.download()
+            def _track_compilestats(project, experiment, config, clang,
+                                    **kwargs):
+                from pprof.utils import run as r
+                from pprof.settings import config as c
+                from pprof.utils.db import persist_compilestats
+                from pprof.utils.run import handle_stdin
 
-                def _track_compilestats(project, experiment, config, clang,
-                                        **kwargs):
-                    from pprof.utils import run as r
-                    from pprof.settings import config as c
-                    from pprof.utils.db import persist_compilestats
-                    from pprof.utils.run import handle_stdin
+                c.update(config)
+                clang = handle_stdin(clang["-mllvm", "-stats"], kwargs)
 
-                    c.update(config)
-                    clang = handle_stdin(clang["-mllvm", "-stats"], kwargs)
+                run, session, retcode, _, stderr = \
+                    r.guarded_exec(clang, project.name, experiment.name, project.run_uuid)
 
-                    run, session, retcode, _, stderr = \
-                        r.guarded_exec(clang, project.name, experiment.name, project.run_uuid)
+                if retcode == 0:
+                    stats = []
+                    for stat in get_compilestats(stderr):
+                        compile_s = CompileStat()
+                        compile_s.name = stat["desc"].rstrip()
+                        compile_s.component = stat["component"].rstrip()
+                        compile_s.value = stat["value"]
+                        stats.append(compile_s)
+                    persist_compilestats(run, session, stats)
 
-                    if retcode == 0:
-                        stats = []
-                        for stat in get_compilestats(stderr):
-                            compile_s = CompileStat()
-                            compile_s.name = stat["desc"].rstrip()
-                            compile_s.component = stat["component"].rstrip()
-                            compile_s.value = stat["value"]
-                            stats.append(compile_s)
-                        persist_compilestats(run, session, stats)
-
-                p.compiler_extension = partial(_track_compilestats, p, self,
-                                               config)
-                p.configure()
-            with substep("Build Project"):
-                p.build()
+            p.compiler_extension = partial(_track_compilestats, p, self,
+                                           config)
+            p.configure()
+        with step("Build Project"):
+            p.build()
 
 
 def get_compilestats(prog_out):

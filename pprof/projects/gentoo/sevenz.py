26f5286d5b7281f58b6de471e92d6e13ac1eae71
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-02-15 15:07:51 +0100

    gentoo: new projects: 7z, gzip, xz-utils
    Same behavior as pprof counterparts, just within gentoo uchroot.
    
    
    Former-commit-id: 33c9117ffacd68c127e6d46f8210ff2e97b94957
    Former-commit-id: bcdaeefba3f6f71775dd206a11a3217d02a09b2a [formerly 2113a83d36891683f6388e73986ecc97f5493d62] [formerly 7fc18f941d460bea64460fab039ca6f1604061f1 [formerly 2556f3c78bd97d5a98107965911f2625c7838d5c]]
    Former-commit-id: 1d48c3edf101822e7c1f13ef39a8e7a5ab1a8e09 [formerly 2d12c88698b90bc83c44573d8cf33683e988c418]
    Former-commit-id: 745d236544c616f8ead594726cbcff0edb4738df

---
+++ pprof/projects/gentoo/sevenz.py
@@ -0,0 +1,28 @@
+"""
+p7zip experiment within gentoo chroot.
+"""
+from os import path
+from pprof.projects.gentoo.gentoo import GentooGroup
+from pprof.utils.run import run, uchroot
+from plumbum import local
+
+
+class SevenZip(GentooGroup):
+    """
+        app-arch/p7zip
+    """
+    NAME = "gentoo-7z"
+    DOMAIN = "app-arch"
+
+    def build(self):
+        with local.cwd(self.builddir):
+            emerge_in_chroot = uchroot()["/usr/bin/emerge"]
+            run(emerge_in_chroot["app-arch/p7zip"])
+
+    def run_tests(self, experiment):
+        from pprof.project import wrap
+
+        wrap(path.join(self.builddir, "usr", "bin", "7z"), experiment,
+             self.builddir)
+        sevenz = uchroot()["/usr/bin/7z"]
+        run(sevenz["b", "-mmt1"])

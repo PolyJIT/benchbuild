2a33ca43a6bc1d2fe7861bda02d412a6cc886312
Florian Sattler <vuld3r@gmail.com>
2016-02-25 09:44:48 +0100

    added pprof gentoo auto ebuild-test Automatic creation of ebuild list, filterable by language.
    Former-commit-id: 96a4742d8d511273c5edfd5332cad443f13d7f21
    Former-commit-id: bd71e5c0f4cfc9df21adedef34748527d8b844e7 [formerly 81d19b8ad9c6d25882b66bd1b2c7021dc5f3c819] [formerly f2ccc9a277794e5a4debf333b9e14c73832d172f [formerly 7f71868a19570c93037a3bf06b9c684c24445353]]
    Former-commit-id: 9e5b49639f24507003522b1699f1ffa54ad25fa5 [formerly a63a603f93fd569c3469d8a674ea163231177ed4]
    Former-commit-id: 58d89a23bc91757c0fefc8aaa7e0e4edf3c46e2b

--- pprof/projects/gentoo/info.py
+++ pprof/projects/gentoo/info.py
@@ -1,5 +1,5 @@
 """
-get packages infos from gentoo chroot
+Get package infos, e.g., specific ebuilds for given languages, from gentoo chroot.
 """
 from pprof.projects.gentoo.portage_gen import Portage_Gen
 from pprof.utils.run import run, uchroot
@@ -12,7 +12,7 @@ class Info(Portage_Gen):
     """
 
     NAME = "gentoo-info"
-    DOMAIN = "info"
+    DOMAIN = "debug"
 
     def build(self):
         with local.cwd(self.builddir):
@@ -21,6 +21,28 @@ class Info(Portage_Gen):
                 run(emerge_in_chroot["app-portage/portage-utils"])
 
             qgrep_in_chroot = uchroot()["/usr/bin/qgrep"]
-            file_location = config["pprof-gentoo-autotest"]
-            (qgrep_in_chroot["-l", "tc-getCC"] > file_location)()
+            ebuilds = set()
+            for language in config["pprof-gentoo-autotest-lang"].split(','):
+                output = qgrep_in_chroot("-l", \
+                        get_string_for_language(language))
+                for line in output.split('\n'):
+                    if "ebuild" in line:
+                        parts = line.split('.ebuild')[0].split('/')
+                        ebuilds.add(parts[0] + '/' + parts[1])
+
+            file_location = config["pprof-gentoo-autotest-loc"]
+            with open(file_location, "w") as output_file:
+                for ebuild in sorted(ebuilds):
+                    output_file.write(str(ebuild) + "\n")
+                output_file.flush()
+
+def get_string_for_language(language_name):
+    """
+    Maps language names to the corresponding string for qgrep.
+    """
+    language_name = language_name.lower().lstrip()
+    if language_name == "c":
+        return "tc-getCC"
+    if language_name == "c++" or language_name == "cxx":
+        return "tc-getCXX"
 

b26351ce561ea4ea4109c153960f0ca63420b785
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-03-01 20:34:25 +0100

    portage_gen: document PortageFactory
    Former-commit-id: 919c813d9fbedbe84aa628336513b18b485be7d7
    Former-commit-id: 47783058d20e34226fcdb55505fc4b46038eecd3 [formerly cc47ac7381a7b6375cf0e4a77d2f13c3fdf06cd2] [formerly 524c573bfd07ae31e9c4fec495fc3dc911a57095 [formerly f1fc3bd76eca4f247e2f5bb1fd593af086ee58bf]]
    Former-commit-id: 624cf0e06cee2b4f0f037bf5706d3f3fbbc05a14 [formerly 08dba2d0164cdecfa4f88871cc3b2bc083656d6d]
    Former-commit-id: 418f7d70c191a5b02c32e02bf332cb8945ac4011

--- pprof/projects/gentoo/portage_gen.py
+++ pprof/projects/gentoo/portage_gen.py
@@ -20,6 +20,38 @@ class AutoPortage(GentooGroup):
         pass
 
 def PortageFactory(name, NAME, DOMAIN, BaseClass=AutoPortage):
+    """
+    Create a new dynamic portage project.
+
+    Auto-Generated projects can only be used for compilie-time experiments,
+    because there simply is no run-time test defined for it. Therefore,
+    we implement the run symbol as a noop (with minor logging).
+
+    This way we avoid the default implementation for run() that all projects
+    inherit.
+
+    Args:
+        name: Name of the dynamic class.
+        NAME: NAME property of the dynamic class.
+        DOMAIN: DOMAIN property of the dynamic class.
+        BaseClass: Base class to use for the dynamic class.
+
+    Returns:
+        A new class with NAME,DOMAIN properties set, unable to perform
+        run-time tests.
+
+    Examples:
+        >>> from pprof.projects.gentoo.portage_gen import PortageFactory
+        >>> from pprof.experiments.empty import Empty
+        >>> c = PortageFactory("test", "NAME", "DOMAIN")
+        >>> c
+        <class 'pprof.projects.gentoo.portage_gen.test'>
+        >>> i = c(Empty())
+        >>> i.NAME
+        'NAME'
+        >>> i.DOMAIN
+        'DOMAIN'
+    """
     newclass = type(name, (BaseClass,), {
         "NAME" : NAME,
         "DOMAIN" : DOMAIN

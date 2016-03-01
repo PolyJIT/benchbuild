ed42b47ba88200dd818f37f9106808ae04b2db59
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-03-01 16:25:23 +0100

    schema: don't overwrite the CFG object with a dictionary.
    Former-commit-id: f98613c181edf8edd75df57d7d998464da7a42bb
    Former-commit-id: 3a0ffeb23d481791ac95df7b1cbc674fb0e2036f [formerly 9712140183e1d276d5e0588727e00ca939c505db] [formerly e4753e262dc28d7a95df0142326b7eadde9853ab [formerly 1ca17463d635bcda81ab7233904b8870345c9949]]
    Former-commit-id: 4d863fe8ea395347e9e2ce60d1ca83a48c8aba76 [formerly bb105b73f3ad92059b75ba63f84fbfb181e8cf42]
    Former-commit-id: d01401372dcf93f2035a04698ec0fb978ee55b65

--- pprof/utils/schema.py
+++ pprof/utils/schema.py
@@ -8,35 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pprof.settings import CFG
 
-CFG['db'] = {
-    "host": {
-        "desc": "host name of our db.",
-        "default": "localhost"
-    },
-    "port": {
-        "desc": "port to connect to the database",
-        "default": 5432
-    },
-    "name": {
-        "desc": "The name of the PostgreSQL database that will be used.",
-        "default": "pprof"
-    },
-    "user": {
-        "desc":
-        "The name of the PostgreSQL user to connect to the database with.",
-        "default": "pprof"
-    },
-    "pass": {
-        "desc":
-        "The password for the PostgreSQL user used to connect to the database with.",
-        "default": "pprof"
-    },
-    "rollback": {
-        "desc": "Rollback all operations after pprof completes.",
-        "default": False
-    }
-}
-
 Base = declarative_base()
 
 

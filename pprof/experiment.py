726837883466f056431aa160d9255159d437ea59
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-03-07 14:02:57 +0100

    experiment: signal the user that we could not clean the experiment
    Former-commit-id: 4cfe4512babf1b22b83b02153db6d036f95042f4
    Former-commit-id: 317018324793c6c0a89dfc22ab8142edf0ef8e08 [formerly 5e4ffa4672a7235a8053730d5338b3203f0e3566] [formerly 107b55c5be096fc37d9b09e293b757e1989d5c20 [formerly 727d1fd27a0f744dd69f9425b56ade40d5649b67]]
    Former-commit-id: 810fc4e13255c6ac07c51d6d4ea2665df269842b [formerly 3245f6d0ac6171aadca1b300da5f11710d7de908]
    Former-commit-id: a5e32e0a468dba9e335ff28b57dcd628f69e3f13

--- pprof/experiment.py
+++ pprof/experiment.py
@@ -26,6 +26,7 @@ An experiment performs the following actions in order:
 
 """
 import warnings
+import logging
 import traceback as tb
 from contextlib import contextmanager
 from abc import abstractmethod
@@ -369,8 +370,17 @@ class Experiment(object, metaclass=ExperimentRegistry):
     def clean(self):
         """Clean the experiment."""
         self.map_projects(self.clean_project, "clean")
-        if (path.exists(self.builddir)) and listdir(self.builddir) == []:
-            rmdir(self.builddir)
+        if path.exists(self.builddir) and listdir(self.builddir) == []:
+            try:
+                rmdir(self.builddir)
+            except ProcessExecutionError as ex:
+                warnings.warn(str(ex), categeory=RuntimeWarning)
+        else:
+            logger = logging.getLogger(__name__)
+            logger.info("Experiment directory '{}' is not clean.".format(
+                self.builddir))
+
+
 
     def prepare(self):
         """

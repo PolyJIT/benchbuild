efbbc219d3806292b9951e49990845a310c65bdb
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-03-10 09:42:02 +0100

    cs: static compilestats experiment, repaired

--- pprof/experiments/compilestats.py
+++ pprof/experiments/compilestats.py
@@ -7,65 +7,65 @@ by llvm
 
 """
 
+import parse
+from plumbum import local
 from pprof.experiment import step, RuntimeExperiment
-from pprof.settings import CFG
-from os import path
+from pprof.utils.run import partial
+
+def collect_compilestats(project, experiment, config, clang, **kwargs):
+    """Collect compilestats."""
+    from pprof.utils import run as r
+    from pprof.settings import CFG as c
+    from pprof.utils.db import persist_compilestats
+    from pprof.utils.run import handle_stdin
+    from pprof.utils.schema import CompileStat
+
+    c.update(config)
+    clang = handle_stdin(clang["-mllvm", "-stats"], kwargs)
+
+    with local.env(PPROF_ENABLE=0):
+        run, session, retcode, _, stderr = r.guarded_exec(
+            clang, project.name, experiment.name, project.run_uuid)
+
+    if retcode == 0:
+        stats = []
+        for stat in get_compilestats(stderr):
+            compile_s = CompileStat()
+            compile_s.name = stat["desc"].rstrip()
+            compile_s.component = stat["component"].rstrip()
+            compile_s.value = stat["value"]
+            stats.append(compile_s)
+        persist_compilestats(run, session, stats)
+
 
 class CompilestatsExperiment(RuntimeExperiment):
     """The compilestats experiment."""
 
-    NAME = "stats"
-
-    def extra_ldflags(self):
-        return []
-
-    def extra_cflags(self):
-        return ["-O3"]
+    NAME = "cs"
 
     def run_project(self, p):
-        """Compile & Run the experiment."""
-        from pprof.utils.schema import CompileStat
-        from pprof.utils.run import partial
+        """
+        Args:
+            p: The project we run.
+        """
+        from pprof.settings import CFG
 
-        llvm_libs = path.join(str(CFG["llvm"]["dir"]), "lib")
-        p.ldflags = ["-L" + llvm_libs] + self.extra_ldflags()
-        p.cflags = self.extra_cflags()
-        with step("Configure Project"):
-            p.download()
-
-            def _track_compilestats(project, experiment, config, clang,
-                                    **kwargs):
-                from pprof.utils import run as r
-                from pprof.utils.db import persist_compilestats
-                from pprof.utils.run import handle_stdin
-
-                clang = handle_stdin(clang["-mllvm", "-stats"], kwargs)
-
-                run, session, retcode, _, stderr = \
-                    r.guarded_exec(clang, project.name, experiment.name, project.run_uuid)
-
-                if retcode == 0:
-                    stats = []
-                    for stat in get_compilestats(stderr):
-                        compile_s = CompileStat()
-                        compile_s.name = stat["desc"].rstrip()
-                        compile_s.component = stat["component"].rstrip()
-                        compile_s.value = stat["value"]
-                        stats.append(compile_s)
-                    persist_compilestats(run, session, stats)
-
-            p.compiler_extension = partial(_track_compilestats, p, self,
-                                           CFG)
-            p.configure()
-        with step("Build Project"):
-            p.build()
+        with local.env(PPROF_ENABLE=1):
+            p.compiler_extension = partial(collect_compilestats, p, self, CFG)
+            with step("Prepare build directory."):
+                p.clean()
+                p.prepare()
+            with step("Downloading sources."):
+                p.download()
+            with step("Bulding {}.".format(p.name)):
+                p.configure()
+                p.build()
 
 
 def get_compilestats(prog_out):
     """ Get the LLVM compilation stats from :prog_out:. """
-    from parse import compile
 
-    stats_pattern = compile("{value:d} {component} - {desc}\n")
+    stats_pattern = parse.compile("{value:d} {component} - {desc}\n")
 
     for line in prog_out.split("\n"):
         res = stats_pattern.search(line + "\n")

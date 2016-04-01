9a158873fce07eff4afbf183066ddb51ffd3e849
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-04-01 12:13:24 +0200

    require pylint for pprof

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ["lazy==1.2", "SQLAlchemy==1.0.4", "dill==0.2.4", "plumbum>=1.5.0",
      "regex==2015.5.28", "wheel==0.24.0", "parse==1.6.6", "virtualenv==13.1.0",
      "sphinxcontrib-napoleon", "psycopg2", "sqlalchemy-migrate", "six>=1.7.0",
-     "psutil>=4.0.0"],
+     "psutil>=4.0.0", "pylint>=1.5.5"],
     author="Andreas Simbuerger",
     author_email="simbuerg@fim.uni-passau.de",
     description="This is the experiment driver for the pprof study",

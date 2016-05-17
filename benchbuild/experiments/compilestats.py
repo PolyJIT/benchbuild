ed0906e5e702aec0c074652c7d2be91dc29ddc5c
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-05-17 23:14:18 +0200

    compilestats: access ri.retcode instead of retcode
    Interface change broke this.

--- benchbuild/experiments/compilestats.py
+++ benchbuild/experiments/compilestats.py
@@ -31,7 +31,8 @@ def collect_compilestats(project, experiment, config, clang, **kwargs):
         with guarded_exec(clang, project, experiment) as run:
             ri = run()
 
-    if retcode == 0:
+
+    if ri.retcode == 0:
         stats = []
         for stat in get_compilestats(ri.stderr):
             compile_s = CompileStat()

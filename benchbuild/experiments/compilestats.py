e33fb2c7daae710f8a88471c95b492df788b7db5
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-05-17 23:14:28 +0200

    cleanup & yapf
    Former-commit-id: 059f4bad3d68b85a6568dafc9821aaccc9dec856
    Former-commit-id: 970d0d30f9398ce1e0e5920065ac3f5a3eff481b [formerly 9b8ccfd21321f9c547bcf5333e5ce85d0d411916] [formerly 1eecc74fdf535a68aea482f4ccd16d1186fc991f [formerly a607ed975bcce8ae5789d512d8961fc51d693dfe]]
    Former-commit-id: dcd0d73be12faf23999b1eb60659aff756c800a1 [formerly 8c2cf583ac286b462e0f8e5eb8b475748ab76c6b]
    Former-commit-id: e769bc2e6f9cc834f4d7b86c5bc8a6ff10fecd82

--- benchbuild/experiments/compilestats.py
+++ benchbuild/experiments/compilestats.py
@@ -13,15 +13,15 @@ import warnings
 from plumbum import local
 from benchbuild.experiment import RuntimeExperiment
 from benchbuild.utils.run import partial
-from benchbuild.utils.actions import (Prepare, Build, Download, Configure, Clean,
-                                 MakeBuildDir, Echo)
+from benchbuild.utils.actions import (Prepare, Build, Download, Configure,
+                                      Clean, MakeBuildDir, Echo)
+
 
 def collect_compilestats(project, experiment, config, clang, **kwargs):
     """Collect compilestats."""
     from benchbuild.utils.run import guarded_exec, handle_stdin
     from benchbuild.settings import CFG as c
     from benchbuild.utils.db import persist_compilestats
-    from benchbuild.utils.run import handle_stdin
     from benchbuild.utils.schema import CompileStat
 
     c.update(config)
@@ -43,10 +43,10 @@ def collect_compilestats(project, experiment, config, clang, **kwargs):
 
         components = c["cs"]["components"].value()
         if components is not None:
-            stats = [ s for s in stats if str(s.component) in components]
+            stats = [s for s in stats if str(s.component) in components]
         names = c["cs"]["names"].value()
         if names is not None:
-            stats = [ s for s in stats if str(s.name) in names]
+            stats = [s for s in stats if str(s.name) in names]
 
         persist_compilestats(ri.db_run, ri.session, stats)
 
@@ -101,7 +101,6 @@ class PollyCompilestatsExperiment(RuntimeExperiment):
         return actns
 
 
-
 def get_compilestats(prog_out):
     """ Get the LLVM compilation stats from :prog_out:. """
 
@@ -115,8 +114,9 @@ def get_compilestats(prog_out):
             try:
                 res = stats_pattern.search(line + "\n")
             except ValueError as e:
-                warnings.warn("Triggered a parser exception for: '" + line +
-                              "'\n", CompileStatsParserError)
+                warnings.warn(
+                    "Triggered a parser exception for: '" + line + "'\n",
+                    CompileStatsParserError)
                 res = None
             if res is not None:
                 yield res

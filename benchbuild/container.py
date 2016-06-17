df56076f6a402a273e61e8d8afc22fbddd75e3d4
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-06-17 13:45:34 +0200

    container: add a container management driver
    This commit adds a separate 'binary' that takes over the role of
    the old gentoo stage3 project.
    
    With calls to container you can setup a fresh container from any
    linux base image and pack it into one that can be used with
    benchbuild.
    
    The output of container help gives more information about the
    available flags.
    
    
    Former-commit-id: 55941daac97220711f5578349da79694c480badb
    Former-commit-id: 50fd33da6db321561fd27103d512a2a19db405bb [formerly d009e1212ca23d764a95f10d62d79fa7e2def419] [formerly b638a9675be7724244a413ce631e1eb837433481 [formerly e13352584280d4645a17ef3091a1b2a634019e18]]
    Former-commit-id: 10cca9066dfa329258ea87f005374328af38670b [formerly 59903c5f2b29b59c1d9cf69e3f283ba59f5429c6]
    Former-commit-id: 6620979b7f2df49c804012a68fe1602926791068

---
+++ benchbuild/container.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+
+from plumbum import cli, local, TF, FG, ProcessExecutionError
+from plumbum.cmd import tar, mkdir, mv, rm, bash
+from benchbuild import settings
+from benchbuild.utils import user_interface as ui
+from benchbuild.utils import log
+from benchbuild.utils.run import uchroot_no_args
+from benchbuild.utils.downloader import Copy, update_hash
+
+import logging
+import sys
+import os
+
+
+def ask(question, default_answer=False, default_answer_str="no"):
+    response = default_answer
+    if sys.stdin.isatty():
+        response = ui.query_yes_no(question, default_answer_str)
+    return response
+
+
+def clean_directories(builddir, in_dir=True, out_dir=True):
+    with local.cwd(builddir):
+        if in_dir and os.path.exists("container-in") and ask(
+                "Should I delete '{0}'?".format(os.path.abspath(
+                    "container-in"))):
+            rm("-rf", "container-in")
+        if out_dir and os.path.exists("container-out") and ask(
+                "Should I delete '{0}'?".format(os.path.abspath(
+                    "container-out"))):
+            rm("-rf", "container-out")
+
+
+def setup_directories(builddir):
+    with local.cwd(builddir):
+        if not os.path.exists("container-in"):
+            mkdir("-p", "container-in")
+        if not os.path.exists("container-out"):
+            mkdir("-p", "container-out")
+
+
+def setup_container(builddir, container):
+    with local.cwd(builddir):
+        container_filename = str(container).split(os.path.sep)[-1]
+        container_in = os.path.join("container-in", container_filename)
+        Copy(container, container_in)
+        uchroot = uchroot_no_args()
+
+        with local.cwd("container-in"):
+            uchroot = uchroot["-E", "-A", "-u", "0", "-g", "0", "-C", "-r",
+                              "/", "-w", os.path.abspath("."), "--"]
+
+        # Check, if we need erlent support for this archive.
+        has_erlent = bash[
+            "-c", "tar --list -f './{0}' | grep --silent '.erlent'".format(
+                container_in)]
+        has_erlent = (has_erlent & TF)
+
+        # Unpack input container to: container-in
+        if not has_erlent:
+            cmd = local["/bin/tar"]["xf"]
+            cmd = uchroot[cmd["./" + container_in]]
+        else:
+            cmd = tar["xf"]
+            cmd = cmd[os.path.abspath(container_in)]
+
+        with local.cwd("container-in"):
+            cmd("--exclude=dev/*")
+        rm(container_in)
+    return os.path.join(builddir, "container-in")
+
+
+def run_in_container(command, container_dir, mounts):
+    uchroot = uchroot_no_args()["-E", "-A", "-u", "0", "-g", "0", "-C", "-w",
+                                "/", "-r", os.path.abspath(container_dir)]
+    uchroot_m = uchroot
+    uchroot = uchroot["--"]
+
+    for mount in mounts:
+        absdir = os.path.abspath(str(mount))
+        dirname = os.path.split(absdir)[-1]
+        uchroot_m = uchroot_m["-m", "{0}:/mnt/{1}".format(absdir, dirname)]
+        mount_path = os.path.join(container_dir, "mnt", dirname)
+        if not os.path.exists(mount_path):
+            uchroot("mkdir", "-p", "/mnt/{0}".format(dirname))
+        print("Mounting: '{0}' inside container at '/mnt/{1}'".format(mount,
+                                                                      dirname))
+
+    cmd_path = os.path.join(container_dir, command.lstrip('/'))
+    if not os.path.exists(cmd_path):
+        logging.error(
+            "The command does not exist inside the container! {0}".format(
+                cmd_path))
+        return
+
+    cmd = uchroot_m[command]
+    return cmd & FG
+
+
+def setup_bash_in_container(builddir, container, outfile, mounts, shell):
+    with local.cwd(builddir):
+        # Switch to bash inside uchroot
+        print("Entering bash inside User-Chroot. Prepare your image and "
+              "type 'exit' when you are done. If bash exits with a non-zero"
+              "exit code, no new container will be stored.")
+        store_new_container = True
+        try:
+            run_in_container(shell, container, mounts)
+        except ProcessExecutionError:
+            store_new_container = False
+
+        if store_new_container:  # pylint: disable=W0104
+            print("Packing new container image.")
+
+            container_out = os.path.join("container-out", container_filename)
+            container_out = os.path.abspath(container_out)
+
+            # Pack the results to: container-out
+            with local.cwd("container-in"):
+                tar("cjf", container_out, ".")
+            update_hash(container_filename, os.path.dirname(container_out))
+            outdir = os.path.dirname(outfile)
+            if not os.path.exists(outdir):
+                mkdir("-p", outdir)
+            mv(container_out, outfile)
+
+
+class Container(cli.Application):
+    """Manage uchroot containers."""
+    VERSION = settings.CFG["version"].value()
+
+    def __init__(self, exe):
+        super(Container, self).__init__(exe)
+        self._input_container = None
+        self._output_container = None
+        self._builddir = None
+        self._mounts = []
+        self._create = False
+        self._shell = "/bin/bash"
+
+    @cli.switch(["-i", "--input-file"],
+                str,
+                mandatory=True,
+                help="Input container path")
+    def input_file(self, container):
+        if os.path.exists(container):
+            self._input_container = container
+        else:
+            raise ValueError("The path '{0}' does not exist.".format(
+                container))
+
+    @cli.switch(["-o", "--output-file"], str, help="Output container path")
+    def output_file(self, container):
+        self._output_container = container
+
+    @cli.switch(["-c", "--create"],
+                help="Create a new container.",
+                requires=["--output-file"])
+    def create(self):
+        self._create = True
+
+    @cli.switch(["-s", "--shell"],
+                str,
+                help="The shell command we invoke inside the container.")
+    def shell(self, custom_shell):
+        self._shell = custom_shell
+
+    @cli.switch(["-t", "-tmp-dir"],
+                cli.ExistingDirectory,
+                help="Temporary directory")
+    def builddir(self, tmpdir):
+        settings.CFG["build_dir"] = tmpdir
+
+    @cli.switch(
+        ["m", "--mount"],
+        cli.ExistingDirectory,
+        list=True,
+        help="Mount the given directory under / inside the uchroot container")
+    def mounts(self, user_mount):
+        self._mounts = user_mount
+
+    verbosity = cli.CountOf('-v', help="Enable verbose output")
+
+    def main(self, *args):
+        self._builddir = settings.CFG["build_dir"].value()
+
+        log.configure()
+        LOG = logging.getLogger()
+        LOG.setLevel({
+            3: logging.DEBUG,
+            2: logging.INFO,
+            1: logging.WARNING,
+            0: logging.ERROR
+        }[self.verbosity])
+
+        builddir = os.path.abspath(self._builddir)
+        if not os.path.exists(builddir):
+            response = ask("The build directory {dirname} does not exist yet. "
+                           "Should I create it?".format(dirname=builddir))
+
+            if response:
+                mkdir("-p", builddir)
+                print("Created directory {0}.".format(builddir))
+
+        setup_directories(builddir)
+        container_dir = self._input_container
+        in_dir = False
+        out_dir = False
+        if os.path.isfile(self._input_container):
+            in_dir = True
+            container_dir = setup_container(builddir, self._input_container)
+        if self._create:
+            out_dir = True
+            setup_bash_in_container(builddir, container_dir,
+                                    self._output_container, self._mounts,
+                                    self._shell)
+        else:
+            run_in_container(" ".join(args), container_dir, self._mounts)
+
+        clean_directories(builddir, in_dir, out_dir)
+
+
+def main(*args):
+    return Container.run(*args)

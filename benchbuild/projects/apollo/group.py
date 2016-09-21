44ee0986e4ff59494b633e6969ed3fbd1cee6390
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-09-21 14:38:59 +0200

    new projects: rodinia & scimark
    2 new benchmark suites used by the apollo project.
    
    
    Former-commit-id: c702f0601083e01904228e381477fbb4c9aa4828
    Former-commit-id: 17e72b73c43846ebf2766805303ba6054db4d706 [formerly d8437aff082f3968b5018571c13245e30d9c8b59] [formerly 0b8834bdc1d84555b35cacd15a8dd37278382c1f [formerly 6e3a62e070245bd697b40dc43b78d383b230aef4]]
    Former-commit-id: a3f775e85b51309034b59b17753b7c07a5ec632c [formerly 2da2685bd137246c147676c9f145c762959ef964]
    Former-commit-id: 0001a96ad0e40c757481abe9aa7a53181353fbb9

---
+++ benchbuild/projects/apollo/group.py
@@ -0,0 +1,14 @@
+from benchbuild.project import Project
+from benchbuild.settings import CFG
+from os import path
+
+
+class ApolloGroup(Project):
+    GROUP = 'apollo'
+
+    path_suffix = "src"
+
+    def __init__(self, exp):
+        super(ApolloGroup, self).__init__(exp, "apollo")
+        self.sourcedir = path.join(str(CFG["src_dir"]), "src", self.name)
+        self.setup_derived_filenames()

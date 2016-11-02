afc4ea24d2ffc63c6030e913755cddb75bf57093
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-11-02 01:43:21 +0100

    js: update to newest gecko
    this requires an old version of autoconf: autoconf-2.13
    
    
    Former-commit-id: bc9152d6e12d780d64d3ba38da99bc0826c79afd
    Former-commit-id: 6fc39cb5e7321560bd5f2acb22f3ffa8614f4249 [formerly 5928f0d0acecc171c7961caadfd85b7e4f78e415] [formerly c3014fa1517bc995b79de9dd863b692540f2c38a [formerly a8d578b40f35c577ef314dd13bbfb313b8409f88]]
    Former-commit-id: cfcfeac9f6bf616162c557d0d4cf1413d2adfe2f [formerly e68962a7556dbb1e140ad78e21cc96fd5636cdb2]
    Former-commit-id: a1c12d5c09f8439d6160dc08dd05eb06f069aac4

--- benchbuild/projects/benchbuild/js.py
+++ benchbuild/projects/benchbuild/js.py
@@ -7,12 +7,16 @@ from benchbuild.utils.run import run
 from benchbuild.utils.versions import get_git_hash
 
 from plumbum import local
-from benchbuild.utils.cmd import make, mkdir
+from benchbuild.utils.cmd import make, mkdir, tar
 
 from os import path
 
 
 class SpiderMonkey(BenchBuildGroup):
+    """
+    SpiderMonkey requires a legacy version of autoconf: autoconf-2.13
+    """
+
     NAME = 'js'
     DOMAIN = 'compilation'
 
@@ -35,23 +39,33 @@ class SpiderMonkey(BenchBuildGroup):
         clang_cxx = lt_clang_cxx(self.cflags, self.ldflags,
                                  self.compiler_extension)
         with local.cwd(js_dir):
-            autoconf = local["autoconf"]
+            make_src_pkg = local["./make-source-package.sh"]
+            with local.env(DIST=self.builddir,
+                           MOZJS_MAJOR_VERSION=0,
+                           MOZJS_MINOR_VERSION=0,
+                           MOZJS_PATCH_VERSION=0):
+                make_src_pkg()
+
+        mozjs_dir = "mozjs-0.0.0"
+        tar("xfj", mozjs_dir + ".tar.bz2")
+        with local.cwd(path.join(mozjs_dir, "js", "src")):
+            mkdir("obj")
+            autoconf = local["autoconf-2.13"]
             autoconf()
-            mkdir("build_OPT.OBJ")
-            with local.cwd("build_OPT.OBJ"):
-                with local.env(CC=str(clang), CXX=str(clang_cxx)):
+            with local.cwd("obj"):
+                with local.env(CC=str(clang),
+                               CXX=str(clang_cxx)):
                     configure = local["../configure"]
                     run(configure)
 
     def build(self):
-        js_dir = path.join(self.src_dir, "js", "src", "build_OPT.OBJ")
-        with local.cwd(js_dir):
-            run(make["-j", CFG["available_cpu_count"].value()])
+        mozjs_dir = path.join("mozjs-0.0.0", "js", "src", "obj")
+        with local.cwd(mozjs_dir):
+            run(make["-j", CFG["jobs"].value()])
 
     def run_tests(self, experiment):
-        js_dir = path.join(self.src_dir, "js", "src")
-        js_build_dir = path.join(js_dir, "build_OPT.OBJ")
-        wrap(path.join(js_build_dir, "bin", "js"), experiment)
+        mozjs_dir = path.join("mozjs-0.0.0", "js", "src", "obj")
+        wrap(path.join(mozjs_dir, "bin", "js"), experiment)
 
-        with local.cwd(js_build_dir):
+        with local.cwd(mozjs_dir):
             run(make["check"])

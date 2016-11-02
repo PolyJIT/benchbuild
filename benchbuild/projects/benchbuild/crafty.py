a68b937b61ee1be73fe6f7d8e1d9f58de8598772
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2016-11-02 01:11:09 +0100

    crafty: update to 25.2
    Former-commit-id: 7d5fc935bd5b1651a4bc3ecf4534e8222476611d
    Former-commit-id: c324fb29b7ed7aaefc546012ab238f41f63344cb [formerly 4c9b630c43e7b934403f0d16bf57fa80df879714] [formerly 042f68cccc0d5c07c4e8958bf19e4f6678d9f6aa [formerly 6f4b146ae13a6fc47825ac00178d0abd87a6b647]]
    Former-commit-id: 57829d5223c70fb7c5754ada01811001031f0857 [formerly b1ac2d4cfa178c9ef085a5b2d954b8f9bd2ae80a]
    Former-commit-id: ee3ee8f63ece9bc572b664cdc3c048d40391b69d

--- benchbuild/projects/benchbuild/crafty.py
+++ benchbuild/projects/benchbuild/crafty.py
@@ -5,7 +5,7 @@ from benchbuild.utils.downloader import Wget
 from benchbuild.utils.run import run
 from os import path
 from plumbum import local
-from benchbuild.utils.cmd import cat, make, unzip, mv
+from benchbuild.utils.cmd import cat, make, unzip, mv, mkdir
 
 
 class Crafty(BenchBuildGroup):
@@ -13,19 +13,22 @@ class Crafty(BenchBuildGroup):
 
     NAME = 'crafty'
     DOMAIN = 'scientific'
-    VERSION = '23.4'
+    VERSION = '25.2'
 
     src_dir = "crafty-{0}".format(VERSION)
-    src_uri = "http://www.craftychess.com/crafty-{0}.zip".format(VERSION)
+    src_uri = "http://www.craftychess.com/downloads/source/crafty-{0}.zip".format(VERSION)
     SRC_FILE = src_dir + ".zip"
 
     def download(self):
         book_file = "book.bin"
-        book_bin = "http://www.craftychess.com/" + book_file
+        book_bin = "http://www.craftychess.com/downloads/book/" + book_file
         Wget(self.src_uri, self.SRC_FILE)
-        Wget(book_bin, "book.bin")
+        Wget(book_bin, book_file)
 
-        unzip(self.SRC_FILE)
+        mkdir(self.src_dir)
+
+        with local.cwd(self.src_dir):
+            unzip(path.join("..", self.SRC_FILE))
         mv(book_file, self.src_dir)
 
     def configure(self):
@@ -33,16 +36,15 @@ class Crafty(BenchBuildGroup):
 
     def build(self):
         clang = lt_clang(self.cflags, self.ldflags, self.compiler_extension)
-        clang_cxx = lt_clang_cxx(self.cflags, self.ldflags,
-                                 self.compiler_extension)
         with local.cwd(self.src_dir):
-            target_opts = ["-DINLINE64", "-DCPUS=1"]
+            target_opts = ["-DCPUS=1", "-DSYZYGY", "-DTEST"]
+            crafty_make = make["target=UNIX", "CC="+str(clang),
+                               "opt="+" ".join(target_opts), "crafty-make"]
+            run(crafty_make)
 
-            run(make["target=LINUX", "CC=" + str(clang), "CXX=" + str(
-                clang_cxx), "opt=" + " ".join(target_opts), "crafty-make"])
 
     def run_tests(self, experiment):
-        exp = wrap(path.join(self.src_dir, "crafty"), experiment)
-
-        run((cat[path.join(self.testdir, "test1.sh")] | exp))
-        run((cat[path.join(self.testdir, "test2.sh")] | exp))
+        with local.cwd(self.src_dir):
+            exp = wrap("./crafty", experiment)
+            run((cat[path.join(self.testdir, "test1.sh")] | exp))
+            run((cat[path.join(self.testdir, "test2.sh")] | exp))

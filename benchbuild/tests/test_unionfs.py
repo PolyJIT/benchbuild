f838dd17c32008d8c21917d8a96a00f6f5938b69
Simon Labrenz <simon.labrenz@outlook.de>
2016-11-22 17:45:43 +0100

    Updated testsuite
    Started first tests, but they do not work as intended yet.
    Therefor this commit only exists for the purpose of saving the newest
    state of the code and pushing it to a branch that i reach from other
    workstations as well.
    
    
    Former-commit-id: b3d5293718d9a6bc33f955de78e27f78ca3de36c
    Former-commit-id: c660549608ee6b7352f38710ab30b00bb69c9bef [formerly 1e97189add3bf888b98e73f1430780322a1db590] [formerly 7b5a28ecfae557f16f3ceb5b34509cfbf1649545 [formerly 0780409a9625761b2562716714a11b41a2d73550]]
    Former-commit-id: 89ba1de380fb747d74106e29691ed0639f74b0e4 [formerly 98d5d1ab42e7ab04b5780ff5722583d1a8bf2199]
    Former-commit-id: df669c5245eb8b8844b925645bef5f45647a333c

--- benchbuild/tests/test_unionfs.py
+++ benchbuild/tests/test_unionfs.py
@@ -1,6 +1,12 @@
+""" Testing suite for the mounting process. """
+
 import unittest
-from benchbuild.utils.run import unionfs
+import os
 from benchbuild.project import Project
+from benchbuild.utils.container import get_base_dir
+from benchbuild.utils.downloader import Wget
+from benchbuild.utils.run import unionfs
+from benchbuild.settings import CFG
 
 
 class TestProject(Project):
@@ -9,43 +15,56 @@ class TestProject(Project):
     The project also gets wrapped inside the unionfs.
     """
 
-    @unionfs("""Adjust base_dir, image_dir etc.""")
+    #adjust parameters in the unionfs call to test different mounts
+    @unionfs('./base', './image', None, './union')
     def mount_test_helper(self):
         """
         A plumbum or benchbuild command is called inside the wrapped unionfs and
         therefor also inside the mount as testing process.
         """
-        from benchbuild.cmd import ls #or other plumbum/benchbuild commands
+        #or any other benchbuild/plumbum command you want to run as a test
+        from benchbuild.cmd import ls
 
+        self.build()
+        self.run(ls)
+
+    @classmethod
     def download(self):
-        """Get the project that is needed and referenced."""
+        """ Get the project source input. """
+        Wget(None, NAME) #None instead of Src_url just temporary
+
+    @classmethod
+    def configure(self):
+        """ Configure the parameters of the project. """
         pass
+
+    @classmethod
+    def build(self):
+        """ Build the project. """
+        self.download()
+        self.configure()
 
 class TestUnionFsMount(unittest.TestCase):
     """
     Class to test the mounting of the unionfs with different paths and check if
     the unmounting works without working with the actual filesystem yet.
     """
-    import os
-    from benchbuild.utils.container import get_path_of_container
 
-    path = get_path_of_container()
     def test_mount_type(self):
         """Tests if the mounting process encountered incorrect types."""
         self.assertRaises(TypeError)
 
-    def test_path(self, test_path):
+    def test_path(self):
         """Tests if the mount is at the expected path."""
-        self.assertEqual(test_path, path)
+        self.assertEqual(CFG["build_dir"].value(), get_base_dir())
 
     def test_correct_unmount(self):
         """Tests if the tear down of the mount was successfull."""
-        self.assertFalse(os.path.exists(path))
+        self.assertFalse(os.path.exists(get_base_dir()))
 
     def test_correct_cleanup(self):
         """Tests if the clean up after the unmounting was successfull."""
-        pass
-
+        self.assertFalse(os.path.exists(CFG["build_dir"].value()))
 
 if __name__ == 'main':
-    unittest.main()
+    unittest.main(verbosity=2)

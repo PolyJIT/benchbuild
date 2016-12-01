0f18dccf05fa39d3aa9f3f962832175b901bd5cf
Simon Labrenz <simon.labrenz@outlook.de>
2016-12-01 15:43:18 +0100

    Adjustet tests and prepared explicit mount testing
    Further updated and adjusted the test suite, so i can work on the actual
    test of the mounting process from now on.
    
    
    Former-commit-id: 425c46cba41fe5ae83107a548ee1a4ee2f097305
    Former-commit-id: be759447b95fb0cf6b328210f7891b2bf2451c6c [formerly 28347d495f187e6f6f27d115da7c3d5590691501] [formerly 5ab02b00a9e2fbbc386f3a6c61a65f66835ff5a3 [formerly 49ecf20dbee9f6314ad05d4a9483e1742aa1f2bc]]
    Former-commit-id: bba0492571d8118223d1a20cca4923f0b9ae6440 [formerly 8c4e6193cbd721983662d13de60c1be402336ccc]
    Former-commit-id: 7fb3f376249eeef70a9c88cfe8a8fdf36407997d

--- benchbuild/tests/test_unionfs.py
+++ benchbuild/tests/test_unionfs.py
@@ -21,7 +21,9 @@ class TestProject(Project):
     def mount_test_helper(self):
         """
         A plumbum or benchbuild command is called inside the wrapped unionfs and
-        therefor also inside the mount as testing process.
+        therefor also inside the mount, added manually here.
+        The function wrapped with this helper is later on compared with the
+        actual unionfs-wrapped function as a testing process.
         """
 
         self.build()
@@ -48,39 +50,50 @@ class TestUnionFsMount(unittest.TestCase):
     Class to test the mounting of the unionfs with different paths and check if
     the unmounting works without working with a whole filesystem yet.
     """
-    from benchbuild.utils.run import uchroot_mounts, unionfs_set_up, \
-        unionfs_tear_down
 
-    build_dir = CFG["build_dir"].value()
-    base_dir = CFG["unionfs"]["base_dir"].value()
-    caller = TestProject(Project)
-    TestProject.mount_test_helper(caller)
+    def test_build_dir(self):
+        """ Check if the needed build_dir exists. """
+        self.assertTrue(os.path.exists(CFG["build_dir"].value()))
 
-    def test_unionfs_set_up(self):
-        """ Tests if the set up of the unionfs was successfull. """
-        unionfs_set_up(get_base_dir(), CFG["tmp_dir"].value(), build_dir)
-        self.assertRaises(ValueError)
+    def test_base_dir(self):
+        """ Check if the needed base_dir exsists. """
+        self.assertTrue(os.path.exists(CFG["unionfs"]["base_dir"].value()))
+
+    def test_unionfs_wrapping(self):
+        """
+        Tests if the wrap of a function inside the unionfs works as expected.
+        """
+        from benchbuild.utils.run import unionfs
+        build_dir = CFG["build_dir"].value()
+#afterwards call unionfs and wrap a function inside
+#call the mount_test_helper, who does the same manually
+#compare the two wrapped functions as assertEqual
 
     def test_mount_location(self):
         """ Tests if the mount is at the expected path. """
+        base_dir = CFG["unionfs"]["base_dir"].value()
         self.assertEqual(base_dir, get_base_dir())
 
     def test_uchroot_mounts(self):
         """ Tests if the mountpoints of the chroot could be located. """
-        expected_mountpoints = []
+        from benchbuild.utils.run import uchroot_mounts
+        build_dir = CFG["build_dir"].value()
+        expected_mountpoints = [build_dir]
         self.assertEquals(expected_mountpoints, uchroot_mounts(
-            build_dir, prefix=None))
+            None, build_dir))
 
     def test_unionfs_tear_down(self):
         """ Tests if the tear down of the unionfs was successfull. """
+        from benchbuild.utils.run import unionfs_tear_down
+        build_dir = CFG["build_dir"].value()
         unionfs_tear_down(build_dir, 3)
         self.assertRaises(ValueError)
         self.assertRaises(RuntimeError)
-#normally you would not test multiple asserts with one test,
-#but here the second one is never executed if the first one does not pass anyway
+        #the second assert is never reached if the first one fails
 
     def test_correct_cleanup(self):
         """ Tests if the clean up after the unmounting was successfull. """
+        base_dir = CFG["unionfs"]["base_dir"].value()
         self.assertFalse(os.path.exists(base_dir))
 
 if __name__ == 'main':

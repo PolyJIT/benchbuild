29ee882be944c1aadf3c8d47049c4e3f7124f5d3
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-01-11 02:06:00 +0100

    polyjit: pj-test -> new runner function
    Substitute the default run_with_time test-runner with a new one that
    executes the same binary with polyjit enabled and polyjit enabled without
    spcializer.

--- benchbuild/experiments/polyjit.py
+++ benchbuild/experiments/polyjit.py
@@ -287,6 +287,70 @@ def run_with_perf(project, experiment, config, jobs, run_f, args, **kwargs):
             persist_perf(ri.db_run, ri.session, run_f + ".svg")
             persist_config(ri.db_run, ri.session, {"cores": str(jobs)})
 
+def time_polyjit_and_polly(project, experiment, config, jobs, run_f, args,
+                           **kwargs):
+    """
+    Run the given binary wrapped with time.
+
+    Args:
+        project: The benchbuild.project.
+        experiment: The benchbuild.experiment.
+        config: The benchbuild.settings.config.
+        jobs: Number of cores we should use for this exection.
+        run_f: The file we want to execute.
+        args: List of arguments that should be passed to the wrapped binary.
+        **kwargs: Dictionary with our keyword args. We support the following
+            entries:
+
+            project_name: The real name of our project. This might not
+                be the same as the configured project name, if we got wrapped
+                with ::benchbuild.project.wrap_dynamic
+            has_stdin: Signals whether we should take care of stdin.
+    """
+    from benchbuild.utils.run import guarded_exec, fetch_time_output
+    from benchbuild.settings import CFG as c
+    from benchbuild.utils.db import persist_time, persist_config
+
+    c.update(config)
+    project.name = kwargs.get("project_name", project.name)
+    timing_tag = "BB-JIT: "
+
+    may_wrap = kwargs.get("may_wrap", True)
+
+    run_cmd = local[run_f]
+    run_cmd = run_cmd[args]
+    if may_wrap:
+        run_cmd = time["-f", timing_tag + "%U-%S-%e", run_cmd]
+    
+    def handle_timing_info(ri):
+        if may_wrap:
+            timings = fetch_time_output(
+                timing_tag, timing_tag + "{:g}-{:g}-{:g}", ri.stderr.split("\n"))
+            if timings:
+                persist_time(ri.db_run, ri.session, timings)
+
+
+    with local.env(OMP_NUM_THREADS=str(jobs),
+                   POLLI_LOG_FILE=c["slurm"]["extra_log"].value()):
+        with guarded_exec(run_cmd, project, experiment) as run:
+            ri = run()
+        handle_timing_info(ri)
+    persist_config(ri.db_run, ri.session, {"cores": str(jobs-1),
+                                           "cores-config": str(jobs),
+                                           "recompilation": "enabled",
+                                           "specialization": "enabled"})
+
+    with local.env(OMP_NUM_THREADS=str(jobs),
+                   POLLI_DISABLE_SPECIALIZATION=1,
+                   POLLI_LOG_FILE=c["slurm"]["extra_log"].value()):
+        with guarded_exec(run_cmd, project, experiment) as run:
+            ri = run()
+        handle_timing_info(ri)
+    persist_config(ri.db_run, ri.session, {"cores": str(jobs-1),
+                                           "cores-config": str(jobs),
+                                           "recompilation": "enabled",
+                                           "specialization": "disabled"})
+    return ri
 
 class PolyJIT(RuntimeExperiment):
     """The polyjit experiment."""
@@ -411,7 +475,8 @@ class PJIT_Test(PolyJIT):
         p.run_uuid = uuid.uuid4()
         jobs = int(CFG["jobs"].value())
         p.cflags += ["-mllvm", "-polly-num-threads={0}".format(jobs)]
-        p.runtime_extension = partial(run_with_time, p, self, CFG, jobs)
+        p.runtime_extension = partial(time_polyjit_and_polly,
+                                      p, self, CFG, jobs)
 
         actns.extend([
             MakeBuildDir(p),

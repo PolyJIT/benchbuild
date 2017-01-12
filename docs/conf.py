4e98ab9c9e7cc8f5bd2543df1003247b972304de
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-01-12 10:35:58 +0100

    docs: enable napoleon for documentation
    Former-commit-id: cdbc224ff1baca2ce155acc7a772920bc6c095b3
    Former-commit-id: 0b1f9898fec2cd0e442c1c9d7b403af02678aeaa [formerly dad6131517df439df4651e6ab67f9894358302eb] [formerly fb087f88ab2e193a9faf20819bdc85f3f85c53df [formerly ad6528713eaf3192e40b748fa1e88a84106a8fb1]]
    Former-commit-id: fcf1771b7013528938b2b2445084635f025b0307 [formerly 98964e6f28663e0af410c00269f6be4f7f9461dc]
    Former-commit-id: e8474404cb695c1ee4e9b3630e619fef8ad8ab42

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

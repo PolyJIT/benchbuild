620c30531e1dbcd86d76b1f71360234fdc823b99
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-01-17 18:36:00 +0100

    sql: add sql scripts for expected functions

--- setup.py
+++ setup.py
@@ -9,13 +9,28 @@ extra_files = [
     "templates/slurm-cleanup-node.sh.inc"
 ]
 
+sql_extra_files = [
+    "func.compare_region_wise2.sql",
+    "func.experiments.sql",
+    "func.recompilation.sql",
+    "func.run_regions.sql",
+    "func.total_dyncov_clean.sql",
+    "func.total_speedup.sql",
+    "func.compare_region_wise.sql",
+    "func.project_region_time.sql",
+    "func.run_durations.sql",
+    "func.speedup.sql",
+    "func.total_dyncov.sql"
+]
+
 setup(
     name='benchbuild',
     version='1.3.2',
     url='https://github.com/PolyJIT/benchbuild',
     packages=find_packages(exclude=["docs", "extern", "filters", "linker",
                                     "src", "statistics", "tests", "results"]),
-    package_data={"benchbuild.utils": extra_files},
+    package_data={"benchbuild.utils": extra_files,
+                  "benchbuild": sql_extra_files},
     install_requires=[
         "lazy==1.2", "SQLAlchemy==1.0.4", "dill==0.2.4", "plumbum>=1.5.0",
         "regex==2015.5.28", "wheel==0.24.0", "parse==1.6.6",

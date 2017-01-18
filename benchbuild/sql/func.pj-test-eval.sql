3183b096ded06e4afe0430d9d4b1310615226d1b
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-01-18 13:27:37 +0100

    sql: new function - pj-test-eval
    Adaption of PLDI-Evaluation function for pj-test.
    It is still buggy, don't use it.
    
    
    Former-commit-id: 41b63f9bedd30b275956e2d77d6d6b81a70c35fa
    Former-commit-id: dfd320960a7420b0939d0bef5f1c129b05dbbbce [formerly 0b3ba7c6eb63adf0cba9241a5f92c062e7ea66dc] [formerly 13cf685f652ef084cb1c27606815ed31713b906a [formerly f0f5329af15ed70589619ae0b7af078dd83cbe94]]
    Former-commit-id: 678e584ea0100d14d12395ed348df5e2781410d3 [formerly 487244e04c345691bdd76d0b0f3bc2f10cd3ad5b]
    Former-commit-id: e93ba1506a5965c0323b2a95a0a21c3d88957eea

---
+++ benchbuild/sql/func.pj-test-eval.sql
@@ -0,0 +1,155 @@
+DROP FUNCTION IF EXISTS recompilation(exp_ids UUID[], spec_status VARCHAR);
+CREATE OR REPLACE FUNCTION specialization(exp_ids UUID[], spec_status VARCHAR)
+  returns table(name VARCHAR, value VARCHAR, run_id INTEGER) as $specialization$
+BEGIN
+  RETURN QUERY
+    SELECT config.name,
+           config.value,
+           config.run_id
+    FROM config, run WHERE
+        run.id = config.run_id AND
+        run.status = 'completed' AND
+        config.name = 'specialization' AND
+        config.value = spec_status AND
+        run.experiment_group = ANY (exp_ids);
+END;
+$specialization$ language plpgsql;
+
+DROP FUNCTION IF EXISTS pj_test_project_region_time(region_name VARCHAR, exp_ids UUID[], spec_status VARCHAR);
+CREATE OR REPLACE FUNCTION pj_test_project_region_time(region_name VARCHAR, exp_ids UUID [], spec_status VARCHAR)
+  RETURNS
+    TABLE(project_name VARCHAR,
+          duration     NUMERIC)
+AS $BODY$
+BEGIN
+  RETURN QUERY
+  SELECT
+    run.project_name,
+    sum(regions.duration)
+  FROM run, config, regions,
+        specialization((exp_ids), spec_status) AS spec
+  WHERE
+    run.id = config.run_id AND run.id = regions.run_id AND
+    run.experiment_group = ANY (exp_ids) AND
+    run.id = spec.run_id AND
+    regions.name = region_name
+  GROUP BY
+    run.project_name;
+END
+$BODY$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS pj_test_total_dyncov(exp_ids UUID []);
+CREATE OR REPLACE FUNCTION pj_test_total_dyncov(exp_ids UUID [])
+  RETURNS
+    TABLE(project   VARCHAR,
+          total     NUMERIC,
+          scops     NUMERIC,
+          overhead  NUMERIC,
+          ohcov_0   NUMERIC,
+          ohcov_1   NUMERIC,
+          dyncov    NUMERIC,
+          cachehits NUMERIC,
+          variants  NUMERIC,
+          t_0       NUMERIC,
+          t_1       NUMERIC,
+          o_1       NUMERIC)
+AS $BODY$
+BEGIN
+  RETURN QUERY
+  SELECT
+    total.project_name,
+    total.sum                                     AS total,
+    scops.sum                                     AS scops,
+    codegen.sum                                   AS overhead,
+    (codegen.sum / total.sum * 100)               AS ohcov_0,
+    (O_1.duration / T_1.duration * 100)           AS ohcov_1,
+    (scops.sum / (total.sum - codegen.sum) * 100) AS dyncov,
+    ch.sum                                        AS cachehits,
+    variants.sum                                  AS variants,
+    total.sum - codegen.sum                       AS T_0,
+    T_1.duration                                  AS t_1,
+    O_1.duration                                  AS o_1
+  FROM
+    (
+      SELECT
+        run.project_name,
+        sum(regions.duration)
+      FROM run, config, regions,
+            specialization(exp_ids, 'enabled') AS recomp
+      WHERE
+        run.id = config.run_id AND run.id = regions.run_id AND
+        run.experiment_group = ANY (exp_ids) AND
+        config.name = 'cores' AND
+        config.value = '3' AND
+        run.id = recomp.run_id AND
+        regions.name = 'START'
+      GROUP BY
+        run.project_name
+    ) AS total
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      specialization(exp_ids, 'enabled') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'CODEGEN'
+                GROUP BY
+                  run.project_name
+              ) AS codegen ON (codegen.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      specialization(exp_ids, 'enabled') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'CACHE_HIT'
+                GROUP BY
+                  run.project_name
+              ) AS ch ON (ch.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      specialization(exp_ids, 'enabled') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'VARIANTS'
+                GROUP BY
+                  run.project_name
+              ) AS variants ON (variants.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      specialization((exp_ids), 'disabled') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name != ALL ('{START, CODEGEN, VARIANTS, CACHE_HIT}' :: VARCHAR [])
+                GROUP BY
+                  run.project_name
+              ) AS scops ON (scops.project_name = total.project_name)
+    LEFT JOIN
+    (SELECT *
+     FROM pj_test_project_region_time('START', (exp_ids), 'enabled')) AS T_1
+      ON (T_1.project_name = total.project_name)
+    LEFT JOIN
+    (SELECT *
+     FROM pj_test_project_region_time('CODEGEN', (exp_ids), 'enabled')) AS O_1
+      ON (O_1.project_name = total.project_name)
+  WHERE TRUE;
+END
+$BODY$ LANGUAGE plpgsql;

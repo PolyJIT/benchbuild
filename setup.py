e101e492116637f39867fc32aa9c8dd111ff617b
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-01-21 01:36:00 +0100

    add forgotten requirements to setup.py
    Former-commit-id: 7a9da604567a41232cb7bdea2ea4cc4c953042e0
    Former-commit-id: 066430cbb2faea9373164cc25b08e04aef6a18c2 [formerly ab870fcdaea656575e1fdd21860f72b3ffb2b4c3] [formerly 2f1ea5062218624ce231b6b7dad9c4d9b25ffef7 [formerly 1607cae8a22e3e48b6cc2dd9bace2be29cd43820]]
    Former-commit-id: e024529916a5a14f490a7cde8cfc7ea1080b0c47 [formerly bf9854177ca7c6ba39b0dfd170c44a77b397f7d6]
    Former-commit-id: 69586268dc3aee73567cc45c93a79add85e29550

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ setup(
         "lazy==1.2", "SQLAlchemy==1.0.4", "dill==0.2.4", "plumbum>=1.5.0",
         "regex==2015.5.28", "wheel==0.24.0", "parse==1.6.6",
         "virtualenv==13.1.0", "sphinxcontrib-napoleon", "psycopg2",
-        "sqlalchemy-migrate", "six>=1.7.0", "psutil>=4.0.0", "pylint>=1.5.5"
+        "sqlalchemy-migrate", "six>=1.7.0", "psutil>=4.0.0", "pylint>=1.5.5",
+        "seaborn>=0.7.1", "pandas>=0.19.2", "matplotlib>=2.0"
     ],
     author="Andreas Simbuerger",
     author_email="simbuerg@fim.uni-passau.de",

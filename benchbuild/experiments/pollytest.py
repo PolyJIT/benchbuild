f845dccdfef3effea195d1d331c2b1c0689c30aa
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-06-14 15:32:21 +0200

    extensions: add extension support to benchbuild
    This provides a sane API for RunTime and CompileTime extensions.

--- benchbuild/experiments/pollytest.py
+++ benchbuild/experiments/pollytest.py
@@ -11,6 +11,7 @@ import uuid
 from functools import partial
 from plumbum import local
 
+from benchbuild.extensions import RunWithTime
 from benchbuild.experiment import Experiment
 from benchbuild.settings import CFG
 from benchbuild.utils.actions import (Build, Clean, Configure, Download, Echo,
@@ -20,53 +21,6 @@ from benchbuild.utils.db import (persist_compilestats, persist_config,
 from benchbuild.utils.run import track_execution, handle_stdin
 LOG = logging.getLogger()
 
-
-def timing_extension(project, experiment, config, run_f, *args, **kwargs):
-    """
-    Collect the time measurement.
-
-    Args:
-        project: The benchbuild project that called for this measurement.
-        experiment: The benchbuild experiment this function is operating under.
-        config: The benchbuild configuration used for the current run.
-        run_f: The file that gets executed.
-        *args: List of arguments that should be passed to the wrapped binary.
-        **kwargs: Dictionary with further keyword arguments. The following
-            entries are supported:
-            project_name: The real name of the project, that might differ from
-                the configured one.
-            may_wrap: Signals if a project is suitable for wrapping.
-    """
-    from benchbuild.utils.cmd import time
-    from benchbuild.utils.run import fetch_time_output
-    CFG.update(config)
-    project.name = kwargs.get("project_name", project.name)
-    may_wrap = kwargs.get("may_wrap", True)
-    run_config = kwargs.get("run_config", None)
-    timing_tag = "BB-Time: "
-
-    run_cmd = local[run_f]
-    run_cmd = run_cmd[args]
-    if may_wrap:
-        run_cmd = time["-f", timing_tag + "%U-%S-%e", run_cmd]
-
-    def handle_timing(run_info):
-        """Takes care of the formating for the timing statistics."""
-        if may_wrap:
-            timings = fetch_time_output(
-                timing_tag, timing_tag + "{:g}-{:g}-{:g}",
-                run_info.stderr.split("\n"))
-            if timings:
-                persist_time(run_info.db_run, run_info.session, timings)
-            else:
-                LOG.info("No timing information found.")
-        return run_info
-
-    with track_execution(run_cmd, project, experiment, **kwargs) as run:
-        run_info = handle_timing(run())
-        if run_config is not None:
-            persist_config(run_info.db_run, run_info.session, run_config)
-    return run_info
 
 def compilestats_ext(project, experiment, config, clang, **kwargs):
     """
@@ -146,8 +100,7 @@ class PollyTest(Experiment):
         cfg = {
             "cflags": "-O3"
         }
-        newp.runtime_extension = partial(timing_extension, p, self, CFG,
-                                         run_config=cfg)
+        newp.runtime_extension = RunWithTime(p, self, config=cfg)
         newp.compiler_extension = partial(compilestats_ext, p, self, CFG,
                                           run_config=cfg)
         actns.extend(actions(newp))
@@ -158,8 +111,7 @@ class PollyTest(Experiment):
         cfg = {
             "cflags": "-O3 -polly"
         }
-        newp.runtime_extension = partial(timing_extension, p, self, CFG,
-                                         run_config=cfg)
+        newp.runtime_extension = RunWithTime(p, self, config=cfg)
         newp.compiler_extension = partial(compilestats_ext, p, self, CFG,
                                           run_config=cfg)
         actns.extend(actions(newp))
@@ -173,8 +125,7 @@ class PollyTest(Experiment):
         cfg = {
             "cflags": "-O3 -polly -polly-position=before-vectorizer"
         }
-        newp.runtime_extension = partial(timing_extension, p, self, CFG,
-                                         run_config=cfg)
+        newp.runtime_extension = RunWithTime(p, self, config=cfg)
         newp.compiler_extension = partial(compilestats_ext, p, self, CFG,
                                           run_config=cfg)
         actns.extend(actions(newp))
@@ -190,8 +141,7 @@ class PollyTest(Experiment):
             "cflags": "-O3 -polly -polly-position=before-vectorizer "
                       "-polly-process-unprofitable"
         }
-        newp.runtime_extension = partial(timing_extension, p, self, CFG,
-                                         run_config=cfg)
+        newp.runtime_extension = RunWithTime(p, self, config=cfg)
         newp.compiler_extension = partial(compilestats_ext, p, self, CFG,
                                           run_config=cfg)
         actns.extend(actions(newp))
@@ -205,8 +155,7 @@ class PollyTest(Experiment):
         cfg = {
             "cflags": "-O3 -polly -polly-process-unprofitable"
         }
-        newp.runtime_extension = partial(timing_extension, p, self, CFG,
-                                         run_config=cfg)
+        newp.runtime_extension = RunWithTime(p, self, config=cfg)
         newp.compiler_extension = partial(compilestats_ext, p, self, CFG,
                                           run_config=cfg)
         actns.extend(actions(newp))

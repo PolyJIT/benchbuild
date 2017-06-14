60baab87d33d83efdadeabe3a41697e42a2ecdbe
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-06-14 20:34:11 +0200

    extract inner class
    Former-commit-id: 17833be9df2f9689d22ff1a650369c08f50da65e
    Former-commit-id: 3ae942319354169552a3a439a191ac481598c705 [formerly c20e23859d58e083c74b14b59489858ec18bf5dc] [formerly 911a9735245281e88e33aeefdadcddad8484d45d [formerly 936d93712404d7ca276c587523f42dffbee06710]]
    Former-commit-id: 015026c1255f9ed7f86f9425c88fa0019d400c7c [formerly 3a0d0b398e2e89850be90de4dc7d96581ff094c2]
    Former-commit-id: c5504afd99f53fda7aa96cec29b1f3c38cc8a7be

--- benchbuild/projects/gentoo/portage_gen.py
+++ benchbuild/projects/gentoo/portage_gen.py
@@ -8,6 +8,63 @@ from benchbuild.utils.run import run, uchroot_no_args
 from benchbuild.utils.container import Gentoo
 from plumbum import local, ProcessExecutionError
 
+#pylint: disable=too-few-public-methods
+class FuncClass(object):
+
+    """
+    Finds out the current version number of a gentoo package.
+
+    The package name is created by combining the domain and the name.
+    Then uchroot is used to switch into a gentoo shell where the 'emerge'
+    command is used to recieve the version number.
+    The function then parses the version number back into the file.
+
+    Args:
+        Name: Name of the project.
+        Domain: Categorie of the package.
+    """
+
+    def __init__(self, name, domain, container):
+        self.name = name
+        self.domain = domain
+        self.container = container
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        try:
+            domain, _, name = self.name.partition("_")
+            package = domain + '/' + name
+            container = self.container()
+
+            uchroot = uchroot_no_args()
+            uchroot = uchroot["-E", "-A", "-C", "-w", "/", "-r"]
+            uchroot = uchroot[container.local]
+            with local.env(CONFIG_PROTECT="-*"):
+                fake_emerge = uchroot["emerge",
+                                      "--autounmask-only=y",
+                                      "--autounmask-write=y",
+                                      "--nodeps"]
+                run(fake_emerge[package])
+
+            emerge_in_chroot = uchroot["emerge",
+                                       "-p",
+                                       "--nodeps",
+                                       package]
+            _, stdout, _ = emerge_in_chroot.run()
+
+            for line in stdout.split('\n'):
+                if package in line:
+                    _, _, package_name = line.partition("/")
+                    _, name, version = package_name.partition(name)
+                    version, _, _ = version.partition(" ")
+                    return version[1:]
+        except ProcessExecutionError:
+            logger = logging.getLogger(__name__)
+            logger.info("This older package might not exist any more.")
+        return "Default"
+
 
 def PortageFactory(name, NAME, DOMAIN, BaseClass=autoportage.AutoPortage):
     """
@@ -42,63 +99,6 @@ def PortageFactory(name, NAME, DOMAIN, BaseClass=autoportage.AutoPortage):
         >>> i.DOMAIN
         'DOMAIN'
     """
-
-    #pylint: disable=too-few-public-methods
-    class FuncClass(object):
-
-        """
-        Finds out the current version number of a gentoo package.
-
-        The package name is created by combining the domain and the name.
-        Then uchroot is used to switch into a gentoo shell where the 'emerge'
-        command is used to recieve the version number.
-        The function then parses the version number back into the file.
-
-        Args:
-            Name: Name of the project.
-            Domain: Categorie of the package.
-        """
-
-        def __init__(self, name, domain, container):
-            self.name = name
-            self.domain = domain
-            self.container = container
-
-        def __repr__(self):
-            return self.__str__()
-
-        def __str__(self):
-            try:
-                domain, _, name = self.name.partition("_")
-                package = domain + '/' + name
-                container = self.container()
-
-                uchroot = uchroot_no_args()
-                uchroot = uchroot["-E", "-A", "-C", "-w", "/", "-r"]
-                uchroot = uchroot[container.local]
-                with local.env(CONFIG_PROTECT="-*"):
-                    fake_emerge = uchroot["emerge",
-                                          "--autounmask-only=y",
-                                          "--autounmask-write=y",
-                                          "--nodeps"]
-                    run(fake_emerge[package])
-
-                emerge_in_chroot = uchroot["emerge",
-                                           "-p",
-                                           "--nodeps",
-                                           package]
-                _, stdout, _ = emerge_in_chroot.run()
-
-                for line in stdout.split('\n'):
-                    if package in line:
-                        _, _, package_name = line.partition("/")
-                        _, name, version = package_name.partition(name)
-                        version, _, _ = version.partition(" ")
-                        return version[1:]
-            except ProcessExecutionError:
-                logger = logging.getLogger(__name__)
-                logger.info("This older package might not exist any more.")
-            return "Default"
 
     def run_not_supported(self, *args, **kwargs):  # pylint: disable=W0613
         """Dynamic projects don't support a run() test."""

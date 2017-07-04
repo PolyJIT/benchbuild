e5699fabbde73132807992090452f280a62759ab
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-07-04 17:36:40 +0200

    dict: add a new (extensible) dictionary
    Our ExtensibleDict can be used similar to plumbum's context managers
    that modify the environment of a program (stack-like).
    However, we can append/extend the entries of the environment in contrast
    to plumbum's 'overwriting' functionality. This enables us to expand the configuration of a program step-by-step and construct the final configuration
    as late as possible.

---
+++ benchbuild/utils/dict.py
@@ -0,0 +1,104 @@
+"""
+An extensible dictionary.
+"""
+from contextlib import contextmanager
+
+
+def extend_as_list(original_dict, **kwargs):
+    new_dict = original_dict
+    for k, v in kwargs.items():
+        if k in original_dict:
+            oldv = new_dict[k]
+            if not hasattr(oldv, '__iter__'):
+                oldv = [oldv]
+            if hasattr(v, '__iter__'):
+                oldv.extend(v)
+            else:
+                oldv.append(v)
+            new_dict[k] = oldv
+    return new_dict
+
+class ExtensibleDict(object):
+    """A dictionary that provides temporary modification."""
+
+    _current = dict()
+
+
+    @contextmanager
+    def __call__(self, *args, extender_fn=None, **kwargs):
+        """
+        A context manager to temporarily modify the content of dict.
+
+        Any time you enter the context the existing dictionary is updated with
+        the content of **kwargs. When the context exits the original dictionary
+        is restored.
+
+        If ``extender_fn`` is not None the existing content of the dictionary
+        is not overwritten, but handed to the extender_fn as first argument in
+        addition to the changes supplied in the ``kwargs``.
+
+        The result will be stored temporarily int he dictionary.
+
+        Args:
+            extender_fn:
+        Returns:
+            An updated dictionary.
+        """
+        previous = self._current.copy()
+        self.update(extender_fn, **kwargs)
+        try:
+            yield
+        finally:
+            self._current = previous
+
+    def __iter__(self):
+        return iter(self._current.items())
+
+    def __len__(self):
+        return len(self._current)
+
+    def __contains__(self, name):
+        return name in self._current
+
+    def __getitem__(self, name):
+        return self._current[name]
+
+    def keys(self):
+        return self._current.keys()
+
+    def values(self):
+        return self._current.values()
+
+    def items(self):
+        return self._current.items()
+
+    def get(self, name, *default):
+        return self._current.get(name, *default)
+
+    def __delitem__(self, name):
+        del self._current[name]
+
+    def __setitem__(self, name, value):
+        self._current[name] = value
+
+    def pop(self, name, *default):
+        return self._current.pop(name, *default)
+
+    def clear(self):
+        self._current.clear()
+
+    def update(self, extender_fn, *args, **kwargs):
+        if extender_fn is not None:
+            self._current.update(*args,
+                **extender_fn(self._current, **kwargs))
+        else:
+            self._current.update(*args, **kwargs)
+
+    def getdict(self):
+        return dict((k, str(v)) for k,v in self._current.items())
+
+    def __str__(self):
+        return str(self._current)
+
+    def __repr__(self):
+        return repr(self._current)

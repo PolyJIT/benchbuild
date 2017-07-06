e2c9194c510fdc00043127c34bccefe62b99c780
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-07-06 10:38:00 +0200

    NFC: Refactor polyjit - Extract out likwid action
    Former-commit-id: c9dd4f3f24699a307c39d71e906fd38dbd827c22
    Former-commit-id: 21f1174e1c5d4f15596b86862ef14662e593dda2 [formerly 0571e9519fc1b3f394df45813a0f7bcb2ba45741] [formerly fb3adfb6049f45f6a2a533652109921476e8a7f0 [formerly 6f98d19e9b03c4710e0fb665d3f9d078c4fa27d9]]
    Former-commit-id: 3911f5b6bcf7d14daf236da0dbf8d2ce63f956c1 [formerly a8c3e2800fe162bf9f277f95e580e16ede395294]
    Former-commit-id: d028d243ede75975c3e6623505628b1c636f1fc1

--- benchbuild/experiments/pj_likwid.py
+++ benchbuild/experiments/pj_likwid.py
@@ -5,6 +5,60 @@ import benchbuild.experiments.polyjit as pj
 
 from benchbuild.utils.actions import RequireAll
 
+
+class RunWithLikwid(ext.RuntimeExtension):
+    """
+    Run the given file wrapped by likwid.
+
+    Args:
+        project: The benchbuild.project.
+        experiment: The benchbuild.experiment.
+        config: The benchbuild.settings.config.
+        jobs: Number of cores we should use for this exection.
+        run_f: The file we want to execute.
+        args: List of arguments that should be passed to the wrapped binary.
+        **kwargs: Dictionary with our keyword args. We support the following
+            entries:
+
+            project_name: The real name of our project. This might not
+                be the same as the configured project name, if we got wrapped
+                with ::benchbuild.project.wrap_dynamic
+            has_stdin: Signals whether we should take care of stdin.
+    """
+
+    def __call__(self, binary_command, *args, may_wrap=True, **kwargs):
+        from benchbuild.settings import CFG
+        from benchbuild.utils.db import persist_likwid, persist_config
+        from benchbuild.likwid import get_likwid_perfctr
+        from benchbuild.utils.cmd import rm
+
+        self.project.name = kwargs.get("project_name", self.project.name)
+
+        likwid_f = self.project.name + ".txt"
+        likwid_path = os.path.join(CFG["likwiddir"], "bin")
+        likwid_perfctr = local[os.path.join(likwid_path, "likwid-perfctr")]
+
+        jobs = self.config['jobs']
+        for group in ["CLOCK"]:
+            run_cmd = \
+                likwid_perfctr["-O", "-o", likwid_f, "-m",
+                               "-C", "0-{0:d}".format(jobs),
+                               "-g", group, binary_command]
+
+            res = []
+            with local.env(POLLI_ENABLE_LIKWID=1):
+                res = self.call_next(run_cmd, *args, **kwargs)
+
+            likwid_measurement = get_likwid_perfctr(likwid_f)
+            for run_info in res:
+                persist_likwid(run_info.db_run, run_info.session, likwid_measurement)
+                persist_config(run_info.db_run, run_info.session, {
+                    "cores": str(jobs),
+                    "likwid.group": group
+                })
+            rm("-f", likwid_f)
+
+
 class PJITlikwid(pj.PolyJIT):
     """
     An experiment that uses likwid's instrumentation API for profiling.
@@ -28,7 +82,7 @@ class PJITlikwid(pj.PolyJIT):
             cp = copy.deepcopy(p)
             cp.run_uuid = uuid.uuid4()
             cp.runtime_extension = \
-                pj.RunWithLikwid(
+                RunWithLikwid(
                     cp, self,
                     ext.RuntimeExtension(cp, self, config={'jobs': i}),
                     config={'jobs': i})

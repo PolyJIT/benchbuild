be4fc2ce8a8dac39d8fc3b157083cd3bbda585f8
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-07-11 13:44:01 +0200

    test: add a project that always fails at run-time
    Former-commit-id: 974c6b31ecb66883807dfe0c0b670ed944274cfd
    Former-commit-id: 3a9f4fda6efe731b87106d53163d2ba3264f6629 [formerly f1436273bee1761d1e9b5f582334c541a300a15c] [formerly 4adb2432bbd6d013ec9a328b772b2971fb53ae7e [formerly eb93ecd19d11d1694e33332eae813135e7052d0d]]
    Former-commit-id: 11b010ba2bda3e690dd55641e7f61eaad1edbd8f [formerly 3a3a4a286b3ab4aa0cc2b090894f20712f602de8]
    Former-commit-id: 9bea369299b59ab9cc2176acf239056648466a4b

--- benchbuild/projects/test/test.py
+++ benchbuild/projects/test/test.py
@@ -4,9 +4,7 @@ import benchbuild.utils.run as run
 import benchbuild.utils.wrapping as wrapping
 
 class TestProject(prj.Project):
-    """
-        Test project that does nothing.
-    """
+    """Test project that does nothing."""
     NAME = "test"
     DOMAIN = "test"
     GROUP = "test"
@@ -21,6 +19,39 @@ int main(int argc, char **argv) {
     std::cout << "Hello (stdout) World" << std::endl;    
     std::cerr << "Hello (stderr) World" << std::endl;
     return 0;
+}
+            """
+            test_source.write(lines)
+
+    def configure(self):
+        pass
+
+    def build(self):
+        clang = compiler.lt_clang_cxx(self.cflags, self.ldflags,
+                                      self.compiler_extension)
+        run.run(clang[self.src_file, "-o", self.src_file + ".out"])
+
+    def run_tests(self, experiment, run):
+        exp = wrapping.wrap(self.src_file + ".out", experiment)
+        run(exp)
+
+class TestProjectRuntimeFail(prj.Project):
+    """Test project that _always_ fails at runtime."""
+
+    NAME = "test-fail"
+    DOMAIN = "test"
+    GROUP = "test"
+    VERSION = "1.0"
+    SRC_FILE = "test.cpp"
+
+    def prepare(self):
+        with open(self.src_file, 'w') as test_source:
+            lines = """
+#include <iostream>
+int main(int argc, char **argv) {
+    std::cout << "Hello (stdout) World" << std::endl;
+    std::cerr << "Hello (stderr) World" << std::endl;
+    return 1;
 }
             """
             test_source.write(lines)

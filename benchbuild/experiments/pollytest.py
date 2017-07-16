cd04c6b97823944e114e1327e1015025226ed7ef
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-07-16 22:37:44 +0200

    pollytest: add eval scripts & report

--- benchbuild/experiments/pollytest.py
+++ benchbuild/experiments/pollytest.py
@@ -6,16 +6,20 @@ time's behavior regarding the position of polly and unprofitable processes.
 """
 import copy
 import logging
+import os
+import pandas as pd
 import uuid
 
-from benchbuild.extensions import (RunWithTime, RuntimeExtension,
-                                   ExtractCompileStats)
-from benchbuild.experiment import Experiment
+import sqlalchemy as sa
+import benchbuild.experiment as exp
+import benchbuild.extensions as ext
+import benchbuild.reports as report
+
 
 LOG = logging.getLogger(__name__)
 
 
-class PollyTest(Experiment):
+class PollyTest(exp.Experiment):
     """
     An experiment that executes projects with different configurations.
 
@@ -33,7 +37,8 @@ class PollyTest(Experiment):
         cfg = {
             "name": "-O3"
         }
-        newp.compiler_extension = ExtractCompileStats(newp, self, config=cfg)
+        newp.compiler_extension = \
+            ext.ExtractCompileStats(newp, self, config=cfg)
         actns.extend(self.default_compiletime_actions(newp))
 
         newp = copy.deepcopy(project)
@@ -42,7 +47,8 @@ class PollyTest(Experiment):
         cfg = {
             "name": "-O3 -polly"
         }
-        newp.compiler_extension = ExtractCompileStats(newp, self, config=cfg)
+        newp.compiler_extension = \
+            ext.ExtractCompileStats(newp, self, config=cfg)
         actns.extend(self.default_compiletime_actions(newp))
 
         newp = copy.deepcopy(project)
@@ -54,7 +60,8 @@ class PollyTest(Experiment):
         cfg = {
             "name": "-O3 -polly -polly-position=before-vectorizer"
         }
-        newp.compiler_extension = ExtractCompileStats(newp, self, config=cfg)
+        newp.compiler_extension = \
+            ext.ExtractCompileStats(newp, self, config=cfg)
         actns.extend(self.default_compiletime_actions(newp))
 
         newp = copy.deepcopy(project)
@@ -68,7 +75,8 @@ class PollyTest(Experiment):
             "name": "-O3 -polly -polly-position=before-vectorizer "
                     "-polly-process-unprofitable"
         }
-        newp.compiler_extension = ExtractCompileStats(newp, self, config=cfg)
+        newp.compiler_extension = \
+            ext.ExtractCompileStats(newp, self, config=cfg)
         actns.extend(self.default_compiletime_actions(newp))
 
         newp = copy.deepcopy(project)
@@ -80,6 +88,40 @@ class PollyTest(Experiment):
         cfg = {
             "name": "-O3 -polly -polly-process-unprofitable"
         }
-        newp.compiler_extension = ExtractCompileStats(newp, self, config=cfg)
+        newp.compiler_extension = \
+            ext.ExtractCompileStats(newp, self, config=cfg)
         actns.extend(self.default_compiletime_actions(newp))
         return actns
+
+
+class PollyTestReport(report.Report):
+    SUPPORTED_EXPERIMENTS = ["pollytest"]
+    QUERY_EVAL = \
+        sa.sql.select([
+        ]).\
+        select_from(
+            sa.func.pollytest_eval(
+                sa.sql.bindparam('exp_ids'),
+                sa.sql.bindparam('components')))
+
+    def report(self):
+        print("I found the following matching experiment ids")
+        print("  \n".join([str(x) for x in self.experiment_ids]))
+
+        qry = PollyTestReport.\
+            QUERY_EVAL.unique_params(
+                exp_ids=self.experiment_ids,
+                components=["polly-scops", "polly-detect", "polly"]
+            )
+
+        df = pd.read_sql_query(qry, self.session.bind)
+        print(df.head())
+
+        return df
+
+    def generate(self):
+        fname = os.path.basename(self.out_path)
+        fname = "{prefix}_pollytest{ending}".format(
+            prefix=os.path.splitext(fname)[0],
+            ending=os.path.splitext(fname)[-1])
+        self.report()
---
+++ benchbuild/sql/func.compilestats_eval.sql
@@ -0,0 +1,83 @@
+DROP FUNCTION IF EXISTS cs_config (exp_ids UUID[], cs_config_str VARCHAR);
+CREATE OR REPLACE FUNCTION cs_config(exp_ids UUID[], cs_config_str VARCHAR)
+  returns table(name VARCHAR, value VARCHAR, run_id INTEGER) as $BODY$
+BEGIN
+  RETURN QUERY
+    SELECT config.name,
+           config.value,
+           config.run_id
+    FROM config, run WHERE
+        run.id = config.run_id AND
+        run.status = 'completed' AND
+        config.name = 'name' AND
+        config.value = cs_config_str AND
+        run.experiment_group = ANY (exp_ids);
+END;
+$BODY$ language plpgsql;
+
+DROP FUNCTION IF EXISTS cs_component_vals_per_config(exp_ids UUID[], cs_components VARCHAR[], config VARCHAR);
+CREATE OR REPLACE FUNCTION cs_component_vals_per_config(exp_ids UUID[], cs_components VARCHAR[], config VARCHAR)
+  returns table(project VARCHAR, variable TEXT, value NUMERIC) as $BODY$
+BEGIN
+  RETURN QUERY
+    SELECT
+      run.project_name AS project,
+      compilestats.name AS variable,
+      SUM(compilestats.value) AS value
+    FROM
+      run, compilestats, cs_config(exp_ids, config) AS cfg
+    WHERE
+      run.id = compilestats.run_id AND
+      run.experiment_group = ANY (exp_ids) AND
+      compilestats.component = ANY (cs_components) AND
+      run.id = cfg.run_id
+    GROUP BY
+      run.project_name,
+      compilestats.name;
+END;
+$BODY$ language plpgsql;
+
+DROP FUNCTION IF EXISTS pollytest_eval(exp_ids UUID[], components VARCHAR[]);
+CREATE OR REPLACE FUNCTION pollytest_eval(exp_ids UUID[], components VARCHAR[])
+  RETURNS
+    TABLE(
+      project VARCHAR,
+      component TEXT,
+      p     NUMERIC,
+      pv    NUMERIC,
+      pvu   NUMERIC,
+      pu    NUMERIC
+    )
+AS $BODY$
+BEGIN
+  RETURN QUERY
+  SELECT
+    P1.project AS project,
+    P1.variable AS component,
+
+    P1.value AS p,
+    P2.value AS pv,
+    P3.value AS pvu,
+    P4.value AS pu
+  FROM
+              cs_component_vals_per_config(exp_ids, components,
+                '-O3 -polly') as P1
+    LEFT JOIN
+              cs_component_vals_per_config(exp_ids, components,
+                '-O3 -polly -polly-position=before-vectorizer') as P2
+              ON (P2.project = P1.project AND P2.variable = P1.variable)
+    LEFT JOIN
+              cs_component_vals_per_config(exp_ids, components,
+                '-O3 -polly -polly-position=before-vectorizer -polly-process-unprofitable') as P3
+              ON (P3.project = P1.project AND P3.variable = P1.variable)
+    LEFT JOIN
+              cs_component_vals_per_config(exp_ids, components,
+                '-O3 -polly -polly-process-unprofitable') as P4
+              ON (P4.project = P1.project AND P4.variable = P1.variable)
+  WHERE TRUE
+  ORDER BY
+    project,
+    component
+  ;
+END
+$BODY$ LANGUAGE plpgsql;

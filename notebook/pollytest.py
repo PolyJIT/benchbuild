12ffb273c5e5151b73de3fe2672bca24d6f47e1e
Andreas Simbürger <simbuerg@fim.uni-passau.de>
2017-07-20 11:15:01 +0200

    pollytest: start the join with _all_ projects available

--- notebook/pollytest.py
+++ notebook/pollytest.py
@@ -13,13 +13,11 @@ QUERY_EVAL = \
     sa.sql.select([
         sa.column('project'),
         sa.column('variable'),
-        sa.column('p'),
-        sa.column('pv'),
-        sa.column('pvu'),
-        sa.column('pu')
+        sa.column('metric'),
+        sa.column('value')
     ]).\
     select_from(
-        sa.func.pollytest_eval(
+        sa.func.pollytest_eval_melted(
             sa.sql.bindparam('exp_ids'),
             sa.sql.bindparam('components')))
 qry = QUERY_EVAL.unique_params(exp_ids=[uuid.UUID("6c9d1f23-d9ee-4744-88ea-243dcaa50cd4")],
@@ -29,62 +27,65 @@ df = pd.read_sql_query(qry, session.bind)
 print(df.head())
 
 #%%
-comps = df['variable'].unique()
-print(comps)
+configs = df['variable'].unique()
+print(configs)
+metrics = df['metric'].unique()
+print(metrics)
 
-layout = go.Layout(
-    xaxis=dict(tickangle=-45),
-    barmode='group'
-)
-traces = []
-comp = 'Number of valid Scops'
-
-data = df.loc[df.variable == comp]
-traces.append(
-        go.Bar(
-            x = data["project"],
-            y = data["p"],
-            name="-polly"
-        ))
-traces.append(
-        go.Bar(
-            x = data["project"],
-            y = data["pv"],
-            name="-polly -polly-position=before-vectorizer"
-        ))
-traces.append(
-        go.Bar(
-            x = data["project"],
-            y = data["pvu"],
-            name="-polly -polly-position=before-vectorizer -polly-process-unprofitable"
-        ))
-traces.append(
-        go.Bar(
-            x = data["project"],
-            y = data["pu"],
-            name="-polly -polly-process-unprofitable"
-        ))
 #%%
-layout = go.Layout(
-    barmode='grouped',
-    height=600,
-    width=600,
-    xaxis=dict(
-        autorange=True,
-        showgrid=False,
-        zeroline=False,
-        showline=False,
-        autotick=True,
-        ticks='',
-        showticklabels=False
-    ),
-    title=comp
-)
-
 from plotly import __version__
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
-fig = go.Figure(data=traces, layout=layout)
+cfg_rename = {
+    '-O3 -polly': 'P',
+    '-O3 -polly -polly-position=before-vectorizer': 'PV',
+    '-O3 -polly -polly-position=before-vectorizer -polly-process-unprofitable': 'PVU',
+    '-O3 -polly -polly-process-unprofitable': 'PU',
+}
+
+def get_metric_data(metric, all_data):
+    return all_data.loc[all_data.metric == metric]
+
+def generate_traces(data, configs):
+    traces = []
+    for config in configs:
+        cfg_data = data.loc[data.variable == config]
+        traces.append(
+                go.Bar(
+                    x = cfg_data["project"],
+                    y = cfg_data["value"],
+                    name = cfg_rename[config]
+                ))
+    return traces
+
+def generate_group_plot(title, traces):
+    layout = go.Layout(
+        barmode='grouped',
+        height=800,
+        width=800,
+        xaxis=dict(
+            autorange=True,
+            showgrid=False,
+            zeroline=False,
+            showline=False,
+            autotick=True,
+            showticklabels=True
+        ),
+        yaxis=dict(
+            zeroline=True,
+            showline=True,
+            autorange=True
+        ),
+        title=title,
+        legend=dict(x=-.1, y=1.2)
+    )
+    fig = go.Figure(data=traces, layout=layout)
+    plot(fig, filename="gp-{}".format(title).replace(' ', '_'))
+
 init_notebook_mode()
-iplot(fig, filename='grouped-bar.html')
-print(__version__)
+for metric in metrics:
+    if metric == 'Number of valid Scops':
+        m_data = get_metric_data(metric, df)
+        traces = generate_traces(m_data, configs)
+        generate_group_plot(metric, traces)
+        print("Writing %s" % metric + ".png")

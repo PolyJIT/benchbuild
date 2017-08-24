65d76f13ba3b8e09b532a8eb7b8526d38f8c1163
Simon Labrenz <simon.labrenz@outlook.de>
2017-08-24 14:23:30 +0200

    statistics: Update file to current state of progress
    With some adjustments to the extensions structure this can allready
    calculate the first values until the run_information object runs into a
    key_error. This shall be fixed later on and i am working on it.

--- benchbuild/statistics.py
+++ benchbuild/statistics.py
@@ -2,24 +2,45 @@
 Handle all statsitic related classes and methods.
 """
 import logging
+import parse
 
 from statistics import median
 from benchbuild.extensions import Extension
 from benchbuild.utils.schema import Session
+from benchbuild.utils.run import track_execution, handle_stdin
 from scipy import stats
 
 LOG = logging.getLogger(__name__)
 
 TIMEOUT = 5
-POPMEAN = 0.0
-P_VAL = 0.9
+NULLHYPO = 0.0 # means the results are not related to each other
+P_VAL = 0.6 # with a chance of 60% the null hypothesis is wrong
 
-def dist_func(results):
+def dist_func(run_info):
     """
     The default p value calculation of the experiment under the assumption
-    that the results are normally distributed.
+    that the results are normally distributed in a two-tailed test static.
+    The p value gets calculated based on one sample array of results.
+    This sample pool is created by putting all values of a measured project
+    into a list.
     """
-    _, p_val = stats.ttest_1samp(results, POPMEAN)
+    values = []
+    p_val = 0.0
+    if run_info.db_run.status == "completed":
+        results = []
+        result_pattern = parse.compile("{value:d} {component} - {desc}\n")
+
+        for line in run_info.stderr.split("\n"):
+            if line:
+                try:
+                    results = result_pattern.search(line + "\n")
+                except ValueError:
+                    LOG.warning("Could not parse: '" + line + "'\n")
+
+        for stat in results:
+            values.append(stat["value"])
+            print("value added to the array: " + stat["value"])
+        _, p_val = stats.ttest_1samp(values, NULLHYPO)
     return p_val
 
 
@@ -34,33 +55,46 @@ class Statistics(Extension):
 
         super(Statistics, self).__init__(*extensions, config=config)
 
-    def __call__(self, *args, min_p_val=P_VAL, results=[], **kwargs):
+    def __call__(self, cc, *args, min_p_val=P_VAL, timeout=TIMEOUT, **kwargs):
         """
         The call of this extension runs the following extensions until a the
         p value the user specified is reached or the run times out.
 
+        Args:
+            cc: The compiler commando that becomes clang.
         Kwargs:
             min_p_val: The minimum p value the user wants to reach with
                        the running experiment.
+            timeout: The amount of trys the user wants to give the experiment
+                     before it gets interrupted.
         Returns:
             The run after executing the afterwards following extensions.
         """
-        session = Session()
-        iterator = 0
-        res = None
-        cur_p_val = 0
         calculated_ps = []
+        cur_p_val = 0.0
+        iterator = 1
+        res = None
+        session = Session()
 
         LOG.info("Started experiment until it has reached the wanted p value.")
-        while iterator < TIMEOUT and cur_p_val < min_p_val:
+        clang = handle_stdin(cc["-mllvm", "-stats"], kwargs)
+        while iterator < timeout and cur_p_val < min_p_val:
+            results = self.call_next(cc, *args, **kwargs)
+            if results is not None:
+                with track_execution(clang, self.project, self.experiment):
+                    run_info = results[0]()
+                cur_p_val = dist_func(run_info)
+                LOG.debug(
+                    "In the %s. iteration a p-value of %s was calculated.",
+                    iterator, cur_p_val)
+                calculated_ps.append(cur_p_val)
+                session.commit()
+            else:
+                LOG.info("There were no following extensions to run.")
+                break
             iterator += 1
-            LOG.debug("Iteration: %s", str(iterator))
-            cur_p_val = dist_func(results)
-            calculated_ps.append(cur_p_val)
-            res = self.call_next(*args, **kwargs)
-            session.commit()
 
-        if iterator < TIMEOUT:
+        if iterator < TIMEOUT and min_p_val <= cur_p_val:
             LOG.info("The statistically significance of p = %s was reached.",
                      cur_p_val)
         else:

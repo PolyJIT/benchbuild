46517c351ae73ec338707dd50a83c03a9acb0c85
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-09-18 16:15:14 +0200

    lammps: use examples from git, not test-inputs
    This uses all examples delivered by lammps with the source code instead
    of the preloaded ones from our own test repository.

--- benchbuild/projects/benchbuild/lammps.py
+++ benchbuild/projects/benchbuild/lammps.py
@@ -1,12 +1,12 @@
-from os import path
 from glob import glob
+import os
 
 from benchbuild.utils.wrapping import wrap
 from benchbuild.projects.benchbuild.group import BenchBuildGroup
 from benchbuild.utils.compiler import lt_clang_cxx
 from benchbuild.utils.downloader import Git
 from benchbuild.utils.run import run
-from benchbuild.utils.cmd import cp, make
+from benchbuild.utils.cmd import make
 from plumbum import local
 
 
@@ -17,17 +17,16 @@ class Lammps(BenchBuildGroup):
     DOMAIN = 'scientific'
     SRC_FILE = 'lammps.git'
 
-    def prepare(self):
-        super(Lammps, self).prepare()
-        cp("-vr", self.testdir, "test")
-
     def run_tests(self, experiment, run):
-        lammps_dir = path.join(self.builddir, self.src_dir, "src")
-        exp = wrap(path.join(lammps_dir, "lmp_serial"), experiment)
+        lammps_dir = os.path.join(self.builddir, self.src_dir, "src")
+        exp = wrap(os.path.join(lammps_dir, "lmp_serial"), experiment)
 
-        with local.cwd("test"):
-            tests = glob(path.join(self.testdir, "in.*"))
-            for test in tests:
+        examples_dir = os.path.join(self.builddir, self.src_dir, "examples")
+        tests = glob(os.path.join(examples_dir, "./*/in.*"))
+
+        for test in tests:
+            dirname = os.path.dirname(test)
+            with local.cwd(dirname):
                 cmd = (exp < test)
                 run(cmd, None)
 
@@ -37,16 +36,13 @@ class Lammps(BenchBuildGroup):
     def download(self):
         Git(self.src_uri, self.src_dir)
 
-    def configure(self):
-        pass
-
     def build(self):
         self.ldflags += ["-lgomp"]
 
         clang_cxx = lt_clang_cxx(self.cflags, self.ldflags,
                                  self.compiler_extension)
 
-        with local.cwd(path.join(self.src_dir, "src")):
-            run(make[
-                "CC=" + str(clang_cxx), "LINK=" + str(
-                    clang_cxx), "clean", "serial"])
+        with local.cwd(os.path.join(self.src_dir, "src")):
+            run(make["CC=" + str(clang_cxx),
+                     "LINK=" + str(clang_cxx),
+                     "clean", "serial"])

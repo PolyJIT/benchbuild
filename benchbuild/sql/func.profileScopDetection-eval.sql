e8eb11dd330e952e369e20320ad35e37544d5e59
Stefan Huber <junktogo@web.de>
2017-10-05 07:47:11 +0200

    ProfileScopDetection: Purge tabs
    Former-commit-id: 29693e477b082e721c10234dea87f55ea3223664
    Former-commit-id: 5c8c04ff28f572e6f9c427ddde0d58166fc6115a [formerly 2bb61b5e5e2fe140c4c8d977dfd28eeba3e2e0b8] [formerly ea7cac36901b2e0f8f73972d01270eb732cebfc0 [formerly a86a07b0c90e6c0c488eb0fbfad3bd8b6814991d]]
    Former-commit-id: 845e6f08b3dcafe1f140c534f11fe4df22f0165f [formerly fc167e3d1736ca79484cf8a2c6636e4c8fc17bb3]
    Former-commit-id: e79c4c80995903233620955939903c42d92d8ae5

--- benchbuild/sql/func.profileScopDetection-eval.sql
+++ benchbuild/sql/func.profileScopDetection-eval.sql
@@ -22,83 +22,83 @@ $BODY$
 
 DROP FUNCTION IF EXISTS profile_scops_valid_regions(exp_ids UUID[]);
 CREATE OR REPLACE FUNCTION profile_scops_valid_regions(exp_ids UUID[])
-	returns
+    returns
     table(run_id INTEGER,
           duration NUMERIC,
-	  id NUMERIC,
-	  name VARCHAR,
-	  events BIGINT)
+      id NUMERIC,
+      name VARCHAR,
+      events BIGINT)
 AS $BODY$
 BEGIN
   RETURN QUERY
-	select
-		regions.run_id,
-		regions.duration,
-		regions.id,
-		regions.name,
-		regions.events
-	from
-		run, regions, metrics
-	where
-		run.id = regions.run_id and
-		run.id = metrics.run_id and
-		metrics.name = 'time.real_s' and
-		metrics.value * 1000000 >= regions.duration and
-		run.experiment_group = ANY (exp_ids);
+    select
+        regions.run_id,
+        regions.duration,
+        regions.id,
+        regions.name,
+        regions.events
+    from
+        run, regions, metrics
+    where
+        run.id = regions.run_id and
+        run.id = metrics.run_id and
+        metrics.name = 'time.real_s' and
+        metrics.value * 1000000 >= regions.duration and
+        run.experiment_group = ANY (exp_ids);
 end
 $BODY$ language plpgsql;
 
 DROP FUNCTION IF EXISTS profile_scops_count_invalid_regions(exp_ids UUID[]);
 CREATE OR REPLACE FUNCTION profile_scops_count_invalid_regions(exp_ids UUID[])
-	returns INTEGER
+    returns INTEGER
 AS $BODY$
 BEGIN
   RETURN
-	(select
-		count(*) as num_invalid
-	from
-		run, regions, metrics
-	where
-		run.id = regions.run_id and
-		run.id = metrics.run_id and
-		metrics.name = 'time.real_s' and
-		metrics.value * 1000000 < regions.duration and
-		run.experiment_group = ANY (exp_ids));
+    (select
+        count(*) as num_invalid
+    from
+        run, regions, metrics
+    where
+        run.id = regions.run_id and
+        run.id = metrics.run_id and
+        metrics.name = 'time.real_s' and
+        metrics.value * 1000000 < regions.duration and
+        run.experiment_group = ANY (exp_ids));
 end
 $BODY$ language plpgsql;
 
 DROP FUNCTION IF EXISTS profile_scops_ratios(exp_ids UUID[], filter_str VARCHAR);
 CREATE OR REPLACE FUNCTION profile_scops_ratios(exp_ids UUID[], filter_str VARCHAR)
-	returns
+    returns
     table (
-	project VARCHAR,
-	T_SCoP NUMERIC,
-	T_Total DOUBLE PRECISION,
-	DynCov DOUBLE PRECISION)
+    project VARCHAR,
+    T_SCoP NUMERIC,
+    T_Total DOUBLE PRECISION,
+    DynCov DOUBLE PRECISION)
 AS $BODY$
 BEGIN
   RETURN QUERY
-	select
-		total.project,
-		scops.t_scop,
-		total.exectime_us,
-		(scops.t_scop * 100/ total.exectime_us) as DynCov_pct
-	from
-		profile_scops_exec_times(exp_ids) as total,
-		(
-		  select
-			run.project_name as project,
-			SUM(valid_regions.duration) as T_SCoP
-		  from
-			profile_scops_valid_regions(exp_ids) as valid_regions,
-			run
-		  where
-			  valid_regions.name like filter_str and
-			  run.id = valid_regions.run_id
-		  group by
-			  run.project_name
-		) as scops
-	where
-		total.project = scops.project;
+    select
+        total.project,
+        scops.t_scop,
+        total.exectime_us,
+        (scops.t_scop * 100/ total.exectime_us) as DynCov_pct
+    from
+        profile_scops_exec_times(exp_ids) as total,
+        (
+          select
+            run.project_name as project,
+            SUM(valid_regions.duration) as T_SCoP
+          from
+            profile_scops_valid_regions(exp_ids) as valid_regions,
+            run
+          where
+              valid_regions.name like filter_str and
+              run.id = valid_regions.run_id
+          group by
+              run.project_name
+        ) as scops
+    where
+        total.project = scops.project;
 end
 $BODY$ language plpgsql;

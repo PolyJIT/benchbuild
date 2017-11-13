1a7f0ec64ed59e7644d3abe64f5896e28fd96b85
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-11-13 12:22:41 +0100

    ijpp: extend report and add forgotten .sql
    Former-commit-id: 46b962ada1cfac630ac2bfc746adfe036378675f
    Former-commit-id: 51b847f3196037f9e16ffcd2293d556779e1b228 [formerly 1bb5c55cf615dae37919bc9e51acdc04b3e9a69c] [formerly 4a0ad2a9d4ec43a8d55271c003ea4823ead4d049 [formerly 0829b86b25ce73628cb11b96049b152996ec1b7d]]
    Former-commit-id: 24075f6a73cf97e48037eb7626763d3cb59d0398 [formerly ba709c82169d57818a9544b8eb45cee620266a47]
    Former-commit-id: 7ee57c277c85771fa3fb4f76f226e1afa2ff8cf2

---
+++ benchbuild/sql/func.ijpp.sql
@@ -0,0 +1,82 @@
+DROP FUNCTION IF EXISTS ijpp_total_runtime(exp_ids UUID[]);
+CREATE OR REPLACE FUNCTION ijpp_total_runtime(exp_ids UUID [])
+RETURNS TABLE(
+	project VARCHAR,
+	"group" VARCHAR,
+        domain VARCHAR,
+	config VARCHAR,
+	"time" DOUBLE PRECISION,
+	variants NUMERIC,
+        cachehits NUMERIC
+      )
+AS $BODY$ BEGIN
+RETURN QUERY
+  SELECT
+	project_name AS project,
+	project_group AS "group",
+        prj.domain AS "domain",
+	config.value AS config,
+    	SUM(coalesce(r.duration / 1000000, metrics.value)) AS time,
+    	SUM(COALESCE(vars.duration, 0)) AS variants,
+        SUM(COALESCE(chits.duration, 0)) AS cachehits
+  FROM (
+	SELECT run.* FROM run LEFT JOIN config ON (run.id = config.run_id)
+	WHERE
+	  config.name = 'baseline' AND
+	  config.value = 'False' AND
+	  run.experiment_group = ANY (exp_ids)
+       ) AS rrun
+      RIGHT JOIN config ON (rrun.id = config.run_id)
+      LEFT JOIN metrics ON (rrun.id = metrics.run_id)
+      FULL OUTER JOIN
+	(SELECT * FROM regions WHERE regions.name = 'VARIANTS') AS vars
+      ON (rrun.id = vars.run_id)
+      FULL OUTER JOIN
+	(SELECT * FROM regions WHERE regions.name = 'CACHE_HIT') AS chits
+      ON (rrun.id = chits.run_id)
+      FULL OUTER JOIN
+	(SELECT * FROM regions WHERE regions.name = 'START') AS r
+      ON (rrun.id = r.run_id)
+      LEFT JOIN project AS prj ON (rrun.project_name = prj.name AND
+                                   rrun.project_group = prj.group_name)
+  WHERE 
+	config.name = 'name' AND
+	metrics.name = 'time.real_s'
+  GROUP BY
+	project, "group", prj.domain, config
+  ORDER BY
+	project, config;
+END $BODY$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS ijpp_region_wise(exp_ids UUID[]);
+CREATE OR REPLACE FUNCTION ijpp_region_wise(exp_ids UUID [])
+RETURNS TABLE(
+	project VARCHAR,
+	"region" VARCHAR,
+        "config" VARCHAR,
+	"runtime" NUMERIC
+      )
+AS $BODY$ BEGIN
+RETURN QUERY
+  SELECT
+	rrun.project_name AS project,
+	regions.name AS region,
+	config.value AS config,
+	SUM(regions.duration) AS runtime
+  FROM (
+	SELECT run.* FROM run LEFT JOIN config ON (run.id = config.run_id)
+	WHERE
+	  config.name = 'baseline' AND
+	  config.value = 'False' AND
+	  run.experiment_group = '69a1c642-a03d-4c99-9d22-bc5361f01ba9'
+       ) as rrun
+      LEFT JOIN config ON (rrun.id = config.run_id)
+      LEFT JOIN regions ON (rrun.id = regions.run_id)
+  WHERE
+    config.name = 'name' AND
+    regions.name != ALL ('{START, CODEGEN, CACHE_HIT, VARIANTS}'::VARCHAR[])
+  GROUP BY
+    rrun.project_name, regions.name, config.value
+  ORDER BY project, region, config;
+END $BODY$ LANGUAGE plpgsql;
+

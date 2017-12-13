861bdf75d5c75bc526d195331f0c7fb3bf9f9a25
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2017-12-13 12:42:59 +0100

    ijpp: port pjtest queries over and adapt to ijpp schema
    Former-commit-id: c3ca9ffd1ebfd20a470b40a7e25d5e140b5dda48
    Former-commit-id: 99617453190538940c7af05fbebdc6bb82321b3a [formerly 5b78b7b090a5d67e58e5b1b467cc9ae7320cf063] [formerly a25a574cc7ac3b3c7d7c610224445a3d0fec5c30 [formerly c747dccb3fca76273de5d5ae14d2b42b9f6ff290]]
    Former-commit-id: 4f50fb652582f7aa8fa5538daa3fe4e349343ee8 [formerly bbddaea317575b9ab5ca91fd6acfe76c1705784e]
    Former-commit-id: 15d7aabe7a459dd14638256aceb1bbc07f8e629d

--- benchbuild/sql/func.ijpp.sql
+++ benchbuild/sql/func.ijpp.sql
@@ -1,3 +1,11 @@
+CREATE OR REPLACE VIEW ijpp_runs AS
+  SELECT
+    run.*
+  FROM run LEFT JOIN config ON (run.id = config.run_id)
+  WHERE
+    config.name = 'baseline' AND
+    config.value = 'False';
+
 DROP FUNCTION IF EXISTS ijpp_total_runtime(exp_ids UUID[]);
 CREATE OR REPLACE FUNCTION ijpp_total_runtime(exp_ids UUID [])
 RETURNS TABLE(
@@ -19,13 +27,7 @@ RETURN QUERY
     	SUM(coalesce(r.duration / 1000000, metrics.value)) AS time,
     	SUM(COALESCE(vars.duration, 0)) AS variants,
         SUM(COALESCE(chits.duration, 0)) AS cachehits
-  FROM (
-	SELECT run.* FROM run LEFT JOIN config ON (run.id = config.run_id)
-	WHERE
-	  config.name = 'baseline' AND
-	  config.value = 'False' AND
-	  run.experiment_group = ANY (exp_ids)
-       ) AS rrun
+  FROM ijpp_runs AS rrun
       RIGHT JOIN config ON (rrun.id = config.run_id)
       LEFT JOIN metrics ON (rrun.id = metrics.run_id)
       FULL OUTER JOIN
@@ -39,9 +41,10 @@ RETURN QUERY
       ON (rrun.id = r.run_id)
       LEFT JOIN project AS prj ON (rrun.project_name = prj.name AND
                                    rrun.project_group = prj.group_name)
-  WHERE 
+  WHERE
 	config.name = 'name' AND
-	metrics.name = 'time.real_s'
+	metrics.name = 'time.real_s' AND
+        rrun.experiment_group = ANY (exp_ids)
   GROUP BY
 	project, "group", prj.domain, config
   ORDER BY
@@ -63,16 +66,11 @@ RETURN QUERY
 	regions.name AS region,
 	config.value AS config,
 	SUM(regions.duration) AS runtime
-  FROM (
-	SELECT run.* FROM run LEFT JOIN config ON (run.id = config.run_id)
-	WHERE
-	  config.name = 'baseline' AND
-	  config.value = 'False' AND
-	  run.experiment_group = ANY (exp_ids)
-       ) as rrun
+  FROM ijpp_runs AS rrun
       LEFT JOIN config ON (rrun.id = config.run_id)
       LEFT JOIN regions ON (rrun.id = regions.run_id)
   WHERE
+    rrun.experiment_group = ANY (exp_ids) AND
     config.name = 'name' AND
     regions.name != ALL ('{START, CODEGEN, CACHE_HIT, VARIANTS}'::VARCHAR[])
   GROUP BY
@@ -130,7 +128,8 @@ RETURNS TABLE(
 AS $BODY$ BEGIN
 RETURN QUERY
   select
-    coalesce(t1."project", t2."project") as "project", coalesce(t1."group", t2."group") as "group",
+    coalesce(t1."project", t2."project") as "project",
+    coalesce(t1."group", t2."group") as "group",
     coalesce(t1."function", t2."function") as "function",
     t1.ast AS jit_ast, t1.schedule AS jit_schedule,
     t1.stderr AS jit_stderr,
@@ -142,3 +141,353 @@ RETURN QUERY
 	ijpp_db_export_per_config(exp_ids, '{polly.inside}') AS t2
 	on (t1.project = t2.project AND t1."function" = t2."function");
 END $BODY$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS ijpp_runs_by_config(exp_ids UUID[], config_name VARCHAR);
+CREATE OR REPLACE FUNCTION ijpp_runs_by_config(exp_ids UUID[], config_name VARCHAR)
+  returns table(name VARCHAR, value VARCHAR, run_id INTEGER) as $BODY$
+BEGIN
+  RETURN QUERY
+    SELECT config.name,
+           config.value,
+           config.run_id
+    FROM ijpp_runs as rrun LEFT JOIN config ON (rrun.id = config.run_id)
+    WHERE
+      rrun.experiment_group = ANY (exp_ids) AND
+      rrun.status = 'completed' AND
+      config.name = 'name' AND config.value = config_name;
+END $BODY$ language plpgsql;
+
+DROP FUNCTION IF EXISTS ijpp_project_region_time(region_name VARCHAR, exp_ids UUID[], config_name VARCHAR);
+CREATE OR REPLACE FUNCTION ijpp_project_region_time(region_name VARCHAR, exp_ids UUID [], config_name VARCHAR)
+  RETURNS
+    TABLE(project_name VARCHAR,
+          duration     NUMERIC)
+AS $BODY$
+BEGIN
+  RETURN QUERY
+  SELECT
+    run.project_name,
+    sum(regions.duration)
+  FROM run, config, regions,
+        ijpp_runs_by_config((exp_ids), config_name) AS spec
+  WHERE
+    run.id = config.run_id AND run.id = regions.run_id AND
+    run.experiment_group = ANY (exp_ids) AND
+    run.id = spec.run_id AND
+    regions.name = region_name
+  GROUP BY
+    run.project_name;
+END
+$BODY$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS ijpp_total_dyncov(exp_ids UUID []);
+CREATE OR REPLACE FUNCTION ijpp_total_dyncov(exp_ids UUID [])
+  RETURNS
+    TABLE(project   VARCHAR,
+          ohcov_0   NUMERIC,
+          ohcov_1   NUMERIC,
+          dyncov_0   NUMERIC,
+          dyncov_1   NUMERIC,
+          cachehits_0 NUMERIC,
+          cachehits_1 NUMERIC,
+          variants_0 NUMERIC,
+          variants_1 NUMERIC,
+          codegen_0   NUMERIC,
+          codegen_1   NUMERIC,
+          scops_0   NUMERIC,
+          scops_1   NUMERIC,
+          t_0       NUMERIC,
+          o_0       NUMERIC,
+          t_1       NUMERIC,
+          o_1       NUMERIC)
+AS $BODY$
+BEGIN
+  RETURN QUERY
+  SELECT
+    total.project_name                            AS project,
+    (O_0.duration / T_0.duration * 100)           AS ohcov_0,
+    (O_1.duration / T_1.duration * 100)           AS ohcov_1,
+    (scops_0.sum / T_0.duration * 100)            AS dyncov_0,
+    (scops_1.sum / T_1.duration * 100)            AS dyncov_1,
+    ch_0.sum                                      AS cachehits_0,
+    ch_1.sum                                      AS cachehits_1,
+    variants_0.sum                                AS variants_0,
+    variants_1.sum                                AS variants_1,
+    codegen_0.sum                                 AS codegen_0,
+    codegen_1.sum                                 AS codegen_1,
+    scops_0.sum                                   AS scops_0,
+    scops_1.sum                                   AS scops_1,
+    T_0.duration                                  AS t_0,
+    O_0.duration                                  AS o_0,
+    T_1.duration                                  AS t_1,
+    O_1.duration                                  AS o_1
+  FROM
+    (
+      SELECT
+        run.project_name,
+        sum(regions.duration)
+      FROM run, config, regions,
+            ijpp_runs_by_config(exp_ids, 'polly.inside.no-delin') AS recomp
+      WHERE
+        run.id = config.run_id AND run.id = regions.run_id AND
+        run.experiment_group = ANY (exp_ids) AND
+        run.id = recomp.run_id AND
+        regions.name = 'START'
+      GROUP BY
+        run.project_name
+    ) AS total
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config(exp_ids, 'polly.inside.no-delin') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'CODEGEN'
+                GROUP BY
+                  run.project_name
+              ) AS codegen_0 ON (codegen_0.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config(exp_ids, 'PolyJIT') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'CODEGEN'
+                GROUP BY
+                  run.project_name
+              ) AS codegen_1 ON (codegen_1.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config(exp_ids, 'polly.inside.no-delin') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'CACHE_HIT'
+                GROUP BY
+                  run.project_name
+              ) AS ch_0 ON (ch_0.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config(exp_ids, 'PolyJIT') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'CACHE_HIT'
+                GROUP BY
+                  run.project_name
+              ) AS ch_1 ON (ch_1.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config(exp_ids, 'polly.inside.no-delin') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'VARIANTS'
+                GROUP BY
+                  run.project_name
+              ) AS variants_0 ON (variants_0.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config(exp_ids, 'PolyJIT') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name = 'VARIANTS'
+                GROUP BY
+                  run.project_name
+              ) AS variants_1 ON (variants_1.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config((exp_ids), 'polly.inside.no-delin') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name != ALL ('{START, CODEGEN, VARIANTS, CACHE_HIT}' :: VARCHAR [])
+                GROUP BY
+                  run.project_name
+              ) AS scops_0 ON (scops_0.project_name = total.project_name)
+    LEFT JOIN (
+                SELECT
+                  run.project_name,
+                  sum(regions.duration)
+                FROM run, config, regions,
+                      ijpp_runs_by_config((exp_ids), 'PolyJIT') AS spec
+                WHERE
+                  run.id = config.run_id AND run.id = regions.run_id AND
+                  run.experiment_group = ANY (exp_ids) AND
+                  run.id = spec.run_id AND
+                  regions.name != ALL ('{START, CODEGEN, VARIANTS, CACHE_HIT}' :: VARCHAR [])
+                GROUP BY
+                  run.project_name
+              ) AS scops_1 ON (scops_1.project_name = total.project_name)
+    LEFT JOIN (SELECT * FROM ijpp_project_region_time('START', (exp_ids), 'polly.inside.no-delin')) AS T_0 ON (T_0.project_name = total.project_name)
+    LEFT JOIN (SELECT * FROM ijpp_project_region_time('CODEGEN', (exp_ids), 'polly.inside.no-delin')) AS O_0 ON (O_0.project_name = total.project_name)
+    LEFT JOIN (SELECT * FROM ijpp_project_region_time('START', (exp_ids), 'PolyJIT')) AS T_1 ON (T_1.project_name = total.project_name)
+    LEFT JOIN (SELECT * FROM ijpp_project_region_time('CODEGEN', (exp_ids), 'PolyJIT')) AS O_1 ON (O_1.project_name = total.project_name)
+  WHERE TRUE;
+END
+$BODY$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS ijpp_eval(exp_ids UUID []);
+CREATE OR REPLACE FUNCTION ijpp_eval(exp_ids UUID [])
+  RETURNS
+    TABLE(project   VARCHAR,
+          domain    VARCHAR,
+          ohcov_0   NUMERIC,
+          ohcov_1   NUMERIC,
+          dyncov_0   NUMERIC,
+          dyncov_1   NUMERIC,
+          cachehits_0 NUMERIC,
+          cachehits_1 NUMERIC,
+          variants_0 NUMERIC,
+          variants_1 NUMERIC,
+          codegen_0   NUMERIC,
+          codegen_1   NUMERIC,
+          scops_0   NUMERIC,
+          scops_1   NUMERIC,
+          t_0       NUMERIC,
+          o_0       NUMERIC,
+          t_1       NUMERIC,
+          o_1       NUMERIC,
+          speedup   NUMERIC)
+AS $BODY$
+BEGIN
+  RETURN QUERY
+SELECT
+  coverage.project,
+  p.domain                                    AS domain,
+  coverage.ohcov_0                            AS OhCov_POLLY,
+  coverage.ohcov_1                            AS OhCov_PJIT,
+  coverage.dyncov_0                           AS DynCov_POLLY,
+  coverage.dyncov_1                           AS DynCov_PJIT,
+  coverage.cachehits_0                        AS CH_POLLY,
+  coverage.cachehits_1                        AS CH_PJIT,
+  coverage.variants_0                         AS VARS_POLLY,
+  coverage.variants_1                         AS VARS_PJIT,
+  coverage.codegen_0 / 1000000                AS Oh_POLLY,
+  coverage.codegen_1 / 1000000                AS Oh_PJIT,
+  coverage.scops_0 / 1000000                  AS Scop_POLLY,
+  coverage.scops_1 / 1000000                  AS Scops_PJIT,
+  coverage.t_0 / 1000000                      AS T_POLLY,
+  coverage.o_0 / 1000000                      AS OH_POLLY,
+  coverage.t_1 / 1000000                      AS T_PJIT,
+  coverage.o_1 / 1000000                      AS OH_PJIT,
+  speedup(coverage.t_0, coverage.t_1)         AS speedup
+FROM
+  ijpp_total_dyncov(exp_ids)                  AS coverage
+  LEFT JOIN
+    project AS p ON (coverage.project = p.name)
+WHERE
+  coverage.t_0 > 1000 AND
+  coverage.scops_1 is not NULL and
+  coverage.variants_1 > 0 and
+  coverage.t_1 is not NULL
+ORDER BY
+  p.domain ASC,
+  coverage.dyncov_1 DESC;
+END
+$BODY$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS ijpp_run_regions(exp_ids UUID[], config_name VARCHAR);
+CREATE OR REPLACE FUNCTION ijpp_run_regions(exp_ids UUID[], config_name VARCHAR)
+  returns table(
+    name VARCHAR,
+    region VARCHAR,
+    runtime NUMERIC,
+    value VARCHAR,
+    specialization VARCHAR) AS $BODY$
+BEGIN
+  RETURN QUERY
+    SELECT
+      rrun.project_name AS project,
+      regions.name AS region_name,
+      SUM(regions.duration) AS runtime,
+      config.value,
+      spec.value AS specialization
+    FROM
+        ijpp_runs AS rrun,
+        ijpp_runs_by_config(exp_ids, config_name) AS spec,
+        config,
+        regions
+    WHERE
+    rrun.experiment_group = ANY (exp_ids) AND
+    rrun.id = regions.run_id AND
+    rrun.id = config.run_id AND
+    rrun.id = spec.run_id AND
+    config.name = 'cores'
+    GROUP BY rrun.project_name, regions.name, config.value, spec.value
+    ORDER BY project, runtime, config.value;
+END
+$BODY$ language plpgsql;
+
+DROP FUNCTION IF EXISTS ijpp_region_wise_compare(exp_ids UUID[]);
+CREATE OR REPLACE FUNCTION ijpp_region_wise_compare(exp_ids UUID[])
+	returns
+    table(Project VARCHAR,
+          Region VARCHAR,
+          Cores VARCHAR,
+          T_Polly NUMERIC,
+          T_PolyJIT NUMERIC,
+          speedup NUMERIC)
+AS $compare_region_wise$
+BEGIN
+  RETURN QUERY
+select * from
+(
+select
+  results.project,
+  results.region,
+  results.cores,
+  results.runtime_polly,
+  results.runtime_polyjit,
+  speedup(results.runtime_polly, results.runtime_polyjit) as speedup FROM
+  (
+    SELECT
+      spec_enabled.name AS project,
+      spec_enabled.region,
+      spec_enabled.value AS cores,
+      spec_enabled.runtime AS runtime_polly,
+      spec_disabled.runtime AS runtime_polyjit
+    FROM
+      ijpp_run_regions(exp_ids, 'PolyJIT') as spec_enabled,
+      ijpp_run_regions(exp_ids, 'polly.inside.no-delin') as spec_disabled
+    WHERE
+	    spec_enabled.name = spec_disabled.name and
+	    spec_enabled.region = spec_disabled.region and
+	    spec_enabled.value = spec_disabled.value and
+	    spec_enabled.region != ALL ('{START, CODEGEN, VARIANTS, CACHE_HIT}'::VARCHAR[])
+    order by
+      project, cores, region
+  ) as results
+) as reulsts_f
+order by speedup desc;
+end
+$compare_region_wise$ language plpgsql;

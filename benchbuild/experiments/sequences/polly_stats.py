522bf146f7ff350bb6edb0e4f611df0069f8c265
Andreas Simbürger <simbuerg@fim.uni-passau.de>
2018-03-18 17:02:02 +0100

    fix: embarassing syntax error pt. 2

--- benchbuild/experiments/sequences/polly_stats.py
+++ benchbuild/experiments/sequences/polly_stats.py
@@ -68,7 +68,7 @@ def __get_number_of_certain_stats_line(opt_flags, program, line_a, line_b=None,
     command.extend(STATS_FLAGS)
     command.append(program)
     fnull = open(os.devnull, 'w')
-    p = compiler(CFG["compiler"]["cxx"].value()
+    p = compiler(CFG["compiler"]["cxx"].value())
     _, stderr = p.communicate()
     stderr = stderr.splitlines()
 

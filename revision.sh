127a27a5d83f51fa1f4944e1e8b9eb6a4d62d70e
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2018-04-09 15:43:26 +0200

    version: do not add __version__ to git

--- revision.sh
+++ revision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set oux
 revision_path=$(dirname $(realpath $0))
-version_path="${revision_path}/benchbuild/utils/__version__.py"
+version_path="${revision_path}/benchbuild/__version__.py"
 git_rev=$(git describe --tags)
 echo "VERSION = \"$git_rev\"" > $version_path

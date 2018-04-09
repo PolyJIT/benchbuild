e67ed776d49534fe5001b40ea90a7100f026d3a0
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2018-04-09 15:55:59 +0200

    version: remove revision.sh
    Former-commit-id: f9957ba35f7ed0a4da75e19a3052a622e5bfc358
    Former-commit-id: f2a83391bebcc7464eba3f60598340a8eaf1b7b5 [formerly 2593f6d9b7c87f7711ce794d60412fb7e310356f] [formerly 7010a4575e4dde61932cb53f55ba5853d702465e [formerly c0fc36e29c0e4ee2ddcab037e2aa13dfbd69aa40]]
    Former-commit-id: 132ab6f73114a1b686cdac2750adde2841ff4fec [formerly a7876059c36b876ddeb89095a8dfbc48203fb685]
    Former-commit-id: 06947248a8bf5845a0e7658a4c52a8173afc7ac3

--- revision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set oux
-revision_path=$(dirname $(realpath $0))
-version_path="${revision_path}/benchbuild/__version__.py"
-git_rev=$(git describe --tags)
-echo "VERSION = \"$git_rev\"" > $version_path

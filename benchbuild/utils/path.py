bb9f43f2dfa83db3559a062b21b12317b102a24e
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2018-04-11 12:50:55 +0200

    utils/path remove try/catch of unknown exception

--- benchbuild/utils/path.py
+++ benchbuild/utils/path.py
@@ -17,16 +17,10 @@ def path_to_list(pathstr):
 
 def determine_path():
     """Borrowed from wxglade.py"""
-    try:
-        root = __file__
-        if os.path.islink(root):
-            root = os.path.realpath(root)
-        return os.path.dirname(os.path.abspath(root))
-    except:
-        print("I'm sorry, but something is wrong.")
-        print("There is no __file__ variable. Please contact the author.")
-        sys.exit()
-
+    root = __file__
+    if os.path.islink(root):
+        root = os.path.realpath(root)
+    return os.path.dirname(os.path.abspath(root))
 
 def template_files(path, exts=[]):
     """

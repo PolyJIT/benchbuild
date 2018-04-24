63a4e4aa79d0acfb2d5ebd7f82bf89fd21413523
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2018-04-24 18:33:04 +0200

    setup: yapf formatting

--- setup.py
+++ setup.py
@@ -4,43 +4,35 @@ with open('README.md') as f:
     long_description = f.read()
 
 extra_files = [
-    "templates/run_compiler.py.inc",
-    "templates/run_static.py.inc",
-    "templates/run_dynamic.py.inc",
-    "templates/slurm.sh.inc"
+    "templates/run_compiler.py.inc", "templates/run_static.py.inc",
+    "templates/run_dynamic.py.inc", "templates/slurm.sh.inc"
 ]
 
-src_extra_files = [
-    "patches/linpack.patch"
-]
+src_extra_files = ["patches/linpack.patch"]
 
 sql_extra_files = [
-    "func.compare_region_wise2.sql",
-    "func.experiments.sql",
-    "func.recompilation.sql",
-    "func.run_regions.sql",
-    "func.total_dyncov_clean.sql",
-    "func.total_speedup.sql",
-    "func.compare_region_wise.sql",
-    "func.project_region_time.sql",
-    "func.run_durations.sql",
-    "func.speedup.sql",
-    "func.total_dyncov.sql",
-    "func.pj-test-eval.sql",
-    "func.compilestats_eval.sql",
-    "func.polly_mse.sql",
-    "func.profileScopDetection-eval.sql"
+    "func.compare_region_wise2.sql", "func.experiments.sql",
+    "func.recompilation.sql", "func.run_regions.sql",
+    "func.total_dyncov_clean.sql", "func.total_speedup.sql",
+    "func.compare_region_wise.sql", "func.project_region_time.sql",
+    "func.run_durations.sql", "func.speedup.sql", "func.total_dyncov.sql",
+    "func.pj-test-eval.sql", "func.compilestats_eval.sql",
+    "func.polly_mse.sql", "func.profileScopDetection-eval.sql"
 ]
 
 setup(
     name='benchbuild',
     use_scm_version=True,
     url='https://github.com/PolyJIT/benchbuild',
-    packages=find_packages(exclude=["docs", "extern", "filters", "linker",
-                                    "src", "statistics", "tests", "results"]),
-    package_data={"benchbuild.utils": extra_files,
-                  "benchbuild": sql_extra_files,
-                  "becnbuild.projects": src_extra_files},
+    packages=find_packages(exclude=[
+        "docs", "extern", "filters", "linker", "src", "statistics", "tests",
+        "results"
+    ]),
+    package_data={
+        "benchbuild.utils": extra_files,
+        "benchbuild": sql_extra_files,
+        "becnbuild.projects": src_extra_files
+    },
     include_package_data=True,
     setup_requires=["pytest-runner", "setuptools_scm"],
     tests_require=["pytest"],
@@ -67,8 +59,10 @@ setup(
     long_description_content_type='text/markdown',
     license="MIT",
     entry_points={
-        'console_scripts': ['benchbuild=benchbuild.driver:main',
-                            'container=benchbuild.container:main']
+        'console_scripts': [
+            'benchbuild=benchbuild.driver:main',
+            'container=benchbuild.container:main'
+        ]
     },
     classifiers=[
         'Development Status :: 4 - Beta', 'Intended Audience :: Developers',

2c37f53d4510f81a847d2333a3eba12072e494b9
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2018-07-11 15:48:41 +0200

    utils/log: refactor logging & silence sqlalchemy.migrate logs

--- benchbuild/utils/log.py
+++ benchbuild/utils/log.py
@@ -3,6 +3,23 @@ import logging
 from benchbuild import settings
 
 
+def configure_migrate_log():
+    migrate_log = logging.getLogger("migrate.versioning")
+    migrate_log.setLevel(logging.ERROR)
+    migrate_log.propagate = True
+
+
+def configure_plumbum_log():
+    plumbum_format = logging.Formatter('$> %(message)s')
+    plumbum_hdl = logging.StreamHandler()
+    plumbum_hdl.setFormatter(plumbum_format)
+    plumbum_local = logging.getLogger("plumbum.local")
+    if settings.CFG["debug"].value():
+        plumbum_local.setLevel(logging.DEBUG)
+    plumbum_local.addHandler(plumbum_hdl)
+    plumbum_local.propagate = False
+
+
 def configure():
     """Load logging configuration from our own defaults."""
     log_levels = {
@@ -23,20 +40,14 @@ def configure():
         details_hdl.setFormatter(details_format)
         root_logger.addHandler(details_hdl)
     else:
-        brief_format = logging.Formatter('%(message)s')
+        brief_format = logging.Formatter('%(name)s %(message)s')
         console_hdl = logging.StreamHandler()
         console_hdl.setFormatter(brief_format)
         root_logger.addHandler(console_hdl)
     root_logger.setLevel(log_levels[settings.CFG["verbosity"].value()])
 
-    plumbum_format = logging.Formatter('$> %(message)s')
-    plumbum_hdl = logging.StreamHandler()
-    plumbum_hdl.setFormatter(plumbum_format)
-    plumbum_local = logging.getLogger("plumbum.local")
-    if settings.CFG["debug"].value():
-        plumbum_local.setLevel(logging.DEBUG)
-    plumbum_local.addHandler(plumbum_hdl)
-    plumbum_local.propagate = False
+    configure_plumbum_log()
+    configure_migrate_log()
 
 
 def set_defaults():

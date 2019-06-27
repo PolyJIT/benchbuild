0609a2f60feab14001207deb5910508dec341a26
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2019-06-27 02:29:42 +0200

    add support for indented __str__

--- benchbuild/tasks/actions.py
+++ benchbuild/tasks/actions.py
@@ -2,10 +2,11 @@
 import itertools
 import logging
 import os
+import textwrap
 from abc import abstractmethod
-from contextlib import contextmanager
+from contextlib import AbstractContextManager, ExitStack, contextmanager
 from datetime import datetime
-from typing import List, Union, Iterable
+from typing import Iterable, List, Union
 
 import attr
 import sqlalchemy as sa
@@ -20,49 +21,66 @@ from benchbuild.utils.cmd import mkdir, rm, rmdir
 
 LOG = logging.getLogger(__name__)
 
+@attr.s
+class NamedTaskEntity:
+    def __attrs_post_init__(self):
+        self.name: str = type(self).__name__
+        self.description: str = type(self).__doc__
 
 @attr.s
-class GroupPolicy:
+class GroupPolicy(NamedTaskEntity):
     children: List[Union['GroupPolicy', 'Task']] = attr.ib()
     state: StepResult = attr.ib(kw_only=True, default=StepResult.OK)
 
     def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+
         for child in self.children:
             child.owner = self
 
     @abstractmethod
-    def can_continue(self, step_result):
-        pass
+    def can_continue(self, task_result: StepResult) -> bool:
+        ...
 
     def tasks(self):
         all_tasks = [child.tasks() for child in self.children]
         return list(itertools.chain(*all_tasks))
 
+    def __str__(self, indent=0) -> str:
+        ret_str = textwrap.indent(
+            "* {name}: {desc}\n".format(name=self.name, desc=self.description),
+            indent * ' ')
+        ret_str += "\n".join([
+            textwrap.indent(child.__str__(indent=indent + 1), indent * " ")
+            for child in self.children
+        ])
+        return ret_str
+
 
 @attr.s
 class FailOnError(GroupPolicy):
-    def can_continue(self, task_result):
+    def can_continue(self, task_result: StepResult) -> bool:
         res = task_result in [StepResult.OK, StepResult.CAN_CONTINUE]
         if not res:
-            self.state = res
+            self.state = task_result
 
         return self.state in [StepResult.OK, StepResult.CAN_CONTINUE]
 
 
 @attr.s
 class ContinueOnError(GroupPolicy):
-    def can_continue(self, step_result):
-        self.state = step_result
+    def can_continue(self, task_result: StepResult) -> bool:
+        self.state = task_result
         return True
 
 @attr.s
-class Task:
+class Task(NamedTaskEntity):
     def __attrs_post_init__(self):
-        self.name: str = type(self).__name__
-        self.description: str = type(self).__doc__
+        super().__attrs_post_init__()
+
         self.owner: 'GroupPolicy' = None
 
-    def catch_exceptions(self, task):
+    def catch_exceptions(self, task: 'Task') -> StepResult:
         try:
             result = task()
         except (ProcessExecutionError, KeyboardInterrupt, OSError) as ex:
@@ -72,26 +90,29 @@ class Task:
         return result
 
     @abstractmethod
-    def on_error(self, step_result, exception=None):
+    def on_error(self, task_result: StepResult, exception=None):
         """What do we do with an error."""
-        pass
 
     @abstractmethod
-    def on_result(self, step_result):
+    def on_result(self, task_result: StepResult):
         """What do we do with a result."""
-        pass
 
-    def can_continue(self, step_result):
+    def can_continue(self, task_result: StepResult) -> bool:
         """Can execution of multiple child steps continue?"""
         if self.owner:
-            return self.owner.can_continue(step_result)
+            return self.owner.can_continue(task_result)
         return True
 
-    def tasks(self):
+    def tasks(self) -> List[Union['Task', GroupPolicy]]:
         return [self]
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self) -> StepResult:
         """Perform actions that define this step implementation."""
+
+    def __str__(self, indent=0):
+        return textwrap.indent(
+            "* {name}: {desc}".format(name=self.name, desc=self.description),
+            indent * ' ')
 
 
 @attr.s
@@ -103,13 +124,16 @@ class Echo(Task):
         LOG.info(self.message)
         return StepResult.OK
 
+    def __str__(self, indent=0):
+        return textwrap.indent(self.message, indent * ' ')
+
 
 @attr.s
 class Run(Task):
     """Run a project."""
     project: Project = attr.ib()
 
-    def __call__(self):
+    def __call__(self) -> StepResult:
         result = self.project.run()
         return result
 
@@ -118,7 +142,7 @@ class Compile(Task):
     """Compile the project."""
     project: Project = attr.ib()
 
-    def __call__(self):
+    def __call__(self) -> StepResult:
         result = self.project.compile()
         return result
 
@@ -127,9 +151,10 @@ class MakeBuildDir(Task):
     """Create the build directory."""
     project: Project = attr.ib()
 
-    def __call__(self):
+    def __call__(self) -> StepResult:
         if not os.path.exists(self.project.builddir):
             mkdir("-p", self.project.builddir)
+        return StepResult.OK
 
 @attr.s
 class Clean(Task):
@@ -156,10 +181,10 @@ class Clean(Task):
                 else:
                     umount_paths.append(part.mountpoint)
 
-    def __call__(self):
+    def __call__(self) -> StepResult:
         if not CFG['clean']:
             LOG.warning("Clean disabled by config.")
-            return
+            return StepResult.CAN_CONTINUE
         obj_builddir = os.path.abspath(self.project.builddir)
         if os.path.exists(obj_builddir):
             LOG.debug("Path %s exists", obj_builddir)
@@ -170,12 +195,12 @@ class Clean(Task):
                 rm("-rf", obj_builddir)
         else:
             LOG.debug("Path %s did not exist anymore", obj_builddir)
-        self.status = StepResult.OK
+        return StepResult.OK
 
 @attr.s
 class CleanExtra(Task):
     """Cleans the extra directories."""
-    def __call__(self):
+    def __call__(self) -> StepResult:
         if not CFG['clean']:
             return StepResult.OK
 
@@ -183,16 +208,19 @@ class CleanExtra(Task):
         for p in paths:
             if os.path.exists(p):
                 rm("-r", p)
+        return StepResult.OK
+
+
 
 @attr.s
-class TaskManager:
+class TaskManager(NamedTaskEntity):
     plan: Union['GroupPolicy', 'Task'] = attr.ib()
 
     def __merge_results__(self, results, merge_results):
         if isinstance(merge_results, Iterable):
             return list(itertools.chain(results, merge_results))
 
-        new_results = results 
+        new_results = results
         new_results.append(merge_results)
         return new_results
 
@@ -209,13 +237,38 @@ class TaskManager:
 
     @contextmanager
     def execution_context(self, task: Task):
-        yield task
+        log_ctx = LogTasks(task)
+
+        mgrs = [
+            log_ctx
+        ]
+
+        with ExitStack() as stack:
+            for mgr in mgrs:
+                stack.enter_context(mgr)
+            yield task
+
+    def __str__(self, indent=0):
+        ret_str = textwrap.indent(
+            "* {name}: {desc}\n".format(name=self.name,
+                                        desc=self.description), indent * ' ')
+        ret_str += self.plan.__str__(indent=indent+1)
+        return ret_str
 
 @attr.s
 class Experiment(TaskManager, Task):
     """Run an experiment, wrapped in a db transaction."""
     experiment = attr.ib()
-    plan: List[Union['GroupPolicy', 'Task']] = attr.ib()
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        name = self.experiment.name
+        self.plan = FailOnError([
+            Echo(message="Start experiment: {0}".format(name)),
+            self.plan,
+            Echo(message="Completed experiment: {0}".format(name))
+        ])
+
 
     def begin_transaction(self):
         experiment, session = db.persist_experiment(self.experiment)
@@ -248,21 +301,22 @@ class Experiment(TaskManager, Task):
             LOG.error(inv_req)
 
     @contextmanager
-    def execution_context(self, task: Task):
+    def execution_context(self, task: Task) -> List[AbstractContextManager]:
         experiment, session = self.begin_transaction()
         yield task
         self.end_transaction(experiment, session)
 
-    def __extended_plan__(self):
-        name = self.experiment.name
-        extended_plan = FailOnError([
-            Echo(message="Start experiment: {0}".format(name)),
-            self.plan,
-            Echo(message="Completed experiment: {0}".format(name))
-        ])
-        return extended_plan
-
-    def __call__(self):
-        self.plan = self.__extended_plan__()
+    def __call__(self) -> StepResult:
         results = self.run()
         return results
+
+
+@attr.s
+class LogTasks(AbstractContextManager):
+    task: Task = attr.ib()
+
+    def __enter__(self):
+        LOG.info("\n%s - %s", self.task.name, self.task.description)
+
+    def __exit__(self, type, value, traceback):
+        LOG.info("%s COMPLETE")

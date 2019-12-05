fdbb56e98f066ee79ecda9b552927de668e35909
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2019-12-05 20:38:32 +0100

    downloads: export public symbols at the top-level

--- benchbuild/downloads/__init__.py
+++ benchbuild/downloads/__init__.py
@@ -3,3 +3,7 @@ Declarative API for downloading sources required by benchbuild.
 """
 from .git import Git
 from .http import HTTP
+
+from .base import default
+from .base import product
+from .base import BaseSource

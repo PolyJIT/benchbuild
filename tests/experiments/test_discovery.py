dc2814684a64108c67c155dad560dcbe9f2ad009
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-01-25 19:54:12 +0100

    test: reduce test-errors since project/experiment split

--- tests/experiments/test_discovery.py
+++ tests/experiments/test_discovery.py
@@ -1,19 +1,21 @@
 import logging
 
-from benchbuild.experiments import discover
+from benchbuild.plugins import discover
 from benchbuild.settings import CFG
 
 
 def test_discovery(caplog):
     caplog.set_level(logging.DEBUG, logger='benchbuild')
+    CFG['plugins']['projects'] = []
+    CFG['plugins']['reports'] = []
     CFG["plugins"]["experiments"] = [
         "benchbuild.non.existing", "benchbuild.reports.raw"
     ]
     discover()
 
     assert caplog.record_tuples == [
-        ('benchbuild.experiments', logging.ERROR,
+        ('benchbuild.plugins', logging.ERROR,
          "Could not find 'benchbuild.non.existing'"),
-        ('benchbuild.experiments', logging.ERROR,
+        ('benchbuild.plugins', logging.ERROR,
          "ImportError: No module named 'benchbuild.non'"),
     ]

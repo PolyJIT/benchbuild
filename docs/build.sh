49df7cc3c52c1d987cadd328669e66592e9a093d
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-06-16 01:22:06 +0200

    docs: portray needs to create the buildroot itself

--- docs/build.sh
+++ docs/build.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -eu
 
-die() { echo "ERROR: $*" >&2; exit 1; }
-
 ROOT="$(dirname "$(readlink -f "$0")")"
 BUILDROOT="$ROOT/build"
 
 echo
 echo 'Build documentation'
 echo
-
-mkdir -p "$BUILDROOT"
-[ -d "$BUILDROOT" ] || die "could not create BUILDROOT"
 
 portray as_html -o "$BUILDROOT"
 

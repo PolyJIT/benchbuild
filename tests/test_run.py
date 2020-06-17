c4d5940a28cdda27484a0f6e43476eba10970a60
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-06-17 09:21:42 +0200

    tests: remove dead test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,7 +1,6 @@
 """
 This Test will run through benchbuild's execution pipeline.
 """
-import os
 import unittest
 
 from benchbuild.utils import cmd
@@ -50,44 +49,3 @@ class TestShadow(unittest.TestCase):
         self.assertEqual(mkdir.formulate(),
                          outside.formulate(),
                          msg="mkdir (before) is not the same as mkdir (after)")
-
-
-class TestRun(unittest.TestCase):
-
-    @shadow_commands("true")
-    def test_run(self):
-        from benchbuild import experiment
-        from benchbuild.utils.actions import Experiment
-
-        class MockExp(experiment.Experiment):
-            NAME = "mock-exp"
-
-            def actions_for_project(self, project):
-                from benchbuild.utils.actions import (Prepare, Download,
-                                                      Configure, Build, Run,
-                                                      Clean)
-                actns = []
-                project.builddir = "/tmp/throwaway"
-                actns = [
-                    Prepare(project),
-                    Download(project),
-                    Configure(project),
-                    Build(project),
-                    Run(project),
-                    Clean(project)
-                ]
-                return actns
-
-        exp = MockExp()
-        eactn = Experiment(obj=exp, actions=exp.actions())
-        old_exists = os.path.exists
-        os.path.exists = lambda p: True
-        print(eactn)
-        eactn()
-        os.path.exists = old_exists
-
-
-if __name__ == "__main__":
-    from benchbuild.utils import log
-    log.configure()
-    TestRun().test_run()

fb08563a3a2615c570460c331eb840c90ed0c5d4
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-07-24 08:53:44 +0200

    tests: switch test experiment to 'empty'

--- tests/integration/test_cli_slurm.py
+++ tests/integration/test_cli_slurm.py
@@ -7,6 +7,6 @@ from benchbuild.cli.slurm import Slurm
 def test_slurm_command(tmp_path):
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         with local.cwd(tmp_path):
-            Slurm.run(argv=['slurm', '-E', 'raw', 'test'])
+            Slurm.run(argv=['slurm', '-E', 'empty', 'test'])
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 0

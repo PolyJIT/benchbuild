75ac44547397cc0ec90db17446e256a39dbceee4
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-07-28 00:40:40 +0200

    fix: restore experiment registry & settings after testing.
    This fixes test errors when running the complete test suite

--- tests/integration/test_cli_slurm.py
+++ tests/integration/test_cli_slurm.py
@@ -1,10 +1,16 @@
 import pytest
 from plumbum import local
+from rich import print
 
 from benchbuild.cli.slurm import Slurm
+from benchbuild.experiment import ExperimentRegistry
+from benchbuild.settings import CFG
 
 
 def test_slurm_command(tmp_path):
+    print(CFG['plugins']['experiments'])
+
+    print(ExperimentRegistry.experiments.items())
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         with local.cwd(tmp_path):
             Slurm.run(argv=['slurm', '-E', 'empty', 'test'])

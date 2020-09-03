2595d675d9566ea654630886c458b7f5c4b7981f
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-09-03 02:59:25 +0200

    fix: no-measurement experiment requires compiler_extension

--- benchbuild/experiments/empty.py
+++ benchbuild/experiments/empty.py
@@ -29,5 +29,7 @@ class NoMeasurement(Experiment):
 
     def actions_for_project(self, project):
         """Execute all actions but don't do anything as extension."""
+        project.compiler_extension = run.WithTimeout(
+            compiler.RunCompiler(project, self))
         project.runtime_extension = run.RuntimeExtension(project, self)
         return self.default_runtime_actions(project)

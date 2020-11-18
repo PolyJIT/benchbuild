189d212637ed8cc719c7e17440e4008aed5d4b9e
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-11-19 00:02:27 +0100

    Add support for Protocols when using python3.7

--- benchbuild/environments/service_layer/ensure.py
+++ benchbuild/environments/service_layer/ensure.py
@@ -1,13 +1,18 @@
-import typing as tp
+import sys
 
 from . import unit_of_work
+
+if sys.version_info <= (3, 8):
+    from typing_extensions import Protocol
+else:
+    from typing import Protocol
 
 
 class ImageNotFound(Exception):
     ...
 
 
-class NamedCommand(tp.Protocol):
+class NamedCommand(Protocol):
 
     @property
     def name(self) -> str:
--- setup.py
+++ setup.py
@@ -33,8 +33,9 @@ setup(
     install_requires=[
         "Jinja2~=2.10", "PyYAML~=5.1", "attrs>=19.3,<21.0", "dill~=0.3",
         "pathos~=0.2", "parse~=1.14", "pdoc3~=0.8", "plumbum~=1.6",
-        "psutil~=5.6", "psycopg2-binary~=2.8", "pygit2>=1.2.1,<1.5.0", "pygtrie~=2.3",
-        "pyparsing~=2.4", "rich>=6.1,<10.0", "sqlalchemy-migrate~=0.13",
+        "psutil~=5.6", "psycopg2-binary~=2.8", "pygit2>=1.2.1,<1.5.0",
+        "pygtrie~=2.3", "pyparsing~=2.4", "rich>=6.1,<10.0",
+        "sqlalchemy-migrate~=0.13", "typing-extensions~=3.7.4.3",
         "virtualenv>=16.7,<21.0"
     ],
     author="Andreas Simbuerger",

fd048b0a4fa5ede24f71fe3ace32ffed72d9df61
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2020-11-23 00:41:24 +0100

    Add fixtures to test git repositories
    This adds 3 fixtures to our tests for easier testing against git repositories.
    
    1. mk_git_repo:
    
    A factory test fixture that can generate a random git repository in temporary
    directory. The repo will be filled with a number of random small commits.
    You cannot make any assumptions about file contents.
    You can add an optional git submodule to the repository by giving it an
    existing git repo, created by this fixture before.
    
    2. simple_repo:
    Wrap mk_git_repo and provide a simple git repository without submodule.
    
    3. repo_with_submodule:
    Wrap mk_git_repo and create 2 repositories, where one is submodule of the other.

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,10 +1,13 @@
 # pylint: disable=redefined-outer-name
+import tempfile as tf
 import typing as tp
 from unittest import mock
 
 import attr
+import git
 import plumbum as pb
 import pytest
+import testdata as td
 
 from benchbuild import source
 from benchbuild.source import FetchableSource, Variant
@@ -179,9 +182,68 @@ def test_versions_product():
     pass
 
 
-def describe_git_submodules():
+RepoT = tp.Tuple[pb.local.path, git.Repo]
 
-    def versions_do_not_get_expanded():
+
+@pytest.fixture
+def mk_git_repo():
+    tmp_dir = pb.local.path(tf.mkdtemp())
+
+    def _git_repository(
+        num_commits: int = 2,
+        git_submodule: tp.Optional[git.Repo] = None
+    ) -> RepoT:
+        nonlocal tmp_dir
+
+        a_repo_name = td.get_file_name()
+        a_repo_base = tmp_dir / a_repo_name
+        repo = git.Repo.init(a_repo_base)
+
+        for _ in range(num_commits):
+            some_content = td.get_str(255)
+            a_name = td.get_file_name()
+            a_file = td.create_file(
+                a_name, contents=some_content, tmpdir=a_repo_base
+            )
+            repo.index.add(a_file)
+            repo.index.commit(f'Add {a_name}')
+
+        if git_submodule:
+            a_sm_path = td.get_file_name()
+            a_sm_name = td.get_file_name()
+            repo.create_submodule(
+                a_sm_name,
+                a_sm_path,
+                url=git_submodule.git_dir,
+                branch='master'
+            )
+            repo.index.commit(
+                f'Add submodule {a_sm_name} to {a_sm_path} from: {repo.git_dir}'
+            )
+
+            return (tmp_dir, repo)
+        return (tmp_dir, repo)
+
+    yield _git_repository
+    tmp_dir.delete()
+
+
+@pytest.fixture
+def simple_repo(mk_git_repo) -> RepoT:
+    yield mk_git_repo()
+
+
+@pytest.fixture
+def repo_with_submodule(mk_git_repo) -> RepoT:
+    base_dir, sub_repo = mk_git_repo()
+    _, main_repo = mk_git_repo(git_submodule=sub_repo)
+
+    yield (base_dir, main_repo)
+
+
+def describe_git():
+
+    def submodule_versions_do_not_get_expanded():
         git_repo = source.Git('remote.git', 'local.git', clone=False)
         git_repo.versions = mock.MagicMock(name='versions')
         git_repo.versions.return_value = ['1', '2', '3']

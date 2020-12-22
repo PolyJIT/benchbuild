4982efe88c2038a64c7008803f2a878c742c2554
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2020-12-22 13:13:25 +0100

    Pass the image name to image_exists() instead of the command. (#350)
    Resolves #349

--- benchbuild/environments/service_layer/ensure.py
+++ benchbuild/environments/service_layer/ensure.py
@@ -1,27 +1,13 @@
-import sys
-
 from . import unit_of_work
-
-if sys.version_info <= (3, 8):
-    from typing_extensions import Protocol
-else:
-    from typing import Protocol
 
 
 class ImageNotFound(Exception):
     pass
 
 
-class NamedCommand(Protocol):
-
-    @property
-    def name(self) -> str:
-        ...
-
-
 def image_exists(
-    cmd: NamedCommand, uow: unit_of_work.AbstractUnitOfWork
+    image_name: str, uow: unit_of_work.AbstractUnitOfWork
 ) -> None:
-    image = uow.registry.get_image(cmd.name)
+    image = uow.registry.get_image(image_name)
     if not image:
-        raise ImageNotFound(cmd)
+        raise ImageNotFound(image_name)

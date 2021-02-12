7af65f194227f68b876ecd7f7f38b59e6edc4c0f
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2021-02-12 01:03:18 +0100

    feat(cli/project): change project view to a condensed format

--- benchbuild/cli/project.py
+++ benchbuild/cli/project.py
@@ -1,5 +1,6 @@
 """Subcommand for project handling."""
 import typing as tp
+from functools import reduce
 
 from plumbum import cli
 
@@ -41,14 +42,12 @@ def print_projects(projects: ProjectIndex) -> None:
         projects: The populated project index to print.
 
     """
-    grouped_by: tp.Dict[str, tp.List[str]] = {}
     if not projects:
         print("Your selection didn't include any projects for this experiment.")
         return
 
-    for name in projects:
-        prj = projects[name]
-
+    grouped_by: tp.Dict[str, tp.List[str]] = {}
+    for prj in set(projects.values()):
         if prj.GROUP not in grouped_by:
             grouped_by[prj.GROUP] = []
 
@@ -56,39 +55,42 @@ def print_projects(projects: ProjectIndex) -> None:
             "{name}/{group}".format(name=prj.NAME, group=prj.GROUP)
         )
 
+    project_column_width = max([
+        len(f'{p.NAME}/{p.GROUP}') for p in projects.values()
+    ])
+    project_header_format = "{:<{width}} | {:^15} | {:^15} | {}"
+    project_row_format = "{:<{width}} | {:^15} | {:^15} | {}"
+
     for name in grouped_by:
-        print(f'::  group: {name}')
         group_projects = sorted(grouped_by[name])
+        print(
+            project_header_format.format(
+                f'{name}',
+                "# Sources",
+                "# Versions",
+                "Description",
+                width=project_column_width
+            )
+        )
+        print()
         for prj_name in group_projects:
             prj_cls = projects[prj_name]
-
             project_id = f'{prj_cls.NAME}/{prj_cls.GROUP}'
-            project_version = str(bb.source.default(*prj_cls.SOURCE))
-
-            project_lines = [
-                f'::  {project_id}'
-                f'    default: {project_version:<24}'
-            ]
-            for src in prj_cls.SOURCE:
-                source_lines = [
-                    f'\n    * source: {src.local}',
-                ]
-                if isinstance(src.remote, str):
-                    source_lines.append(f' remote: {src.remote}')
-                    source_lines.extend([
-                        f'\n      - {str(version)}'
-                        for version in src.versions()
-                    ])
-                else:
-                    source_lines.extend([
-                        f'\n      - {str(version)} '
-                        f'remote: {src.remote[str(version)]}'
-                        for version in src.versions()
-                    ])
-                project_lines.extend(source_lines)
-
-            print(*project_lines)
+            num_project_sources = len(prj_cls.SOURCE)
+            num_combinations = reduce(
+                lambda x, y: x * y,
+                [len(src.versions()) for src in prj_cls.SOURCE]
+            )
+            docstr = ""
             if prj_cls.__doc__:
                 docstr = prj_cls.__doc__.strip("\n ")
-                print(f'    description: {docstr}')
+            print(
+                project_row_format.format(
+                    project_id,
+                    num_project_sources,
+                    num_combinations,
+                    docstr,
+                    width=project_column_width
+                )
+            )
         print()

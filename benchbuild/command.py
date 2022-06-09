02b12716bf690fc7a98af6bf78b799694ea7b4e8
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2022-06-09 21:37:05 +0200

    feat: add env and __str__ to commands

--- benchbuild/command.py
+++ benchbuild/command.py
@@ -2,7 +2,6 @@ import typing as tp
 from pathlib import Path, PosixPath
 
 from plumbum import local
-from plumbum.commands.base import BaseCommand
 
 from benchbuild.project import Project
 from benchbuild.utils.run import watch
@@ -26,14 +25,15 @@ class Command:
         *args: tp.Any,
         output: Path = None,
         output_param: tp.List[str] = None,
-        env: tp.Dict[str, str] = None
+        env: tp.Dict[str, str] = None,
     ) -> None:
 
         self._path = path
+        self._args = args if args is not None else []
         self._output = output
-        self._output_param = output_param
-        self._args = args
-        self._env = env
+
+        self._output_param = output_param if output_param is not None else []
+        self._env = env if env is not None else {}
 
     @property
     def name(self) -> str:
@@ -51,26 +51,38 @@ class Command:
     def dirname(self) -> Path:
         return self._path.parent
 
+    @property
+    def output(self) -> Path:
+        return self._output
+
     def exists(self) -> bool:
         return self._path.exists()
 
-    def __getitem__(self, *args: tp.Any) -> 'Command':
+    def env(self, **kwargs: str) -> None:
+        self._env.update(kwargs)
+
+    def __getitem__(self, *args: tp.Any) -> "Command":
         return Command(
-            self._path,
-            *(*self._args, *args),
-            output=self._output,
-            env=self._env
+            self._path, *(*self._args, *args), output=self._output, env=self._env
         )
 
     def __call__(self, *args: tp.Any) -> tp.Any:
         if self.exists():
             cmd = local[str(self.path)]
-            output_params = \
-                    [arg.format(output=self.output) for arg in self._output_param]
+            output_params = [
+                arg.format(output=self.output) for arg in self._output_param
+            ]
             cmd_w_output = cmd[output_params]
 
             return cmd_w_output(*args)
         return None
+
+    def __str__(self) -> str:
+        env_str = " ".join([f"{k}={v}" for k, v in self._env.items()])
+        print(self._args)
+        args_str = " ".join(self._args)
+
+        return f"{env_str} {self._path} {args_str}"
 
 
 class ProjectCommand:
@@ -86,7 +98,7 @@ class ProjectCommand:
         path = self.command.path
 
         num_parents = len(path.parents)
-        assert num_parents >= 2, f'expected 2 parent path elements, got {num_parents}'
+        assert num_parents >= 2, f"expected 2 parent path elements, got {num_parents}"
 
         source_name = path.parts[0]
         source_dir = self.project.source_of(source_name)

51d99e585c322912da2ca831a76df0b9b0d137d0
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2022-07-05 00:33:48 +0200

    feat(workload): convert scimark2

--- benchbuild/projects/apollo/scimark.py
+++ benchbuild/projects/apollo/scimark.py
@@ -1,6 +1,7 @@
 from plumbum import local
 
 import benchbuild as bb
+from benchbuild.command import Command, WorkloadSet, SourceRoot
 from benchbuild.environments.domain.declarative import ContainerImage
 from benchbuild.source import HTTP
 from benchbuild.utils.cmd import make, unzip
@@ -21,14 +22,11 @@ class SciMark(bb.Project):
     ]
     CONTAINER = ContainerImage().from_('benchbuild:alpine')
 
+    JOBS = {WorkloadSet(): [Command(SourceRoot(".") / "scimark2")]}
+
     def compile(self):
         scimark_source = local.path(self.source_of('scimark.zip'))
         clang = bb.compiler.cc(self)
         _clang = bb.watch(clang)
         unzip(local.cwd / scimark_source)
         make("CC=" + str(_clang), "scimark2")
-
-    def run_tests(self):
-        scimark2 = bb.wrap(local.path('scimark2'), self)
-        _scimark2 = bb.watch(scimark2)
-        _scimark2()

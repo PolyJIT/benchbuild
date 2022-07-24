6eaaf7f4c59a24c5fcec2c33e112f9ba1a76f16f
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2022-07-24 14:25:04 +0200

    fix(x264): repair broken cli args

--- benchbuild/projects/benchbuild/x264.py
+++ benchbuild/projects/benchbuild/x264.py
@@ -39,39 +39,39 @@ class X264(bb.Project):
     WORKLOADS = {
         WorkloadSet(inputfile="tbbt-small.y4m"): [
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--crf", "30", "-b1", "-m1", "-r1", "--me", "dia" "--no-cabac", "--direct temporal", "--ssim", "--no-weightb"),
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--crf", "30", "-b1", "-m1", "-r1", "--me", "dia", "--no-cabac", "--direct", "temporal", "--ssim", "--no-weightb"),
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--crf", "16", "-b2", "-m3", "-r3", "--me", "hex", "--no-8x8dct", "--direct", "spatial", "--no-dct-decimate", "-t0", "", "--slice-max-mbs", "50"),
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--crf", "16", "-b2", "-m3", "-r3", "--me", "hex", "--no-8x8dct", "--direct", "spatial", "--no-dct-decimate", "-t0", "--slice-max-mbs", "50"),
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--cr", "2", "-b", "-m", "-r", "--m", "he", "--cq", "jv", "--n", "10", "--psn", "--no-mixed-ref", "--b-adap", "", "--slice-max-siz", "1500"),
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--crf", "26", "-b4", "-m5", "-r2", "--me", "hex", "--cqm", "jvt", "--nr", "100", "--psnr", "--no-mixed-refs", "--b-adapt", "2", "--slice-max-size", "1500"),
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--cr", "1", "-b", "-m", "-r", "--m", "um", "-t", "-", "al", "--b-pyrami", "norma", "--direc", "aut", "--no-fast-pski", "--no-mbtree"),
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--crf", "18", "-b3", "-m9", "-r5", "--me", "umh", "-t1", "-A", "all", "--b-pyramid", "normal", "--direct", "auto", "--no-fast-pskip", "--no-mbtree"),
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--cr", "2", "-b", "-m", "-r", "--m", "es", "-t", "-", "al", "--psy-r", "1.0:1.", "--slice", "4"),
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--crf", "22", "-b3", "-m7", "-r4", "--me", "esa", "-t2", "-A", "all", "--psy-rd", "1.0:1.0", "--slices", "4"),
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--frame", "5", "--cr", "2", "-b", "-m1", "-r", "--m", "tes", "-t2"),
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--frames", "50", "--crf", "24", "-b3", "-m10", "-r3", "--me", "tesa", "-t2"),
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--frame", "5", "-q", "-m", "-r", "--m", "he", "-Aall"),
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--frames", "50", "-q0", "-m9", "-r2", "--me", "hex", "-Aall"),
             Command(SourceRoot("x264.git") / "x264",
-                "tbbt-small.raw", "--threads", "1", "-o", "/dev/null", "--frame", "5", "-q", "-m", "-r", "--m", "he", "--no-cabac")
+                "tbbt-small.y4m", "--threads", "1", "-o", "/dev/null", "--frames", "50", "-q0", "-m2", "-r1", "--me", "hex", "--no-cabac")
         ],
         WorkloadSet(inputfile="sintel.raw"): [
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--crf", "30", "-b1", "-m1", "-r1", "--me", "dia" "--no-cabac", "--direct temporal", "--ssim", "--no-weightb"),
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--crf", "30", "-b1", "-m1", "-r1", "--me", "dia", "--no-cabac", "--direct", "temporal", "--ssim", "--no-weightb"),
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--crf", "16", "-b2", "-m3", "-r3", "--me", "hex", "--no-8x8dct", "--direct", "spatial", "--no-dct-decimate", "-t0", "", "--slice-max-mbs", "50"),
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--crf", "16", "-b2", "-m3", "-r3", "--me", "hex", "--no-8x8dct", "--direct", "spatial", "--no-dct-decimate", "-t0", "--slice-max-mbs", "50"),
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--cr", "2", "-b", "-m", "-r", "--m", "he", "--cq", "jv", "--n", "10", "--psn", "--no-mixed-ref", "--b-adap", "", "--slice-max-siz", "1500"),
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--crf", "26", "-b4", "-m5", "-r2", "--me", "hex", "--cqm", "jvt", "--nr", "100", "--psnr", "--no-mixed-refs", "--b-adapt", "2", "--slice-max-size", "1500"),
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--cr", "1", "-b", "-m", "-r", "--m", "um", "-t", "-", "al", "--b-pyrami", "norma", "--direc", "aut", "--no-fast-pski", "--no-mbtree"),
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--crf", "18", "-b3", "-m9", "-r5", "--me", "umh", "-t1", "-A", "all", "--b-pyramid", "normal", "--direct", "auto", "--no-fast-pskip", "--no-mbtree"),
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--cr", "2", "-b", "-m", "-r", "--m", "es", "-t", "-", "al", "--psy-r", "1.0:1.", "--slice", "4"),
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--crf", "22", "-b3", "-m7", "-r4", "--me", "esa", "-t2", "-A", "all", "--psy-rd", "1.0:1.0", "--slices", "4"),
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--frame", "5", "--cr", "2", "-b", "-m1", "-r", "--m", "tes", "-t2"),
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--frames", "50", "--crf", "24", "-b3", "-m10", "-r3", "--me", "tesa", "-t2"),
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--frame", "5", "-q", "-m", "-r", "--m", "he", "-Aall"),
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--frames", "50", "-q0", "-m9", "-r2", "--me", "hex", "-Aall"),
             Command(SourceRoot("x264.git") / "x264",
-                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--frame", "5", "-q", "-m", "-r", "--m", "he", "--no-cabac")
+                "sintel.raw", "--input-res", "1280x720", "--threads", "1", "-o", "/dev/null", "--frames", "50", "-q0", "-m2", "-r1", "--me", "hex", "--no-cabac")
         ]
     }
     # yapf: enable

98ba93865c6209f3583416933a74caebd7b84713
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2022-09-21 17:09:01 +0200

    fix(command): use _is_relative_to where needed

--- benchbuild/command.py
+++ benchbuild/command.py
@@ -646,25 +646,25 @@ class ProjectCommand:
         return str(self.command)
 
 
+def _is_relative_to(p: Path, other: Path) -> bool:
+    if sys.version_info <= (3, 8):
+        try:
+            p.relative_to(other)
+            return True
+        except ValueError:
+            return False
+    return p.is_relative_to(other)
+
+
 def _default_prune(project_command: ProjectCommand) -> None:
     command = project_command.command
     project = project_command.project
     builddir = Path(str(project.builddir))
 
-    def is_relative_to(p: Path, other: Path) -> bool:
-        if sys.version_info <= (3, 8):
-            try:
-                p.relative_to(other)
-                return True
-            except ValueError:
-                return False
-        else:
-            return p.is_relative_to(other)
-
     for created in command.creates:
         created_path = created.render(project=project)
         if created_path.exists() and created_path.is_file():
-            if not is_relative_to(created_path, builddir):
+            if not _is_relative_to(created_path, builddir):
                 LOG.error("Pruning outside project builddir was rejected!")
             else:
                 created_path.unlink()
@@ -683,7 +683,7 @@ def _default_backup(
         backup_path = backup.render(project=project)
         backup_destination = backup_path.with_suffix(_suffix)
         if backup_path.exists():
-            if not backup_path.is_relative_to(builddir):
+            if not _is_relative_to(backup_path, builddir):
                 LOG.error("Backup outside project builddir was rejected!")
             else:
                 shutil.copy(backup_path, backup_destination)

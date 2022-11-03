ca716b1ff6ea632e5f4aa04eac9cbf73de5bd9ac
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2022-11-03 23:17:19 +0100

    style: specify encoding for open call

--- benchbuild/projects/test/test.py
+++ benchbuild/projects/test/test.py
@@ -172,7 +172,7 @@ class CAWTestProject(project.Project):
     CONTAINER = ContainerImage().from_('benchbuild:alpine')
 
     def compile(self):
-        with open('test.cpp', 'w') as test_source:
+        with open('test.cpp', 'w', encoding="utf-8") as test_source:
             lines = """
 #include <iostream>
 int main(int argc, char **argv) {

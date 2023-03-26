f60837605854315f89619705eea202c00c6637b9
Jonas Kaufmann <jokau03@gmail.com>
2023-03-26 13:50:33 +0200

    __init__.py: call __SETTINGS__.setup_config() only when any plugin was imported

--- benchbuild/plugins.py
+++ benchbuild/plugins.py
@@ -23,15 +23,19 @@ from benchbuild.settings import CFG
 LOG = logging.getLogger(__name__)
 
 
-def discover() -> None:
+def discover() -> bool:
     """Import all plugins listed in our configuration."""
+    something_imported = False
     if CFG["plugins"]["autoload"]:
         experiment_plugins = CFG["plugins"]["experiments"].value
         project_plugins = CFG["plugins"]["projects"].value
 
         for plugin in itertools.chain(experiment_plugins, project_plugins):
+            something_imported = True
             try:
                 importlib.import_module(plugin)
             except ImportError as import_error:
                 LOG.error("Could not find '%s'", import_error.name)
                 LOG.debug("ImportError: %s", import_error)
+
+    return something_imported

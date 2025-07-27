50ebdb5486def6a5a75a3b8bbd7d8534dc450675
Andreas Simbuerger <simbuerg@lairosiel.de>
2025-07-27 11:11:03 +0200

    add missing requirement to six

--- benchbuild/_version.py
+++ benchbuild/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '6.8'
-__version_tuple__ = version_tuple = (6, 8)
+__version__ = version = '6.9.dev1+gdc2f4cb5.d20250727'
+__version_tuple__ = version_tuple = (6, 9, 'dev1', 'gdc2f4cb5.d20250727')

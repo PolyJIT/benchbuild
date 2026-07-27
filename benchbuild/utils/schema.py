b008b9db94143ca8f28a4cdbcfc8b533793e65d2
Andreas Simbuerger <simbuerg@lairosiel.de>
2026-07-27 23:28:28 +0200

    refactor: sqlalchemy.ext.declarative -> sqlalchemy.orm.declarative_base

--- benchbuild/utils/schema.py
+++ benchbuild/utils/schema.py
@@ -39,8 +39,7 @@ from sqlalchemy import (
     create_engine,
 )
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.types import CHAR, Float, TypeDecorator
 
 from benchbuild import settings
